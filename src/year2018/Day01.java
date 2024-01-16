f6c99809a3903b3bf9a05bfe11bee420df378498
Sei-Jin <kajiquiet@gmail.com>
2024-01-16 09:32:55 -0400

    2018-D1: Begin part two

--- src/year2018/Day01.java
+++ src/year2018/Day01.java
@@ -3,7 +3,9 @@ package year2018;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * --- Day 1: Chronal Calibration ---
@@ -12,9 +14,10 @@ public class Day01
 {
     public static void main(String[] args) throws IOException
     {
-        List<String> lines = Files.readAllLines(Path.of("input.txt"));
+        List<String> lines = Files.readAllLines(Path.of("sample.txt"));
 
         partOne(lines);
+        partTwo(lines);
     }
 
     private static void partOne(List<String> lines)
@@ -26,16 +29,57 @@ public class Day01
             char sign = line.charAt(0);
             int value = Integer.parseInt(line.substring(1));
 
-            if (sign == '+')
+            if (sign == '-')
             {
-                total += value;
+                value *= -1;
+            }
+
+            total += value;
+        }
+
+        System.out.println("The resulting frequency is: " + total);
+    }
+
+    private static void partTwo(List<String> lines)
+    {
+        Set<Integer> seenValues = new HashSet<>();
+
+        int startingValue = 0;
+        seenValues.add(startingValue);
+        int total = startingValue;
+
+        boolean sameFrequency = false;
+
+        for (String line : lines)
+        {
+            char sign = line.charAt(0);
+            int value = Integer.parseInt(line.substring(1));
+
+            if (sign == '-')
+            {
+                value *= -1;
+            }
+
+            total += value;
+
+            if (seenValues.contains(total))
+            {
+                sameFrequency = true;
+                break;
             }
             else
             {
-                total -= value;
+                seenValues.add(total);
             }
         }
 
-        System.out.println(total);
+        if (sameFrequency)
+        {
+            System.out.println("The first frequency reached twice is: " + total);
+        }
+        else
+        {
+            System.out.println("No frequencies reached twice!");
+        }
     }
 }
