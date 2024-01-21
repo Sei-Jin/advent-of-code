099370b174c64fbe67bf18a9194065fdcc63dd0f
SharkFinStew <kajiquiet@gmail.com>
2024-01-21 10:12:51 -0400

    2015-D6: Complete refactoring

--- src/year2015/Day06.java
+++ src/year2015/Day06.java
@@ -1,4 +1,4 @@
-package year2015.Day6;
+package year2015;
 
 import java.awt.*;
 import java.io.IOException;
@@ -7,13 +7,14 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
-public class Part1 {
+public class Day06 {
     
     public static void main(String[] args) throws IOException {
 
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
         
         partOne(inputLines);
+        partTwo(inputLines);
     }
     
     
@@ -35,18 +36,51 @@ public class Part1 {
             }
         }
         
-        int onLights = 0;
-        
-        // Count how many lights are turned on
+        int totalLightsTurnedOn = 0;
+
         for (int row = 0; row < lightGrid.length; row++) {
             for (int column = 0; column < lightGrid[0].length; column++) {
                 if (lightGrid[row][column]) {
-                    onLights++;
+                    totalLightsTurnedOn++;
                 }
             }
         }
         
-        System.out.println("After following the instructions, the number of lit lights is: " + onLights);
+        System.out.println("After following the instructions, the number of lit lights is: " + totalLightsTurnedOn);
+    }
+    
+    
+    private static void partTwo (List<String> inputLines) {
+        
+        int[][] lightGrid = new int[1000][1000];
+        
+        for (String line : inputLines) {
+            Instruction instruction = getInstruction(line);
+            
+            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
+                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
+                    switch (instruction.operation) {
+                        case ON -> lightGrid[row][column]++;
+                        case OFF -> {
+                            if (lightGrid[row][column] > 0) {
+                                lightGrid[row][column]--;
+                            }
+                        }
+                        case TOGGLE -> lightGrid[row][column] += 2;
+                    }
+                }
+            }
+        }
+        
+        int totalBrightness = 0;
+        
+        for (int row = 0; row < lightGrid.length; row++) {
+            for (int column = 0; column < lightGrid[0].length; column++) {
+                totalBrightness += lightGrid[row][column];
+            }
+        }
+        
+        System.out.println("The total brightness of all lights combined after Santa's instructions is: " + totalBrightness);
     }
     
     
