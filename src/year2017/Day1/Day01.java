16ec3b17c40721e3f123fb40854636ff21792d99
SharkFinStew <kajiquiet@gmail.com>
2024-01-20 21:14:00 -0400

    2017-D1: Complete refactoring

---
+++ src/year2017/Day1/Day01.java
@@ -0,0 +1,53 @@
+package year2017.Day1;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * --- Day 1: Inverse Captcha ---
+ */
+public class Day01 {
+    
+    public static void main(String[] args) throws IOException {
+
+        List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
+        
+        for (String line : inputLines) {
+            partOne(line);
+            partTwo(line);
+        }
+    }
+    
+    
+    private static void partOne (String line) {
+        
+        int solution = solveCaptcha(line, 1);
+        
+        System.out.println("The solution to the captcha is: " + solution);
+    }
+    
+    
+    private static void partTwo (String line) {
+        
+        int solution = solveCaptcha(line, line.length() / 2);
+        
+        System.out.println("The solution to the new captcha is: " + solution);
+    }
+    
+    
+    private static int solveCaptcha(String line, int offset) {
+        
+        int runningTotal = 0;
+        
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == line.charAt((i + offset) % line.length())) {
+                runningTotal += Character.getNumericValue(line.charAt(i));
+            }
+        }
+        
+        return runningTotal;
+    }
+}
