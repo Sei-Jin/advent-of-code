9764e8378323d838d591fa2e8e405cc4c0bda977
SharkFinStew <kajiquiet@gmail.com>
2024-01-21 09:55:35 -0400

    2015-D6: Complete refactoring of part 1

--- src/year2015/Day6/Part1.java
+++ src/year2015/Day6/Part1.java
@@ -1,52 +1,42 @@
 package year2015.Day6;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Part1 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        File inputFile = new File("input.txt");
-        Scanner in = new Scanner(inputFile);
-
+        List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
+        
+        partOne(inputLines);
+    }
+    
+    
+    private static void partOne(List<String> inputLines) {
+        
         boolean[][] lightGrid = new boolean[1000][1000];
-
-        while (in.hasNext()) {
-
-            String operation = in.next();
-            if (operation.compareTo("turn") == 0) {
-                operation = in.next();
-            }
-
-            in.useDelimiter("[ ,\n]");
-            int topCornerX = in.nextInt();
-            int topCornerY = in.nextInt();
-
-            in.next();
-            int bottomCornerX = in.nextInt();
-            int bottomCornerY = in.nextInt();
-
-            for (int row = topCornerY; row <= bottomCornerY; row++) {
-                for (int column = topCornerX; column <= bottomCornerX; column++) {
-                    if (operation.compareTo("on") == 0) {
-                        lightGrid[row][column] = true;
-                    }
-
-                    if (operation.compareTo("off") == 0) {
-                        lightGrid[row][column] = false;
-                    }
-
-                    if (operation.compareTo("toggle") == 0) {
-                        lightGrid[row][column] = !lightGrid[row][column];
+        
+        for (String line : inputLines) {
+            Instruction instruction = getInstruction(line);
+            
+            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
+                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
+                    switch (instruction.operation) {
+                        case ON -> lightGrid[row][column] = true;
+                        case OFF -> lightGrid[row][column] = false;
+                        case TOGGLE -> lightGrid[row][column] = !lightGrid[row][column];
                     }
                 }
             }
-
         }
-
+        
         int onLights = 0;
-
+        
         // Count how many lights are turned on
         for (int row = 0; row < lightGrid.length; row++) {
             for (int column = 0; column < lightGrid[0].length; column++) {
@@ -55,7 +45,54 @@ public class Part1 {
                 }
             }
         }
+        
+        System.out.println("After following the instructions, the number of lit lights is: " + onLights);
+    }
+    
+    
+    private static Instruction getInstruction (String line) {
+        
+        int[] values = Arrays.stream(line.split("[^0-9]+"))
+                .skip(1)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        
+        int topCornerX = values[0];
+        int topCornerY = values[1];
+        Point topCorner = new Point(topCornerX, topCornerY);
+        
+        int bottomCornerX = values[2];
+        int bottomCornerY = values[3];
+        Point bottomCorner = new Point(bottomCornerX, bottomCornerY);
+        
+        Operation operation = null;
+        if (line.contains(Operation.ON.getValue())) {
+            operation = Operation.ON;
+        } else if (line.contains(Operation.OFF.getValue())) {
+            operation = Operation.OFF;
+        } else if (line.contains(Operation.TOGGLE.getValue())) {
+            operation = Operation.TOGGLE;
+        }
+        
+        return new Instruction(operation, topCorner, bottomCorner);
+    }
 
-        System.out.println(onLights);
+
+    private record Instruction (Operation operation, Point topCorner, Point bottomCorner) {}
+    
+    
+    private enum Operation {
+        
+        ON ("on"),
+        OFF ("off"),
+        TOGGLE ("toggle");
+        
+        private final String value;
+        
+        Operation(String value) {
+            this.value = value;
+        }
+        
+        private String getValue() { return value; }
     }
 }
