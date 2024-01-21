768dcf228f5ca29a8f7229ab057bcc46bdcb6128
SharkFinStew <kajiquiet@gmail.com>
2024-01-21 09:56:46 -0400

    2015-D6: Remove unused import

--- src/year2015/Day6/Part1.java
+++ src/year2015/Day6/Part1.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Part1 {
     public static void main(String[] args) throws IOException {
