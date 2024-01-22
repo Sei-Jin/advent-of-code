8c466036ad23f24d16225589e21bba165b85290e
Sei-Jin <kajiquiet@gmail.com>
2024-01-22 12:21:21 -0400

    2020-D1: Add whitespace

--- src/year2020/Day01.java
+++ src/year2020/Day01.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class Day01 {
     
     public static void main(String[] args) throws IOException {
+        
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
         
         List<Integer> expenseReport = new ArrayList<>();
