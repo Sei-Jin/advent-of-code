89b636dcc5160873911d06a04c719de22442cb60
Sei-Jin <kajiquiet@gmail.com>
2024-01-22 16:57:02 -0400

    2021-D2: Solved part 2

--- src/year2021/Day02.java
+++ src/year2021/Day02.java
@@ -15,6 +15,7 @@ public class Day02 {
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
         
         partOne(inputLines);
+        partTwo(inputLines);
     }
     
     
@@ -49,7 +50,42 @@ public class Day02 {
     }
     
     
+    private static void partTwo(List<String> inputLines) {
+        
+        int horizontalPosition = 0;
+        int depth = 0;
+        int aim = 0;
+        
+        for (String line : inputLines) {
+            
+            String[] values = line.split(" ");
+            
+            Direction direction = switch (values[0]) {
+                case "forward" -> Direction.FORWARD;
+                case "down" -> Direction.DOWN;
+                case "up" -> Direction.UP;
+                default -> throw new IllegalStateException("Unexpected value: " + values[0]);
+            };
+            
+            int units = Integer.parseInt(values[1]);
+            
+            switch (direction) {
+                case FORWARD -> {
+                    horizontalPosition += units;
+                    depth += aim * units;
+                }
+                case DOWN -> aim += units;
+                case UP -> aim -= units;
+            }
+        }
+        
+        int product = horizontalPosition * depth;
+        
+        System.out.println("The product of the final horizontal position and the final depth is: " + product);
+    }
+    
+    
     private enum Direction {
-        DOWN, FORWARD, UP;
+        DOWN, FORWARD, UP
     }
 }
