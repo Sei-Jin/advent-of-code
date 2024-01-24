f2f8694e04941c706f411ec96e81203019a47831
SharkFinStew <kajiquiet@gmail.com>
2024-01-24 18:48:06 -0400

    2022-D1: Reformat

--- src/year2022/Day01.java
+++ src/year2022/Day01.java
@@ -10,10 +10,10 @@ import java.util.List;
 /**
  * --- Day 1: Calorie Counting ---
  */
-public class Day01 {
-    
-    public static void main(String[] args) throws IOException {
-        
+public class Day01
+{
+    public static void main(String[] args) throws IOException
+    {
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
         
         partOne(inputLines);
@@ -21,22 +21,24 @@ public class Day01 {
     }
     
     
-    private static void partOne(List<String> inputLines) {
-        
+    private static void partOne(List<String> inputLines)
+    {
         List<Integer> elfCalories = getElfCalories(inputLines);
         
         Collections.sort(elfCalories);
+        
         int mostCalories = elfCalories.getLast();
         
         System.out.println("The largest amount of calories carried by an elf is: " + mostCalories);
     }
     
     
-    private static void partTwo(List<String> inputLines) {
-        
+    private static void partTwo(List<String> inputLines)
+    {
         List<Integer> elfCalories = getElfCalories(inputLines);
         
         Collections.sort(elfCalories);
+        
         int topThreeCalorieTotal = elfCalories.subList(elfCalories.size() - 3, elfCalories.size())
                 .stream()
                 .mapToInt(Integer::intValue)
@@ -47,26 +49,29 @@ public class Day01 {
     }
     
     
-    private static List<Integer> getElfCalories(List<String> inputLines) {
-        
+    private static List<Integer> getElfCalories(List<String> inputLines)
+    {
         List<Integer> elfCalories = new ArrayList<>();
         
         int totalCalories = 0;
         
-        for (int lineNumber = 0; lineNumber < inputLines.size(); lineNumber++) {
-            
+        for (int lineNumber = 0; lineNumber < inputLines.size(); lineNumber++)
+        {
             String line = inputLines.get(lineNumber);
             
-            if (line.isEmpty()) {
+            if (line.isEmpty())
+            {
                 elfCalories.add(totalCalories);
                 totalCalories = 0;
             }
-            else if (lineNumber == inputLines.size() - 1) {
+            else if (lineNumber == inputLines.size() - 1)
+            {
                 int calories = Integer.parseInt(line);
                 totalCalories += calories;
                 elfCalories.add(totalCalories);
             }
-            else {
+            else
+            {
                 int calories = Integer.parseInt(line);
                 totalCalories += calories;
             }
