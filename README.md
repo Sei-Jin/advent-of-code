2e011373179e502c7b9003a25443bb0b230d674e
SharkFinStew <kajiquiet@gmail.com>
2024-01-27 15:50:38 -0400

    Update README files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Advent-of-Code
+# Advent of Code
+
+| Year | Events                                               | Solutions                        | Stars     |
+|:-----|:-----------------------------------------------------|:---------------------------------|:----------|
+| 2023 | [Advent of Code 2023](https://adventofcode.com/2015) | [:gift:](src/year2023/README.md) | 4 :star2: |
---
+++ src/year2022/Day02.java
@@ -0,0 +1,26 @@
+package year2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day02
+{
+    public static void main(String[] args) throws IOException
+    {
+        List<String> inputLines = Files.readAllLines(Path.of("sample.txt"));
+        
+        partOne(inputLines);
+    }
+    
+    
+    private static void partOne(List<String> inputLines)
+    {
+        
+        for (String line : inputLines)
+        {
+            List<Character> A
+        }
+    }
+}
