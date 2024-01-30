250409dc161761229f8b653a0341c8b17d51fa47
SharkFinStew <kajiquiet@gmail.com>
2024-01-29 21:32:49 -0400

    2022-D2-Test: Refactor

--- src/test/year2022/Day02Test.java
+++ src/test/year2022/Day02Test.java
@@ -1,6 +1,5 @@
 package test.year2022;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,124 +7,32 @@ import static year2022.Day02.*;
 
 public class Day02Test
 {
-    @Nested
-    class DetermineOutcomeTest
+    @Test
+    void DetermineOutcomeTest()
     {
-        @Test
-        void pickRockWin()
-        {
-            assertEquals(determineOutcome(Choice.SCISSORS, Choice.ROCK), Outcome.WIN);
-        }
-        
-        @Test
-        void pickPaperWin()
-        {
-            assertEquals(determineOutcome(Choice.ROCK, Choice.PAPER), Outcome.WIN);
-        }
-        
-        @Test
-        void pickScissorsWin()
-        {
-            assertEquals(determineOutcome(Choice.PAPER, Choice.SCISSORS), Outcome.WIN);
-        }
-        
-        
-        @Test
-        void pickRockDraw()
-        {
-            assertEquals(determineOutcome(Choice.ROCK, Choice.ROCK), Outcome.DRAW);
-        }
-        
-        @Test
-        void pickPaperDraw()
-        {
-            assertEquals(determineOutcome(Choice.PAPER, Choice.PAPER), Outcome.DRAW);
-        }
-        
-        @Test
-        void pickScissorsDraw()
-        {
-            assertEquals(determineOutcome(Choice.SCISSORS, Choice.SCISSORS), Outcome.DRAW);
-        }
-        
-        
-        @Test
-        void pickRockLose()
-        {
-            assertEquals(determineOutcome(Choice.PAPER, Choice.ROCK), Outcome.LOSS);
-        }
-        
-        @Test
-        void pickPaperLose()
-        {
-            assertEquals(determineOutcome(Choice.SCISSORS, Choice.PAPER), Outcome.LOSS);
-        }
-        
-        @Test
-        void pickScissorsLose()
-        {
-            assertEquals(determineOutcome(Choice.ROCK, Choice.SCISSORS), Outcome.LOSS);
-        }
+        assertEquals(Outcome.WIN, determineOutcome(Choice.SCISSORS, Choice.ROCK));
+        assertEquals(Outcome.WIN, determineOutcome(Choice.ROCK, Choice.PAPER));
+        assertEquals(Outcome.WIN, determineOutcome(Choice.PAPER, Choice.SCISSORS));
+        assertEquals(Outcome.DRAW, determineOutcome(Choice.ROCK, Choice.ROCK));
+        assertEquals(Outcome.DRAW, determineOutcome(Choice.PAPER, Choice.PAPER));
+        assertEquals(Outcome.DRAW, determineOutcome(Choice.SCISSORS, Choice.SCISSORS));
+        assertEquals(Outcome.LOSS, determineOutcome(Choice.PAPER, Choice.ROCK));
+        assertEquals(Outcome.LOSS, determineOutcome(Choice.SCISSORS, Choice.PAPER));
+        assertEquals(Outcome.LOSS, determineOutcome(Choice.ROCK, Choice.SCISSORS));
     }
     
     
-    @Nested
-    class DetermineChoiceTest
+    @Test
+    void DetermineChoiceTest()
     {
-        @Test
-        void winsAgainstRock()
-        {
-            assertEquals(determineChoice(Choice.ROCK, Outcome.WIN), Choice.PAPER);
-        }
-        
-        @Test
-        void winsAgainstPaper()
-        {
-            assertEquals(determineChoice(Choice.PAPER, Outcome.WIN), Choice.SCISSORS);
-        }
-        
-        @Test
-        void winsAgainstScissors()
-        {
-            assertEquals(determineChoice(Choice.SCISSORS, Outcome.WIN), Choice.ROCK);
-        }
-        
-        
-        @Test
-        void drawsAgainstRock()
-        {
-            assertEquals(determineChoice(Choice.ROCK, Outcome.DRAW), Choice.ROCK);
-        }
-        
-        @Test
-        void drawsAgainstPaper()
-        {
-            assertEquals(determineChoice(Choice.PAPER, Outcome.DRAW), Choice.PAPER);
-        }
-        
-        @Test
-        void drawsAgainstScissors()
-        {
-            assertEquals(determineChoice(Choice.SCISSORS, Outcome.DRAW), Choice.SCISSORS);
-        }
-        
-        
-        @Test
-        void losesAgainstRock()
-        {
-            assertEquals(determineChoice(Choice.ROCK, Outcome.LOSS), Choice.SCISSORS);
-        }
-        
-        @Test
-        void losesAgainstPaper()
-        {
-            assertEquals(determineChoice(Choice.PAPER, Outcome.LOSS), Choice.ROCK);
-        }
-        
-        @Test
-        void losesAgainstScissors()
-        {
-            assertEquals(determineChoice(Choice.SCISSORS, Outcome.LOSS), Choice.PAPER);
-        }
+        assertEquals(Choice.PAPER, determineChoice(Choice.ROCK, Outcome.WIN));
+        assertEquals(Choice.SCISSORS, determineChoice(Choice.PAPER, Outcome.WIN));
+        assertEquals(Choice.ROCK, determineChoice(Choice.SCISSORS, Outcome.WIN));
+        assertEquals(Choice.ROCK, determineChoice(Choice.ROCK, Outcome.DRAW));
+        assertEquals(Choice.PAPER, determineChoice(Choice.PAPER, Outcome.DRAW));
+        assertEquals(Choice.SCISSORS, determineChoice(Choice.SCISSORS, Outcome.DRAW));
+        assertEquals(Choice.SCISSORS, determineChoice(Choice.ROCK, Outcome.LOSS));
+        assertEquals(Choice.ROCK, determineChoice(Choice.PAPER, Outcome.LOSS));
+        assertEquals(Choice.PAPER, determineChoice(Choice.SCISSORS, Outcome.LOSS));
     }
 }
