5c58a602862c9f5fd7b8d524d14b70530312e4db
Sei-Jin <kajiquiet@gmail.com>
2024-01-31 16:37:10 -0400

    2016-D3: Solved part 2

--- src/year2016/Day03.java
+++ src/year2016/Day03.java
@@ -3,8 +3,7 @@ package year2016;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Day03
 {
@@ -13,6 +12,7 @@ public class Day03
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
 
         partOne(inputLines);
+        partTwo(inputLines);
     }
 
 
@@ -27,12 +27,65 @@ public class Day03
                     .sorted()
                     .toList();
 
-            if (sideLengths.get(0) + sideLengths.get(1) > sideLengths.get(2))
+            if (possibleTriangle(sideLengths))
             {
                 possibleTriangles++;
             }
         }
 
         System.out.println("The number of possible triangles is: " + possibleTriangles);
+    }
+
+
+    private static void partTwo(List<String> inputLines)
+    {
+        int possibleTriangles = 0;
+
+        List<List<Integer>> storedRows = new ArrayList<>();
+
+        int rowCount = 0;
+
+        for (String line : inputLines)
+        {
+            List<Integer> rowValues = Arrays.stream(line.trim().split("\\s+"))
+                    .map(Integer::parseInt)
+                    .toList();
+
+            storedRows.add(rowValues);
+
+            rowCount++;
+
+            if (rowCount % 3 == 0)
+            {
+                List<Integer> sideLengths = new ArrayList<>();
+
+                for (int index = 0; index < storedRows.size(); index++)
+                {
+                    for (List<Integer> rowVals : storedRows)
+                    {
+                        sideLengths.add(rowVals.get(index));
+                    }
+
+                    Collections.sort(sideLengths);
+
+                    if (possibleTriangle(sideLengths))
+                    {
+                        possibleTriangles++;
+                    }
+
+                    sideLengths.clear();
+                }
+
+                storedRows.clear();
+            }
+        }
+
+        System.out.println("The number of possible triangles is: " + possibleTriangles);
+    }
+
+
+    private static boolean possibleTriangle(List<Integer> sideLengths)
+    {
+        return sideLengths.get(0) + sideLengths.get(1) > sideLengths.get(2);
     }
 }
