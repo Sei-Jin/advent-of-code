6b611169cb1312d8baa58fd62ca9aa0f310c4a29
SharkFinStew <kajiquiet@gmail.com>
2024-02-01 18:32:38 -0400

    2020-D2: Solved part 2

--- src/year2020/Day02.java
+++ src/year2020/Day02.java
@@ -16,6 +16,7 @@ public class Day02
         List<String> inputLines = Files.readAllLines(Path.of("input.txt"));
         
         partOne(inputLines);
+        partTwo(inputLines);
     }
     
     
@@ -25,8 +26,7 @@ public class Day02
         
         for (String line : inputLines)
         {
-            List<String> lineValues = Arrays.stream(line.split("(-| |: )"))
-                    .toList();
+            List<String> lineValues = getLineValues(line);
             
             int minimumCount = Integer.parseInt(lineValues.get(0));
             int maximumCount = Integer.parseInt(lineValues.get(1));
@@ -49,6 +49,57 @@ public class Day02
             }
         }
         
-        System.out.println("The number of valid passwords according to the given policies is: " + validPasswords);
+        System.out.println("The number of valid passwords according to the policies is: " + validPasswords);
+    }
+    
+    
+    private static void partTwo(List<String> inputLines)
+    {
+        int validPasswords = 0;
+        
+        for (String line : inputLines)
+        {
+            List<String> lineValues = getLineValues(line);
+            
+            boolean oneLetterPresent = isOneLetterPresent(lineValues);
+            
+            if (oneLetterPresent)
+            {
+                validPasswords++;
+            }
+        }
+        
+        System.out.println("The number of valid passwords according to the new interpretation of the " +
+                "policies is: " + validPasswords);
+    }
+    
+    
+    private static List<String> getLineValues(String line)
+    {
+        return Arrays.stream(line.split("(-| |: )"))
+                .toList();
+    }
+    
+    
+    private static boolean isOneLetterPresent(List<String> lineValues)
+    {
+        int firstPosition = Integer.parseInt(lineValues.get(0));
+        int secondPosition = Integer.parseInt(lineValues.get(1));
+        char policyLetter = lineValues.get(2).charAt(0);
+        String password = lineValues.get(3);
+        
+        boolean oneLetterPresent = false;
+        
+        if (policyLetter == password.charAt(firstPosition - 1))
+        {
+            oneLetterPresent = !oneLetterPresent;
+        }
+        
+        if (policyLetter == password.charAt(secondPosition - 1))
+        {
+            oneLetterPresent = !oneLetterPresent;
+        }
+        
+        return oneLetterPresent;
     }
 }
