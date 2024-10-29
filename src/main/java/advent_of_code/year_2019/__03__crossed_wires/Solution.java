a484e52327ffa0d6ff92cea6ce34822832d67eb8
Sei-Jin <kajiquiet@gmail.com>
2024-10-29 11:09:06 -0300

    2019-3 - Refactor solution and solve part two

--- src/main/java/advent_of_code/year_2019/__03__crossed_wires/Solution.java
+++ src/main/java/advent_of_code/year_2019/__03__crossed_wires/Solution.java
@@ -7,80 +7,132 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+/**
+ * <p>--- Day 3: Crossed Wires ---
+ *
+ * <p>Puzzle Input - Two sets of instructions, one for each wire. Each set of instructions is a {@code String}
+ * of comma-separated values, and each instruction contains the direction and distance the wire should follow.
+ * <ul>
+ *     <li>{@link Solution#partOne(List)} - Calculates the shortest Manhattan distance between an intersection
+ *     of the two wires and their starting location.</li>
+ *     <li>{@link Solution#partTwo(List)} - Calculates the shortest combined distance of the two wires from their
+ *     starting location to an intersection point.</li>
+ * </ul>
+ */
 public class Solution implements PuzzleSolver
 {
     /**
+     * The location of the central port is (0, 0). The central port is the starting location of the wires,
+     * and it does not count if the wires cross at the central port location, since they cross there by default.
+     */
+    private static final Point STARTING_LOCATION = new Point(0, 0);
+    
+    
+    /**
+     * Calculates the shortest Manhattan distance between an intersection of the two wires and their starting location.
+     *
      * @param inputLines the puzzle input.
-     * @return the Manhattan distance from the central port to the closest intersection.
+     * @return the Manhattan distance from the central port to the closest intersection of the two wires.
      */
     @Override
     public Object partOne(List<String> inputLines)
     {
-        List<Move> firstMoveList = getMoves(inputLines.getFirst());
-        List<Move> secondMoveList = getMoves(inputLines.getLast());
+        List<Instruction> firstWireInstructions = parseInstructionList(inputLines.getFirst());
+        List<Instruction> secondWireInstructions = parseInstructionList(inputLines.getLast());
         
-        Set<Point> firstWirePoints = determineWirePoints(firstMoveList);
-        Set<Point> secondWirePoints = determineWirePoints(secondMoveList);
+        HashMap<Point, Integer> firstWirePoints = createWirePointMapping(firstWireInstructions);
+        HashMap<Point, Integer> secondWirePoints = createWirePointMapping(secondWireInstructions);
         
-        Set<Point> crossingPoints = new HashSet<>(firstWirePoints);
-        crossingPoints.retainAll(secondWirePoints);
+        HashSet<Point> crossingPoints = new HashSet<>(firstWirePoints.keySet());
+        crossingPoints.retainAll(secondWirePoints.keySet());
         
-        return getClosestDistance(crossingPoints);
+        return calculateClosestDistance(crossingPoints);
     }
     
     
-    private record Move(Character direction, int distance) {}
+    /**
+     * The Move record class stores the information for a move in the instruction set.
+     *
+     * @param direction the direction to be taken. Can be {@code U}, {@code D}, {@code L}, {@code R}.
+     * @param distance the distance to be travelled.
+     */
+    private record Instruction(Character direction, int distance) {}
     
     
-    private static List<Move> getMoves(String inputLine)
+    /**
+     * Parses a line from the puzzle input and returns a {@code List} of {@code Instruction}.
+     *
+     * @param inputLine a line from the puzzle input.
+     * @return a {@code List} of {@code Instruction} parsed from a line of the puzzle input.
+     */
+    private static List<Instruction> parseInstructionList(String inputLine)
     {
-        List<Move> moveList = new ArrayList<>();
+        List<Instruction> instructionList = new ArrayList<>();
         
-        List<String> moves = List.of(inputLine.split(","));
+        List<String> instructions = List.of(inputLine.split(","));
         
-        for (String move : moves)
+        for (String instruction : instructions)
         {
-            Character direction = move.charAt(0);
-            int moveDistance = Integer.parseInt(move.substring(1));
+            Character direction = instruction.charAt(0);
+            int moveDistance = Integer.parseInt(instruction.substring(1));
             
-            moveList.add(new Move(direction, moveDistance));
+            instructionList.add(new Instruction(direction, moveDistance));
         }
         
-        return moveList;
+        return instructionList;
     }
     
     
-    private static HashSet<Point> determineWirePoints(List<Move> moves)
+    /**
+     * Creates a mapping between the unique points crossed by a wire and the total length of the wire when it first
+     * reached each point.
+     *
+     * @param instructions a {@code List} of moves that the wire should follow.
+     * @return a {@code HashMap} the unique points crossed by a wire and the total length of the wire when it first
+     * reached each point.
+     */
+    private static HashMap<Point, Integer> createWirePointMapping(List<Instruction> instructions)
     {
-        HashSet<Point> pointsVisited = new HashSet<>();
+        HashMap<Point, Integer> pointsVisited = new HashMap<>();
         
-        int yPosition = 0;
-        int xPosition = 0;
-        
-        Point initialPoint = new Point(0, 0);
-        pointsVisited.add(initialPoint);
+        int xPosition = STARTING_LOCATION.x;
+        int yPosition = STARTING_LOCATION.y;
+        int totalSteps = 0;
 
-        for (Move move : moves)
+        for (Instruction instruction : instructions)
         {
-            for (int distanceTravelled = 0; distanceTravelled < move.distance; distanceTravelled++)
+            for (int steps = 0; steps < instruction.distance; steps++)
             {
-                switch (move.direction)
+                switch (instruction.direction)
                 {
                     case 'U' -> yPosition++;
                     case 'D' -> yPosition--;
                     case 'L' -> xPosition--;
                     case 'R' -> xPosition++;
                 }
-
-                pointsVisited.add(new Point(xPosition, yPosition));
+                totalSteps++;
+                
+                Point pointVisited = new Point(xPosition, yPosition);
+                
+                if (!pointsVisited.containsKey(pointVisited))
+                {
+                    pointsVisited.put(pointVisited, totalSteps);
+                }
             }
         }
         
         return pointsVisited;
     }
-
     
-    private static int getClosestDistance(Set<Point> crossingPoints)
+    
+    /**
+     * Calculates the shortest Manhattan distance from an intersection between the two wires and their
+     * starting location.
+     *
+     * @param crossingPoints a {@code HashSet} of the points where the two wires cross.
+     * @return the Manhattan distance from the central port to the closest intersection of the two wires.
+     */
+    private static int calculateClosestDistance(HashSet<Point> crossingPoints)
     {
         int closestDistance = 0;
         
@@ -107,9 +159,66 @@ public class Solution implements PuzzleSolver
     }
     
     
+    /**
+     * Calculates the shortest combined distance of the two wires from their starting location to an intersection point.
+     *
+     * @param inputLines the puzzle input.
+     * @return the shortest combined distance of the two wires it takes to reach an intersection between them.
+     */
     @Override
     public Object partTwo(List<String> inputLines)
     {
-        return null;
+        List<Instruction> firstWireInstructions = parseInstructionList(inputLines.getFirst());
+        List<Instruction> secondWireInstructions = parseInstructionList(inputLines.getLast());
+        
+        HashMap<Point, Integer> firstWirePoints = createWirePointMapping(firstWireInstructions);
+        HashMap<Point, Integer> secondWirePoints = createWirePointMapping(secondWireInstructions);
+        
+        HashSet<Point> crossingPoints = new HashSet<>(firstWirePoints.keySet());
+        crossingPoints.retainAll(secondWirePoints.keySet());
+        
+        return calculateShortestCombinedDistance(crossingPoints, firstWirePoints, secondWirePoints);
+    }
+    
+    
+    /**
+     * Calculates the shortest combined distance of the two wires from their starting location to an intersection point.
+     *
+     * @param crossingPoints a {@code HashSet} of the points where the two wires cross.
+     * @param firstWirePoints a {@code HashMap} that maps the points where the first wire crosses to
+     *                       the shortest length of the wire at the point of crossing.
+     * @param secondWirePoints a {@code HashMap} that maps the points where the second wire crosses to
+     *                       the shortest length of the wire at the point of crossing.
+     * @return the shortest combined distance of the two wires from their starting location to an intersection point.
+     */
+    private int calculateShortestCombinedDistance
+    (
+            HashSet<Point> crossingPoints,
+            HashMap<Point, Integer> firstWirePoints,
+            HashMap<Point, Integer> secondWirePoints
+    )
+    {
+        int fewestCombinedSteps = 0;
+        
+        for (Point crossingPoint : crossingPoints)
+        {
+            int combinedSteps = firstWirePoints.get(crossingPoint) + secondWirePoints.get(crossingPoint);
+            
+            if (combinedSteps == 0)
+            {
+                continue;
+            }
+            
+            if (fewestCombinedSteps == 0)
+            {
+                fewestCombinedSteps = combinedSteps;
+            }
+            else
+            {
+                fewestCombinedSteps = Math.min(fewestCombinedSteps, combinedSteps);
+            }
+        }
+        
+        return fewestCombinedSteps;
     }
 }
