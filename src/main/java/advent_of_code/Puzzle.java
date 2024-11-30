8b264a6be1db1672146cad17a45beb02947a6463
Sei-Jin <kajiquiet@gmail.com>
2024-11-30 12:43:35 -0400

    Improve the method of determining which solution to run.
    Instead of listing all the runnable solutions, which is tedious and error-prone, the method now determines which solution to run based on the solution's classpath.
    
    The downside to this approach is that the current classpath for solutions does not support this approach without extra overhead, and the packages for each day will need to be renamed.

--- src/main/java/advent_of_code/Puzzle.java
+++ src/main/java/advent_of_code/Puzzle.java
@@ -1,15 +1,17 @@
 package advent_of_code;
 
-import advent_of_code.event.year_2017.__05__a_maze_of_twisty_trampolines_all_alike.Solution;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
 
 public class Puzzle
 {
     private final int day;
     private final int year;
-    private static final Map<Integer, Map<Integer, PuzzleSolver>> puzzleSolverMap = new HashMap<>();
+    
+    public Puzzle(int year, int day)
+    {
+        this.year = year;
+        this.day = day;
+    }
     
     public int getDay()
     {
@@ -28,7 +30,7 @@ public class Puzzle
     /// @return the padded day value.
     public String getDayWithPadding()
     {
-        boolean singleDigitDay = (day / 10 == 0);
+        boolean singleDigitDay = (day < 10);
         
         if (singleDigitDay)
         {
@@ -40,64 +42,33 @@ public class Puzzle
         }
     }
     
-    public Puzzle(int year, int day)
-    {
-        this.year = year;
-        this.day = day;
-    }
-    
+    /// Determines the solution class for the puzzle.
+    ///
+    /// This method uses the Reflection API to dynamically determine what solution implementation matches the puzzle.
+    ///
+    /// @return the solution class for the puzzle.
     public PuzzleSolver getPuzzleSolver()
     {
-        Map<Integer, PuzzleSolver> yearMap = puzzleSolverMap.get(this.year);
-        if (yearMap == null)
+        String classPath = String.format("advent_of_code.event.year_%d.day_%s.Solution", year, getDayWithPadding());
+        
+        Class<?> solutionClass;
+        
+        try
         {
-            throw new IllegalArgumentException("No puzzles found for year: " + this.year);
+            solutionClass = Class.forName(classPath);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new RuntimeException(e);
         }
         
-        PuzzleSolver solver = yearMap.get(this.day);
-        if (solver == null)
+        try
         {
-            throw new IllegalArgumentException("No puzzle found for day: " + this.day + " in year: " + this.year);
+            return (PuzzleSolver) solutionClass.getConstructor().newInstance();
         }
-        
-        return solver;
-    }
-    
-    static
-    {
-        Map<Integer, PuzzleSolver> year2015 = new HashMap<>();
-        year2015.put(1, new advent_of_code.event.year_2015.__01__not_quite_lisp.Solution());
-        year2015.put(2, new advent_of_code.event.year_2015.__02__i_was_told_there_would_be_no_math.Solution());
-        year2015.put(3, new advent_of_code.event.year_2015.__03__perfectly_spherical_houses_in_a_vacuum.Solution());
-        year2015.put(5, new advent_of_code.event.year_2015.__05__doesnt_he_have_intern_elves_for_this.Solution());
-        year2015.put(6, new advent_of_code.event.year_2015.__06__probably_a_fire_hazard.Solution());
-        year2015.put(7, new advent_of_code.event.year_2015.__07__some_assembly_required.Solution());
-        
-        puzzleSolverMap.put(2015, year2015);
-        
-        
-        Map<Integer, PuzzleSolver> year2016 = new HashMap<>();
-        year2016.put(1, new advent_of_code.event.year_2016.__01__no_time_for_a_taxicab.Solution());
-        year2016.put(2, new advent_of_code.event.year_2016.__02__bathroom_security.Solution());
-        year2016.put(3, new advent_of_code.event.year_2016.__03__squares_with_three_sides.Solution());
-        
-        puzzleSolverMap.put(2016, year2016);
-        
-        
-        Map<Integer, PuzzleSolver> year2017 = new HashMap<>();
-        year2017.put(1, new advent_of_code.event.year_2017.__01__inverse_captcha.Solution());
-        year2017.put(2, new advent_of_code.event.year_2017.__02__corruption_checksum.Solution());
-        year2017.put(4, new advent_of_code.event.year_2017.__04__high_entropy_passphrases.Solution());
-        year2017.put(5, new Solution());
-        
-        puzzleSolverMap.put(2017, year2017);
-        
-        Map<Integer, PuzzleSolver> year2019 = new HashMap<>();
-        year2019.put(1, new advent_of_code.event.year_2019.__01__the_tyranny_of_the_rocket_equation.Solution());
-        year2019.put(2, new advent_of_code.event.year_2019.__02__1202_program_alarm.Solution());
-        year2019.put(3, new advent_of_code.event.year_2019.__03__crossed_wires.Solution());
-        year2019.put(4, new advent_of_code.event.year_2019.__04__secure_container.Solution());
-        
-        puzzleSolverMap.put(2019, year2019);
+        catch (InstantiationException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
