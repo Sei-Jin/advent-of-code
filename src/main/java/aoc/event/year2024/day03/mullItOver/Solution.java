03e47911c9a505da867472c27f753fa52680f24e
Sei-Jin <kajiquiet@gmail.com>
2024-12-03 18:47:20 -0400

    2024-3: Complete problem

--- src/main/java/aoc/event/year2024/day03/mullItOver/Solution.java
+++ src/main/java/aoc/event/year2024/day03/mullItOver/Solution.java
@@ -9,45 +9,70 @@ import java.util.regex.Pattern;
 
 public class Solution implements PuzzleSolver
 {
-    /// Determines the sum of all multiplication instructions in the program memory.
+    /// Calculates the sum of all multiplication instructions in the program memory.
     ///
-    /// The puzzle input represents a program memory that was corrupted, and contains a series of instructions
-    /// to follow.
+    /// The puzzle input represents a program memory that was corrupted, and contains a series of multiplication
+    /// instructions to follow. A multiplication instruction is represented as mul(X,Y) and multiplies the
+    /// two numbers X and Y.
     ///
     /// @param programMemory the puzzle input.
     /// @return the total sum of all multiplication instructions.
     @Override
     public Object partOne(List<String> programMemory)
     {
-        int totalProduct = 0;
+        int sumOfProducts = 0;
         
         for (String programSection : programMemory)
         {
-            List<String> instructions = getValidInstructions(programSection);
+            Matcher patternMatcher = getPatternMatcher(programSection, true);
+            List<String> instructions = getValidInstructions(patternMatcher);
             
             for (String instruction : instructions)
             {
-                totalProduct += getProduct(instruction);
+                sumOfProducts += getProduct(instruction);
             }
         }
         
-        return totalProduct;
+        return sumOfProducts;
+    }
+    
+    /// Returns a pattern matcher for the given section of the program.
+    ///
+    /// The pattern that is matched depends on if all multiplications are enabled or not. If not all
+    /// multiplications are enabled then `do` and `don't` instructions should also be matched.
+    ///
+    /// @param programSection a section of the program.
+    /// @param allMultiplicationsEnabled true if all multiplication instructions are enabled, false if otherwise.
+    /// @return a pattern matcher for the section of the program.
+    private static Matcher getPatternMatcher(String programSection, boolean allMultiplicationsEnabled)
+    {
+        StringBuilder regex = new StringBuilder(Instruction.MULTIPLY.getRegex());
+        
+        if (!allMultiplicationsEnabled)
+        {
+            regex.append("|");
+            regex.append(Instruction.DO_NOT.getRegex());
+            regex.append("|");
+            regex.append(Instruction.DO.getRegex());
+        }
+        
+        Pattern pattern = Pattern.compile(regex.toString());
+        return pattern.matcher(programSection);
     }
     
     /// Returns the list of valid instructions from a section of the program.
     ///
-    /// @param programSection a section of the program.
+    /// A valid instruction is any instruction that is matched by the pattern matcher.
+    ///
+    /// @param patternMatcher a matcher for valid instructions.
     /// @return the list of valid instructions.
-    private static List<String> getValidInstructions(String programSection)
+    private static List<String> getValidInstructions(Matcher patternMatcher)
     {
         List<String> instructions = new ArrayList<>();
         
-        Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)");
-        Matcher matcher = pattern.matcher(programSection);
-        
-        while (matcher.find())
+        while (patternMatcher.find())
         {
-            String instruction = matcher.group();
+            String instruction = patternMatcher.group();
             instructions.add(instruction);
         }
         
@@ -55,6 +80,8 @@ public class Solution implements PuzzleSolver
     }
     
     /// Calculates the product of a multiplication instruction.
+    ///
+    /// A multiplication instruction is in the form mul(X,Y) where X and Y are three-digit numbers.
     ///
     /// @param instruction a multiplication instruction.
     /// @return the product of the instruction.
@@ -74,9 +101,62 @@ public class Solution implements PuzzleSolver
         return product;
     }
     
+    /// Calculates the sum of all enabled multiplication instructions in the program memory.
+    ///
+    /// - The program starts with instructions enabled.
+    /// - A `don't` instruction disables the following instructions.
+    /// - A `do` instruction enables the following instructions.
+    ///
+    /// @param programMemory the puzzle input.
+    /// @return the total sum of all enabled multiplication instructions.
     @Override
-    public Object partTwo(List<String> inputLines)
+    public Object partTwo(List<String> programMemory)
     {
-        return null;
+        int sumOfProducts = 0;
+        boolean enableMultiplications = true;
+        
+        for (String programSection : programMemory)
+        {
+            Matcher patternMatcher = getPatternMatcher(programSection, false);
+            List<String> instructions = getValidInstructions(patternMatcher);
+            
+            for (String instruction : instructions)
+            {
+                if (instruction.matches(Instruction.DO_NOT.regex))
+                {
+                    enableMultiplications = false;
+                }
+                else if (instruction.matches(Instruction.DO.regex))
+                {
+                    enableMultiplications = true;
+                }
+                else if (enableMultiplications)
+                {
+                    sumOfProducts += getProduct(instruction);
+                }
+            }
+        }
+        
+        return sumOfProducts;
+    }
+    
+    /// This enum stores the regular expression for each type of instruction.
+    enum Instruction
+    {
+        MULTIPLY("mul\\(\\d+,\\d+\\)"),
+        DO("do\\(\\)"),
+        DO_NOT("don't\\(\\)");
+        
+        private final String regex;
+        
+        Instruction(String regex)
+        {
+            this.regex = regex;
+        }
+        
+        public String getRegex()
+        {
+            return regex;
+        }
     }
 }
--- src/test/java/aoc/event/year2024/day03/mullItOver/SolutionTests.java
+++ src/test/java/aoc/event/year2024/day03/mullItOver/SolutionTests.java
@@ -1,5 +1,6 @@
 package aoc.event.year2024.day03.mullItOver;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -8,10 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTests
 {
-    @Test
-    void example()
+    @Nested
+    class PartOne
     {
-        String memory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        assertEquals(161, new Solution().partOne(Collections.singletonList(memory)));
+        @Test
+        void example()
+        {
+            String programMemory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+            assertEquals(161, new Solution().partOne(Collections.singletonList(programMemory)));
+        }
+    }
+    
+    @Nested
+    class PartTwo
+    {
+        @Test
+        void example()
+        {
+            String programMemory = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+            assertEquals(48, new Solution().partTwo(Collections.singletonList(programMemory)));
+        }
     }
 }
