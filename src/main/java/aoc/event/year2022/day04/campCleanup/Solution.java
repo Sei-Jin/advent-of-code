77fd8f30ddba68ae21b8541d12a1a0e5539e7a69
Sei-Jin <kajiquiet@gmail.com>
2024-12-08 14:17:23 -0400

    2022-4: Complete part one

--- src/main/java/aoc/event/year2022/day04/campCleanup/Solution.java
+++ src/main/java/aoc/event/year2022/day04/campCleanup/Solution.java
@@ -2,6 +2,8 @@ package aoc.event.year2022.day04.campCleanup;
 
 import aoc.PuzzleSolver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution implements PuzzleSolver
@@ -9,7 +11,57 @@ public class Solution implements PuzzleSolver
     @Override
     public Object partOne(List<String> inputLines)
     {
-        return null;
+        int fullyContainedPairs = 0;
+        
+        for (String line : inputLines)
+        {
+            List<Pair> pairs = getPairs(line);
+            boolean fullyContainedPair = isFullyContainedPair(pairs);
+            
+            if (fullyContainedPair)
+            {
+                fullyContainedPairs++;
+            }
+        }
+        
+        return fullyContainedPairs;
+    }
+    
+    record Pair(int startingSection, int endingSection) {}
+    
+    private static List<Pair> getPairs(String line)
+    {
+        List<Integer> inputNumbers = Arrays.stream(line.split("[-,]"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+        
+        List<Pair> pairs = new ArrayList<>();
+        
+        for (int index = 0; index < inputNumbers.size(); index += 2)
+        {
+            pairs.add(new Pair(inputNumbers.get(index), inputNumbers.get(index + 1)));
+        }
+        
+        return pairs;
+    }
+    
+    private static boolean isFullyContainedPair(List<Pair> pairs)
+    {
+        Pair firstPair = pairs.getFirst();
+        Pair secondPair = pairs.getLast();
+        
+        boolean firstContainsSecond = (
+                firstPair.startingSection <= secondPair.startingSection &&
+                firstPair.endingSection >= secondPair.endingSection
+        );
+        
+        boolean secondContainsFirst = (
+                secondPair.startingSection <= firstPair.startingSection &&
+                secondPair.endingSection >= firstPair.endingSection
+        );
+        
+        return (firstContainsSecond || secondContainsFirst);
     }
     
     @Override
--- src/test/java/aoc/event/year2022/day04/campCleanup/SolutionTests.java
+++ src/test/java/aoc/event/year2022/day04/campCleanup/SolutionTests.java
@@ -1,6 +1,28 @@
 package aoc.event.year2022.day04.campCleanup;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SolutionTests
 {
-
+    @Test
+    void example()
+    {
+        List<String> input = new ArrayList<>(
+                Arrays.stream("""
+                2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8
+                """.split("\n")).toList()
+        );
+        assertEquals(2, new Solution().partOne(input));
+    }
 }
