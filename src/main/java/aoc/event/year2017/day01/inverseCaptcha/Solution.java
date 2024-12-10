27b0b68ebfc487d360039321a3590bf4390291ae
Sei-Jin <kajiquiet@gmail.com>
2024-12-10 13:52:51 -0400

    2017-1: Refactor solution

--- src/main/java/aoc/event/year2017/day01/inverseCaptcha/Solution.java
+++ src/main/java/aoc/event/year2017/day01/inverseCaptcha/Solution.java
@@ -4,62 +4,52 @@ import aoc.PuzzleSolver;
 
 import java.util.List;
 
-/**
- * <p>--- Day 1: Inverse Captcha ---
- *
- * <p>The goal is to solve a captcha. To solve a captcha implement the following rules:
- * <ul>
- *     <li>{@link Solution#partOne}: Calculate the sum of all the digits in the input that have a matching digit
- *     immediately after.</li>
- *     <li>{@link Solution#partTwo}: Calculate the sum of all the digits in the input that have a matching digit
- *     halfway around the list.</li>
- * </ul>
- */
 public class Solution implements PuzzleSolver
 {
-    /**
-     * Calculates the sum of all the digits in the input that have a matching digit immediately after.
-     * The list is "circular", so the digit after the last digit is the first digit.
-     *
-     * @param inputLines the puzzle input.
-     * @return the solution to the captcha.
-     */
+    /// Calculates the sum of all digits that match the next digit in the list.
+    ///
+    /// The list is "circular", so the digit after the last digit is the first digit.
+    ///
+    /// - Time Complexity: O(n)
+    ///     - One comparison is done for each of the digits in the input array.
+    ///
+    /// - Space Complexity: O(n)
+    ///     - The puzzle input is converted and stored as a list of digits.
+    ///
+    /// @param inputLines the puzzle input.
+    /// @return the solution to the captcha.
     @Override
     public Object partOne(List<String> inputLines)
     {
         List<Integer> digits = getDigits(inputLines);
-        
         int repetitionOffset = 1;
         
         return sumRepeatingDigits(digits, repetitionOffset);
     }
     
-    
-    /**
-     * Converts the first {@code String} in the puzzle input to a {@code List} of digits. A digit is an
-     * {@code Integer} that ranges from {@code 0–9}.
-     *
-     * @param inputLines the puzzle input.
-     * @return the first line of the puzzle input as a {@code List} of digits.
-     */
+    /// Converts the puzzle input to a `List` of digits.
+    ///
+    /// The puzzle input is in the form `#####...`, where each `#` is a digit.
+    /// A digit is any `Integer` in the range `0–9`.
+    ///
+    /// @param inputLines the puzzle input.
+    /// @return the puzzle input as a list of digits.
     private static List<Integer> getDigits(List<String> inputLines)
     {
         String inputLine = inputLines.getFirst();
         
-        return inputLine.chars()
+        return inputLine
+                .chars()
                 .map(Character::getNumericValue)
                 .boxed()
                 .toList();
     }
     
-    
-    /**
-     * Calculates the sum of the digits that repeat at a given offset value, {@code repetitionOffset}.
-     *
-     * @param digits a {@code List} of digits ({@code Integers} that range from {@code 0–9}).
-     * @param repetitionOffset the offset between the indices of the digits that should be compared.
-     * @return the sum of the digits that have matching pairs at their offset index.
-     */
+    /// Calculates the sum of the digits that repeat at a given offset value, `repetitionOffset`.
+    ///
+    /// @param digits the puzzle input as a list of digits.
+    /// @param repetitionOffset the offset between the indices of the digits that are compared.
+    /// @return the sum of the digits that have matching pairs at their offset index.
     private static int sumRepeatingDigits(List<Integer> digits, int repetitionOffset)
     {
         int repeatingDigitsSum = 0;
@@ -75,19 +65,20 @@ public class Solution implements PuzzleSolver
         return repeatingDigitsSum;
     }
     
-    
-    /**
-     * Calculates the sum of all the digits in the input that have a matching digit halfway around the list.
-     * The list is "circular", so the digit after the last digit is the first digit.
-     *
-     * @param inputLines the puzzle input.
-     * @return the solution to the new captcha.
-     */
+    /// Calculates the sum of all digits that have a matching digit halfway around the list.
+    ///
+    /// - Time Complexity: O(n)
+    ///     - Same as part one.
+    ///
+    /// - Space Complexity: O(n)
+    ///     - Same as part one.
+    ///
+    /// @param inputLines the puzzle input.
+    /// @return the solution to the new captcha.
     @Override
     public Object partTwo(List<String> inputLines)
     {
         List<Integer> digits = getDigits(inputLines);
-        
         int repetitionOffset = digits.size() / 2;
         
         return sumRepeatingDigits(digits, repetitionOffset);
