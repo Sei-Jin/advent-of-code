565b967ef7a399d9951326158ef5456fe7e7f441
Sei-Jin <kajiquiet@gmail.com>
2024-12-25 22:12:13 -0400

    [2023-4] Complete part two

--- src/main/java/aoc/event/year2023/day04/scratchcards/Solution.java
+++ src/main/java/aoc/event/year2023/day04/scratchcards/Solution.java
@@ -2,9 +2,7 @@ package aoc.event.year2023.day04.scratchcards;
 
 import aoc.PuzzleSolver;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,12 +14,17 @@ public class Solution implements PuzzleSolver
     ///
     /// - Recompiling every time a card is parsed.
     /// - Passing through method parameters multiple times.
-    ///
     private static final Pattern NUMBER_PATTERN = Pattern.compile("(\\d+)");
     
     /// Calculates the total points won by all the scratchcards.
     ///
-    /// @param inputLines the puzzle input.
+    /// Time Complexity: O(n)
+    /// - `n` is the number of characters in the puzzle input.
+    ///
+    /// Space Complexity: O(m)
+    /// - `m` is the number of numbers on the largest scratchcard.
+    ///
+    /// @param inputLines the puzzle input is a list of scratchcards.
     /// @return the total points won by all the scratchcards.
     @Override
     public Object partOne(List<String> inputLines)
@@ -31,21 +34,40 @@ public class Solution implements PuzzleSolver
         for (String line : inputLines)
         {
             Card card = parseCard(line);
-            
-            Set<Integer> actualWinningNumbers = new HashSet<>(card.winningNumbers());
-            actualWinningNumbers.retainAll(card.ourNumbers());
-            
-            int points = calculatePoints(actualWinningNumbers);
-            totalPoints += points;
+            totalPoints += calculatePoints(card.getMatchingNumbers().size());
         }
         
         return totalPoints;
     }
     
+    /// Calculates the total number of scratchcards.
+    ///
+    /// Time Complexity: `O(n)`
+    /// - `n` is the number of characters in the puzzle input.
+    ///
+    /// Space Complexity: `O(2k + m)`
+    /// - `k` is the number of scratchcards in the puzzle input, and `m` is the maximum number of
+    ///  matching numbers a scratchcard contains.
+    ///
+    /// @param inputLines the puzzle input is a list of scratchcards.
+    /// @return the total number of scratchcards.
     @Override
     public Object partTwo(List<String> inputLines)
     {
-        return null;
+        Map<Integer, Integer> cardCounts = new HashMap<>();
+        Set<Integer> cardIds = new HashSet<>();
+        
+        for (String line : inputLines)
+        {
+            Card card = parseCard(line);
+            cardIds.add(card.id);
+            
+            increaseCardCount(card.id, cardCounts, 1);
+            increaseNextCardCounts(card.id, cardCounts, card.getMatchingNumbers().size());
+        }
+        
+        cardCounts.keySet().retainAll(cardIds);
+        return sumCardCounts(cardCounts);
     }
     
     /// Parses a line of the puzzle input for the card data.
@@ -56,7 +78,8 @@ public class Solution implements PuzzleSolver
     ///
     /// Splitting the line by `:` and `|` splits the line into three sections:
     ///
-    /// - The first section contains the card number, where `#` represents the number of the card.
+    /// - The first section contains the card number, where `#` represents the number of the
+    /// card.
     /// - The second section contains a set of the winning numbers, where each `A` represents a
     /// winning number.
     /// - The third section contains a set of our numbers, where each `B` represents one of our
@@ -71,10 +94,37 @@ public class Solution implements PuzzleSolver
     {
         String[] sections = line.split("[:|]");
         
+        int id = parseId(sections[0]);
         Set<Integer> winningNumbers = parseNumbers(sections[1]);
         Set<Integer> potentialWinningNumbers = parseNumbers(sections[2]);
         
-        return new Card(winningNumbers, potentialWinningNumbers);
+        return new Card(id, winningNumbers, potentialWinningNumbers);
+    }
+    
+    /// Parses a section of the puzzle input for the card id number.
+    ///
+    /// The section should be in the form:
+    ///
+    /// `Card #`
+    ///
+    /// Where `#` represents a number.
+    ///
+    /// @param section a section of the puzzle input that contains the card id number.
+    /// @return the card id number.
+    private static int parseId(String section)
+    {
+        Matcher matcher = NUMBER_PATTERN.matcher(section);
+        
+        if (matcher.find())
+        {
+            return Integer.parseInt(matcher.group());
+        }
+        else
+        {
+            throw new IllegalArgumentException(
+                    "Invalid card id format encountered in input: " + section
+            );
+        }
     }
     
     /// Parses a set of numbers from the given section of the puzzle input.
@@ -83,11 +133,11 @@ public class Solution implements PuzzleSolver
     ///
     /// ` A A A A A A A `
     ///
-    /// Each `A` represents a number, and the numbers can be more than 1 digit long. The section can
-    /// contain leading and/or trailing spaces.
+    /// Each `A` represents a number, and the numbers can be more than 1 digit long. The section
+    /// can contain a leading and/or trailing space.
     ///
-    /// @param section a section of the puzzle input that contains a set of numbers to be
-    ///         parsed.
+    /// @param section a section of the puzzle input that contains a set of numbers to
+    ///         be parsed.
     /// @return the set of the numbers in `section`.
     private static Set<Integer> parseNumbers(String section)
     {
@@ -104,21 +154,21 @@ public class Solution implements PuzzleSolver
     
     /// Calculates the total points won by a scratchcard.
     ///
-    /// The first winning number is worth one point, and every number after that doubles the point
+    /// The first matching number is worth one point, and every number after that doubles the point
     /// value.
     ///
-    /// @param actualWinningNumbers the winning scratchcard numbers.
+    /// @param totalMatches the number of matches on the card.
     /// @return the points won by a scratchcard.
-    private static int calculatePoints(Set<Integer> actualWinningNumbers)
+    private static int calculatePoints(int totalMatches)
     {
         int points = 0;
         
-        if (!actualWinningNumbers.isEmpty())
+        if (totalMatches >= 1)
         {
             points = 1;
         }
         
-        for (int winners = 1; winners < actualWinningNumbers.size(); winners++)
+        for (int matches = 2; matches <= totalMatches; matches++)
         {
             points *= 2;
         }
@@ -126,6 +176,67 @@ public class Solution implements PuzzleSolver
         return points;
     }
     
+    /// Increases the card count by the given amount.
+    ///
+    /// @param cardId the card id.
+    /// @param cardCounts a mapping between the card ids and their counts.
+    /// @param amount the amount to increase the card id by.
+    private static void increaseCardCount(int cardId, Map<Integer, Integer> cardCounts, int amount)
+    {
+        int count = cardCounts.getOrDefault(cardId, 0);
+        cardCounts.put(cardId, count + amount);
+    }
+    
+    /// Increases the next `m` card counts by the current card count, where `m` is the number of
+    /// matches.
+    ///
+    /// @param currentCardId the current card id.
+    /// @param cardCounts a mapping between each card id and its count.
+    /// @param matches the number of matches on the card.
+    private static void increaseNextCardCounts(
+            int currentCardId,
+            Map<Integer, Integer> cardCounts,
+            int matches)
+    {
+        int startId = currentCardId + 1;
+        int endId = currentCardId + matches;
+        
+        for (int nextCardId = startId; nextCardId <= endId; nextCardId++)
+        {
+            increaseCardCount(nextCardId, cardCounts, cardCounts.get(currentCardId));
+        }
+    }
+    
+    /// Calculates the sum of all the card counts.
+    ///
+    /// @param cardCounts a mapping between each card id and its count.
+    /// @return the sum of all the card counts.
+    private static int sumCardCounts(Map<Integer, Integer> cardCounts)
+    {
+        int sum = 0;
+        
+        for (int count : cardCounts.values())
+        {
+            sum += count;
+        }
+        
+        return sum;
+    }
+    
     /// Stores the data for each card.
-    private record Card(Set<Integer> winningNumbers, Set<Integer> ourNumbers) {}
+    private record Card(int id, Set<Integer> winningNumbers, Set<Integer> ourNumbers)
+    {
+        /// Creates a set of the matching numbers.
+        ///
+        /// A matching number is considered any number that appears in both sets.
+        ///
+        /// @return the set of matching numbers.
+        public Set<Integer> getMatchingNumbers()
+        {
+            Set<Integer> matchingNumbers = new HashSet<>(this.winningNumbers());
+            matchingNumbers.retainAll(this.ourNumbers());
+            
+            return matchingNumbers;
+        }
+    }
 }
--- src/test/java/aoc/event/year2023/day04/scratchcards/SolutionTests.java
+++ src/test/java/aoc/event/year2023/day04/scratchcards/SolutionTests.java
@@ -1,5 +1,6 @@
 package aoc.event.year2023.day04.scratchcards;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,65 +11,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTests
 {
-    @Test
-    void exampleCard1()
+    @Nested
+    class PartOne
     {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
-        );
-        assertEquals(8, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void exampleCard2()
-    {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
-        );
-        assertEquals(2, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void exampleCard3()
-    {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")
-        );
-        assertEquals(2, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void exampleCard4()
-    {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")
-        );
-        assertEquals(1, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void exampleCard5()
-    {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")
-        );
-        assertEquals(0, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void exampleCard6()
-    {
-        List<String> inputLines = new ArrayList<>(
-                List.of("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
-        );
-        assertEquals(0, new Solution().partOne(inputLines));
-    }
-    
-    @Test
-    void completeExample()
-    {
-        List<String> inputLines = new ArrayList<>(
-                Arrays.stream("""
+        @Test
+        void exampleCard1()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+            );
+            assertEquals(8, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void exampleCard2()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
+            );
+            assertEquals(2, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void exampleCard3()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")
+            );
+            assertEquals(2, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void exampleCard4()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")
+            );
+            assertEquals(1, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void exampleCard5()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")
+            );
+            assertEquals(0, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void exampleCard6()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    List.of("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+            );
+            assertEquals(0, new Solution().partOne(inputLines));
+        }
+        
+        @Test
+        void completeExample()
+        {
+            List<String> inputLines = new ArrayList<>(
+                    Arrays.stream("""
                 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
                 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
                 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -76,8 +80,87 @@ public class SolutionTests
                 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
                 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
                 """.split("\n")).toList()
-        );
-        assertEquals(13, new Solution().partOne(inputLines));
+            );
+            assertEquals(13, new Solution().partOne(inputLines));
+        }
     }
     
+    @Nested
+    class PartTwo
+    {
+        @Test
+        void exampleCard1()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                """.split("\n")).toList()
+            );
+            assertEquals(1, new Solution().partTwo(inputLines));
+        }
+        
+        @Test
+        void exampleCards1to2()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                """.split("\n")).toList()
+            );
+            assertEquals(3, new Solution().partTwo(inputLines));
+        }
+        
+        @Test
+        void exampleCards1to3()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                """.split("\n")).toList()
+            );
+            assertEquals(7, new Solution().partTwo(inputLines));
+        }
+        
+        @Test
+        void exampleCards1to4()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                """.split("\n")).toList()
+            );
+            assertEquals(15, new Solution().partTwo(inputLines));
+        }
+        
+        @Test
+        void exampleCards1to5()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                """.split("\n")).toList()
+            );
+            assertEquals(29, new Solution().partTwo(inputLines));
+        }
+        
+        @Test
+        void exampleCards1to6()
+        {
+            List<String> inputLines = new ArrayList<>(Arrays.stream("""
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """.split("\n")).toList()
+            );
+            assertEquals(30, new Solution().partTwo(inputLines));
+        }
+    }
 }
