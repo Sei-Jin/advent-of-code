ba7ce42d2af7722dc721eb57e68ac5b76b242873
Sei-Jin <kajiquiet@gmail.com>
2024-12-28 18:30:49 -0400

    [2016-4] Complete part one

--- src/main/java/aoc/event/year2016/day04/securityThroughObscurity/Solution.java
+++ src/main/java/aoc/event/year2016/day04/securityThroughObscurity/Solution.java
@@ -2,14 +2,112 @@ package aoc.event.year2016.day04.securityThroughObscurity;
 
 import aoc.PuzzleSolver;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution implements PuzzleSolver
 {
+    private static final Pattern ROOM_PATTERN = Pattern.compile("([\\w-]+)-(\\d+)\\[(\\w+)]");
+    
     @Override
     public Object partOne(List<String> inputLines)
     {
-        return null;
+        int sectorIdSum = 0;
+        
+        for (String line : inputLines)
+        {
+            Room room = parseRoom(line);
+            
+            String encryptedName = room.encryptedName.replaceAll("-", "");
+            Map<Character, Integer> letterCount = getLetterCount(encryptedName);
+            
+            List<Character> letters = new ArrayList<>(letterCount.keySet());
+            
+            Comparator<Character> comparator = getLetterComparator(letterCount);
+            letters.sort(comparator);
+            
+            String checksum = getChecksum(letters);
+            boolean realRoom = checksum.equals(room.checksum);
+            
+            if (realRoom)
+            {
+                sectorIdSum += room.sectorId;
+            }
+        }
+        
+        return sectorIdSum;
+    }
+    
+    private static Room parseRoom(String line)
+    {
+        Matcher matcher = ROOM_PATTERN.matcher(line);
+        
+        if (matcher.find())
+        {
+            String encryptedName = matcher.group(1);
+            int sectorId = Integer.parseInt(matcher.group(2));
+            String checksum = matcher.group(3);
+            
+            encryptedName = encryptedName.replaceAll("-", "");
+            
+            return new Room(encryptedName, sectorId, checksum);
+        }
+        else
+        {
+            throw new IllegalArgumentException(
+                    "Input line did not match expected pattern: " + line
+            );
+        }
+    }
+    
+    private static Map<Character, Integer> getLetterCount(String encryptedName)
+    {
+        Map<Character, Integer> letterCount = new HashMap<>();
+        
+        for (int index = 0; index < encryptedName.length(); index++)
+        {
+            char letter = encryptedName.charAt(index);
+            int count = letterCount.getOrDefault(letter, 0) + 1;
+            
+            letterCount.put(letter, count);
+        }
+        
+        return letterCount;
+    }
+    
+    private static Comparator<Character> getLetterComparator(Map<Character, Integer> letterCount)
+    {
+        return (predecessor, successor) ->
+        {
+            int predecessorCount = letterCount.get(predecessor);
+            int successorCount = letterCount.get(successor);
+            
+            if (predecessorCount < successorCount)
+            {
+                return 1;
+            }
+            else if (predecessorCount == successorCount)
+            {
+                return predecessor.compareTo(successor);
+            }
+            else
+            {
+                return -1;
+            }
+        };
+    }
+    
+    private static String getChecksum(List<Character> letters)
+    {
+        StringBuilder checksumBuilder = new StringBuilder();
+        
+        for (int index = 0; index < 5; index++)
+        {
+            checksumBuilder.append(letters.get(index));
+        }
+        
+        return checksumBuilder.toString();
     }
     
     @Override
@@ -17,4 +115,6 @@ public class Solution implements PuzzleSolver
     {
         return null;
     }
+    
+    record Room(String encryptedName, int sectorId, String checksum) {}
 }
--- src/test/java/aoc/event/year2016/day04/securityThroughObscurity/SolutionTests.java
+++ src/test/java/aoc/event/year2016/day04/securityThroughObscurity/SolutionTests.java
@@ -1,6 +1,52 @@
 package aoc.event.year2016.day04.securityThroughObscurity;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SolutionTests
 {
-
+    @Test
+    void exampleLine1()
+    {
+        List<String> input = List.of("aaaaa-bbb-z-y-x-123[abxyz]");
+        assertEquals(123, new Solution().partOne(input));
+    }
+    
+    @Test
+    void exampleLine2()
+    {
+        List<String> input = List.of("a-b-c-d-e-f-g-h-987[abcde]");
+        assertEquals(987, new Solution().partOne(input));
+    }
+    
+    @Test
+    void exampleLine3()
+    {
+        List<String> input = List.of("not-a-real-room-404[oarel]");
+        assertEquals(404, new Solution().partOne(input));
+    }
+    
+    @Test
+    void exampleLine4()
+    {
+        List<String> input = List.of("totally-real-room-200[decoy]");
+        assertEquals(0, new Solution().partOne(input));
+    }
+    
+    @Test
+    void completeExample()
+    {
+        List<String> input = Arrays.stream("""
+                aaaaa-bbb-z-y-x-123[abxyz]
+                a-b-c-d-e-f-g-h-987[abcde]
+                not-a-real-room-404[oarel]
+                totally-real-room-200[decoy]
+                """.split("\n")).toList();
+        
+        assertEquals(1514, new Solution().partOne(input));
+    }
 }
