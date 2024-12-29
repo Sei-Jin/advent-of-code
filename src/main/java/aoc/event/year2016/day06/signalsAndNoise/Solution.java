fd9c200cac905e1481c74e4cc72d683fb1783743
Sei-Jin <kajiquiet@gmail.com>
2024-12-29 19:07:54 -0400

    [2016-6] Complete part one

--- src/main/java/aoc/event/year2016/day06/signalsAndNoise/Solution.java
+++ src/main/java/aoc/event/year2016/day06/signalsAndNoise/Solution.java
@@ -2,14 +2,62 @@ package aoc.event.year2016.day06.signalsAndNoise;
 
 import aoc.PuzzleSolver;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution implements PuzzleSolver
 {
+    private static Map<Character, Integer> getCharacterCounts(
+            List<String> inputLines,
+            int index)
+    {
+        Map<Character, Integer> characterCounts = new HashMap<>();
+        
+        for (String line : inputLines)
+        {
+            char character = line.charAt(index);
+            int count = characterCounts.getOrDefault(character, 0) + 1;
+            
+            characterCounts.put(character, count);
+        }
+        
+        return characterCounts;
+    }
+    
     @Override
     public Object partOne(List<String> inputLines)
     {
-        return null;
+        StringBuilder messageBuilder = new StringBuilder();
+        
+        for (int index = 0; index < inputLines.getFirst().length(); index++)
+        {
+            Map<Character, Integer> characterCounts = getCharacterCounts(inputLines, index);
+            
+            char mostFrequentCharacter = getMostFrequentCharacter(characterCounts);
+            messageBuilder.append(mostFrequentCharacter);
+        }
+        
+        return messageBuilder.toString();
+    }
+    
+    private static char getMostFrequentCharacter(Map<Character, Integer> characterCounts)
+    {
+        char mostFrequentCharacter = 0;
+        int maxCount = 0;
+        
+        for (char character : characterCounts.keySet())
+        {
+            int count = characterCounts.get(character);
+            
+            if (count > maxCount)
+            {
+                maxCount = count;
+                mostFrequentCharacter = character;
+            }
+        }
+        
+        return mostFrequentCharacter;
     }
     
     @Override
--- src/test/java/aoc/event/year2016/day06/signalsAndNoise/SolutionTests.java
+++ src/test/java/aoc/event/year2016/day06/signalsAndNoise/SolutionTests.java
@@ -1,6 +1,35 @@
 package aoc.event.year2016.day06.signalsAndNoise;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SolutionTests
 {
-
+    @Test
+    void example()
+    {
+        List<String> input = Arrays.stream("""
+                eedadn
+                drvtee
+                eandsr
+                raavrd
+                atevrs
+                tsrnev
+                sdttsa
+                rasrtv
+                nssdts
+                ntnada
+                svetve
+                tesnvt
+                vntsnd
+                vrdear
+                dvrsen
+                enarar
+                """.split("\n")).toList();
+        assertEquals("easter", new Solution().partOne(input));
+    }
 }
