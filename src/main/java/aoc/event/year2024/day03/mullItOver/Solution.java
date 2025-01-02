d671a38164c8965be2d0236d2e6fec7303fe11ed
Sei-Jin <kajiquiet@gmail.com>
2025-01-02 19:30:54 -0400

    [2024-3] Refactor solution
    The methods were reordered and some of the naming was changed.
    
    Looking over this solution again, there is a lot that could be improved in both performance and readability.

--- src/main/java/aoc/event/year2024/day03/mullItOver/Solution.java
+++ src/main/java/aoc/event/year2024/day03/mullItOver/Solution.java
@@ -9,11 +9,100 @@ import java.util.regex.Pattern;
 
 public class Solution implements PuzzleSolver
 {
-    /// Calculates the sum of all multiplication instructions in program memory.
+    /// This enum stores the regular expression for each type of instruction.
+    enum Instruction
+    {
+        MULTIPLY("mul\\(\\d+,\\d+\\)"),
+        DO("do\\(\\)"),
+        DO_NOT("don't\\(\\)");
+        
+        private final String regex;
+        
+        Instruction(String regex)
+        {
+            this.regex = regex;
+        }
+        
+        public String getRegex()
+        {
+            return regex;
+        }
+    }
+    
+    /// Creates a pattern matcher for the given section of the program.
     ///
     /// The puzzle input represents a program with corrupted memory and contains a series of
     /// multiplication instructions to follow. A multiplication instruction is represented as
     /// `mul(X,Y)` and multiplies the two numbers `X` and `Y`.
+    ///
+    /// The pattern that is matched depends on if all multiplications are enabled or not. If not all
+    /// multiplications are enabled then `do` and `don't` instructions should also be matched.
+    ///
+    /// @param programSection a section of the program.
+    /// @param allMultiplicationsEnabled true if all multiplication instructions are
+    ///         enabled, false if otherwise.
+    /// @return a pattern matcher for the section of the program.
+    private static Matcher createPatternMatcher(
+            String programSection,
+            boolean allMultiplicationsEnabled)
+    {
+        StringBuilder regex = new StringBuilder(Instruction.MULTIPLY.getRegex());
+        
+        if (!allMultiplicationsEnabled)
+        {
+            regex.append("|");
+            regex.append(Instruction.DO_NOT.getRegex());
+            regex.append("|");
+            regex.append(Instruction.DO.getRegex());
+        }
+        
+        Pattern pattern = Pattern.compile(regex.toString());
+        return pattern.matcher(programSection);
+    }
+    
+    /// Returns the list of valid instructions from a section of the program.
+    ///
+    /// A valid instruction is any instruction that is matched by the pattern matcher.
+    ///
+    /// @param patternMatcher a matcher for valid instructions.
+    /// @return the list of valid instructions.
+    private static List<String> parseValidInstructions(Matcher patternMatcher)
+    {
+        List<String> instructions = new ArrayList<>();
+        
+        while (patternMatcher.find())
+        {
+            String instruction = patternMatcher.group();
+            instructions.add(instruction);
+        }
+        
+        return instructions;
+    }
+    
+    /// Calculates the product of a multiplication instruction.
+    ///
+    /// A multiplication instruction is in the form `mul(X,Y)`, where `X` and `Y` are the two
+    /// numbers to be multiplied together.
+    ///
+    /// @param instruction a multiplication instruction.
+    /// @return the product of the instruction.
+    private static int calculateProduct(String instruction)
+    {
+        int product = 1;
+        
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(instruction);
+        
+        while (matcher.find())
+        {
+            int factor = Integer.parseInt(matcher.group());
+            product *= factor;
+        }
+        
+        return product;
+    }
+    
+    /// Calculates the sum of all multiplication instructions in program memory.
     ///
     /// Time Complexity: O(n)
     /// - One pass is completed over the puzzle input to find and store the instructions. Then each
@@ -32,12 +121,12 @@ public class Solution implements PuzzleSolver
         
         for (String programSection : programMemory)
         {
-            Matcher patternMatcher = getPatternMatcher(programSection, true);
-            List<String> instructions = getValidInstructions(patternMatcher);
+            Matcher patternMatcher = createPatternMatcher(programSection, true);
+            List<String> instructions = parseValidInstructions(patternMatcher);
             
             for (String instruction : instructions)
             {
-                sumOfProducts += getProduct(instruction);
+                sumOfProducts += calculateProduct(instruction);
             }
         }
         
@@ -66,8 +155,8 @@ public class Solution implements PuzzleSolver
         
         for (String programSection : programMemory)
         {
-            Matcher patternMatcher = getPatternMatcher(programSection, false);
-            List<String> instructions = getValidInstructions(patternMatcher);
+            Matcher patternMatcher = createPatternMatcher(programSection, false);
+            List<String> instructions = parseValidInstructions(patternMatcher);
             
             for (String instruction : instructions)
             {
@@ -81,100 +170,11 @@ public class Solution implements PuzzleSolver
                 }
                 else if (enableMultiplications)
                 {
-                    sumOfProducts += getProduct(instruction);
+                    sumOfProducts += calculateProduct(instruction);
                 }
             }
         }
         
         return sumOfProducts;
-    }
-    
-    /// Returns a pattern matcher for the given section of the program.
-    ///
-    /// The pattern that is matched depends on if all multiplications are enabled or not. If not all
-    /// multiplications are enabled then `do` and `don't` instructions should also be matched.
-    ///
-    /// @param programSection a section of the program.
-    /// @param allMultiplicationsEnabled true if all multiplication instructions are
-    ///         enabled, false if otherwise.
-    /// @return a pattern matcher for the section of the program.
-    private static Matcher getPatternMatcher(
-            String programSection,
-            boolean allMultiplicationsEnabled)
-    {
-        StringBuilder regex = new StringBuilder(Instruction.MULTIPLY.getRegex());
-        
-        if (!allMultiplicationsEnabled)
-        {
-            regex.append("|");
-            regex.append(Instruction.DO_NOT.getRegex());
-            regex.append("|");
-            regex.append(Instruction.DO.getRegex());
-        }
-        
-        Pattern pattern = Pattern.compile(regex.toString());
-        return pattern.matcher(programSection);
-    }
-    
-    /// Returns the list of valid instructions from a section of the program.
-    ///
-    /// A valid instruction is any instruction that is matched by the pattern matcher.
-    ///
-    /// @param patternMatcher a matcher for valid instructions.
-    /// @return the list of valid instructions.
-    private static List<String> getValidInstructions(Matcher patternMatcher)
-    {
-        List<String> instructions = new ArrayList<>();
-        
-        while (patternMatcher.find())
-        {
-            String instruction = patternMatcher.group();
-            instructions.add(instruction);
-        }
-        
-        return instructions;
-    }
-    
-    /// Calculates the product of a multiplication instruction.
-    ///
-    /// A multiplication instruction is in the form `mul(X,Y)`, where `X` and `Y` are the two
-    /// numbers to be multiplied together.
-    ///
-    /// @param instruction a multiplication instruction.
-    /// @return the product of the instruction.
-    private static int getProduct(String instruction)
-    {
-        int product = 1;
-        
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(instruction);
-        
-        while (matcher.find())
-        {
-            int factor = Integer.parseInt(matcher.group());
-            product *= factor;
-        }
-        
-        return product;
-    }
-    
-    /// This enum stores the regular expression for each type of instruction.
-    enum Instruction
-    {
-        MULTIPLY("mul\\(\\d+,\\d+\\)"),
-        DO("do\\(\\)"),
-        DO_NOT("don't\\(\\)");
-        
-        private final String regex;
-        
-        Instruction(String regex)
-        {
-            this.regex = regex;
-        }
-        
-        public String getRegex()
-        {
-            return regex;
-        }
     }
 }
