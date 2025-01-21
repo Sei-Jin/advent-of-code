7b4d4d2833df9a97d0816d2b91d1086aa2ac3cc0
Sei-Jin <kajiquiet@gmail.com>
2025-01-21 11:48:17 -0400

    [2018-1] Refactor solution
    The following changes were made:
    
    1. A common parsing method was extracted from both parts of the solution.
    
    2. JavaDoc comments were added, expanded upon, and converted to Markdown format.
    
    3. Logic in the part two method was simplified by adding an immediate return instead of using a boolean condition and a return at the end of the method.
    
    4. A static variable was added to reduce the usage of magic numbers.

--- src/main/java/aoc/event/year2018/day01/chronalCalibration/Solution.java
+++ src/main/java/aoc/event/year2018/day01/chronalCalibration/Solution.java
@@ -2,89 +2,115 @@ package aoc.event.year2018.day01.chronalCalibration;
 
 import aoc.PuzzleSolver;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * --- Day 1: Chronal Calibration ---
- */
 public class Solution implements PuzzleSolver
 {
-    /**
-     * @param inputLines the puzzle input.
-     * @return the resulting frequency.
-     */
-    @Override
-    public Object partOne(List<String> inputLines)
+    /// The starting frequency value.
+    private static final int STARTING_FREQUENCY = 0;
+    
+    /// Parses a frequency change from the input line.
+    ///
+    /// The input line is in the form:
+    ///
+    /// ```
+    /// -32
+    /// +54
+    /// -3
+    /// +6
+    /// ```
+    ///
+    /// Each line starts with a sign, (+/-), followed by a number.
+    /// - The sign represents if the frequency change is positive or negative.
+    /// - The number represents the value of the frequency change.
+    ///
+    /// @param line a line of the puzzle input.
+    /// @return the change in frequency.
+    private static int parseFrequencyChange(String line)
     {
-        int currentFrequency = 0;
+        char sign = line.charAt(0);
+        int frequencyChange = Integer.parseInt(line.substring(1));
         
-        for (String line : inputLines)
+        if (sign == '-')
         {
-            char sign = line.charAt(0);
-            int frequencyChange = Integer.parseInt(line.substring(1));
-            
-            if (sign == '-')
-            {
-                frequencyChange *= -1;
-            }
-            
+            frequencyChange *= -1;
+        }
+        
+        return frequencyChange;
+    }
+    
+    /// Calculates the resulting frequency after all frequency changes are applied.
+    ///
+    /// Time Complexity: `O(n)`
+    /// - `n` is the number of frequency changes.
+    ///
+    /// Space Complexity: `O(1)`
+    /// - One frequency change is stored at a time.
+    ///
+    /// @param puzzleInput the puzzle input.
+    /// @return the resulting frequency.
+    @Override
+    public Object partOne(List<String> puzzleInput)
+    {
+        int currentFrequency = STARTING_FREQUENCY;
+        
+        for (String line : puzzleInput)
+        {
+            int frequencyChange = parseFrequencyChange(line);
             currentFrequency += frequencyChange;
         }
         
         return currentFrequency;
     }
     
-    
-    /**
-     * @param inputLines the puzzle input.
-     * @return the first frequency reached twice.
-     */
+    /// Calculates the first frequency reached twice.
+    ///
+    /// If no duplicate frequency is found then the changes will be applied again until a
+    /// duplicate frequency is found.
+    ///
+    /// Time Complexity: `O(n)`
+    /// - `n` is the number of frequency changes.
+    ///
+    /// Space Complexity: `O(n)`
+    /// - `n` is the number of frequency changes.
+    ///
+    /// @param puzzleInput the puzzle input.
+    /// @return the first frequency reached twice.
+    /// @throws IllegalStateException if there is not a frequency reached twice.
     @Override
-    public Object partTwo(List<String> inputLines)
+    public Object partTwo(List<String> puzzleInput)
     {
-        int startingFrequency = 0;
+        List<Integer> frequencyChanges = new ArrayList<>();
         
-        return findDuplicateFrequency(inputLines, startingFrequency);
-    }
-    
-    
-    private static int findDuplicateFrequency(List<String> inputLines, int startingFrequency)
-    {
-        Set<Integer> previousFrequencies = new HashSet<>();
-        
-        previousFrequencies.add(startingFrequency);
-        int currentFrequency = startingFrequency;
-        
-        boolean foundDuplicate = false;
-        
-        while (!foundDuplicate)
+        for (String line : puzzleInput)
         {
-            for (String line : inputLines)
+            int frequencyChange = parseFrequencyChange(line);
+            frequencyChanges.add(frequencyChange);
+        }
+        
+        int currentFrequency = STARTING_FREQUENCY;
+        
+        Set<Integer> previousFrequencies = new HashSet<>();
+        previousFrequencies.add(STARTING_FREQUENCY);
+        
+        while (true)
+        {
+            for (int frequencyChange : frequencyChanges)
             {
-                char sign = line.charAt(0);
-                int frequencyChange = Integer.parseInt(line.substring(1));
-                
-                if (sign == '-')
-                {
-                    frequencyChange *= -1;
-                }
-                
                 currentFrequency += frequencyChange;
                 
-                if (previousFrequencies.contains(currentFrequency))
-                {
-                    foundDuplicate = true;
-                    break;
-                }
-                else
+                if (!previousFrequencies.contains(currentFrequency))
                 {
                     previousFrequencies.add(currentFrequency);
                 }
+                else
+                {
+                    return currentFrequency;
+                }
             }
         }
-        
-        return currentFrequency;
     }
 }
