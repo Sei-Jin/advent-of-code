6cc7d88d093a8ff1524c4e9df27b356c1c45a4e1
Sei-Jin <seijin.tufts@gmail.com>
2025-01-27 12:07:48 -0400

    [2022-6] Add solution

--- src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
+++ src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
@@ -6,15 +6,75 @@ import java.util.List;
 
 public class Solution implements PuzzleSolver
 {
-    @Override
-    public Object partOne(List<String> inputLines)
+    /// Finds the position of the first marker.
+    ///
+    /// The position of the first marker is calculated by finding the first substring of distinct
+    /// characters with length `windowSize`.
+    ///
+    /// @param line finds the position of the first marker.
+    /// @param windowSize the length of distinct characters.
+    /// @return the position of the first marker.
+    private static int findFirstMarker(String line, int windowSize)
     {
-        return null;
+        for (int i = 0; i < line.length(); i++)
+        {
+            String substring = line.substring(i, i + windowSize);
+            boolean containsDuplicate = containsDuplicate(substring);
+            
+            if (!containsDuplicate)
+            {
+                return i + windowSize;
+            }
+        }
+        
+        throw new IllegalStateException("No markers were detected.");
     }
     
-    @Override
-    public Object partTwo(List<String> inputLines)
+    /// Determines if the string contains a duplicate character.
+    ///
+    /// @param string a string.
+    /// @return true if the string contains a duplicate character, or false otherwise.
+    private static boolean containsDuplicate(String string)
     {
-        return null;
+        for (int i = 0; i < string.length() - 1; i++)
+        {
+            for (int j = i + 1; j < string.length(); j++)
+            {
+                if (string.charAt(i) == string.charAt(j))
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    /// Calculates the position of the first start-of-packet marker.
+    ///
+    /// The start-of-packet marker starts after the first substring of length 4 with all distinct
+    /// characters.
+    ///
+    /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
+    /// @return the position of the first start-of-packet marker.
+    @Override
+    public Object partOne(List<String> puzzleInput)
+    {
+        String line = puzzleInput.getFirst();
+        return findFirstMarker(line, 4);
+    }
+    
+    /// Calculates the position of the first start-of-message marker.
+    ///
+    /// The start-of-message marker starts after the first substring of length 14 with all distinct
+    /// characters.
+    ///
+    /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
+    /// @return the position of the first start-of-message marker.
+    @Override
+    public Object partTwo(List<String> puzzleInput)
+    {
+        String line = puzzleInput.getFirst();
+        return findFirstMarker(line, 14);
     }
 }
--- src/test/java/aoc/event/year2022/day06/tuningTrouble/SolutionTests.java
+++ src/test/java/aoc/event/year2022/day06/tuningTrouble/SolutionTests.java
@@ -1,6 +1,50 @@
 package aoc.event.year2022.day06.tuningTrouble;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SolutionTests
 {
-
+    @Test
+    void example1()
+    {
+        List<String> input = List.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assertEquals(7, new Solution().partOne(input));
+        assertEquals(19, new Solution().partTwo(input));
+    }
+    
+    @Test
+    void example2()
+    {
+        List<String> input = List.of("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        assertEquals(5, new Solution().partOne(input));
+        assertEquals(23, new Solution().partTwo(input));
+    }
+    
+    @Test
+    void example3()
+    {
+        List<String> input = List.of("nppdvjthqldpwncqszvftbrmjlhg");
+        assertEquals(6, new Solution().partOne(input));
+        assertEquals(23, new Solution().partTwo(input));
+    }
+    
+    @Test
+    void example4()
+    {
+        List<String> input = List.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+        assertEquals(10, new Solution().partOne(input));
+        assertEquals(29, new Solution().partTwo(input));
+    }
+    
+    @Test
+    void example5()
+    {
+        List<String> input = List.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        assertEquals(11, new Solution().partOne(input));
+        assertEquals(26, new Solution().partTwo(input));
+    }
 }
