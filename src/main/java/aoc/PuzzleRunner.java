a083e2f723acf21c7050e0e3bfe397070de5036d
Sei-Jin <seijin.tufts@gmail.com>
2025-01-27 15:29:46 -0400

    Add comments to the puzzle runner

--- src/main/java/aoc/PuzzleRunner.java
+++ src/main/java/aoc/PuzzleRunner.java
@@ -4,14 +4,22 @@ import java.util.List;
 
 public class PuzzleRunner
 {
+    /// There are two parts to each puzzle.
     private enum Part
     {
         ONE, TWO
     }
     
+    /// Stores the execution data for a single part of the puzzle.
     private record PartData(Object result, long executionTimeInMicroseconds) {}
+    
+    /// Stores the execution data for both parts of the puzzle.
     protected record RunData(PartData partOne, PartData partTwo) {}
     
+    /// Runs and prints both parts of the puzzle.
+    ///
+    /// @param year the year of the puzzle.
+    /// @param day the day of the puzzle.
     public static void runAndPrint(int year, int day)
     {
         Puzzle puzzle = new Puzzle(year, day);
@@ -19,33 +27,35 @@ public class PuzzleRunner
         print(puzzle, runData);
     }
     
-    /// Executes the steps needed to run a single puzzle.
+    /// Runs both parts of the given puzzle
     ///
-    /// 1. The puzzle input is retrieved.
-    /// 2. The puzzle solution is run with the puzzle input.
-    /// 3. The puzzle results are printed.
-    ///
-    /// @param puzzle the puzzle.
-    /// @return the data for each of the two parts.
+    /// @param puzzle a puzzle.
+    /// @return the execution data for each of the two parts.
     protected static RunData run(Puzzle puzzle)
     {
         PuzzleSolver puzzleSolver = puzzle.determinePuzzleSolver();
-        List<String> inputLines = PuzzleInputRetriever.retrievePuzzleInput(puzzle);
+        List<String> puzzleInput = PuzzleInputRetriever.retrievePuzzleInput(puzzle);
         
-        PartData partOne = runPart(Part.ONE, puzzleSolver, inputLines);
-        PartData partTwo = runPart(Part.TWO, puzzleSolver, inputLines);
+        PartData partOne = runPart(Part.ONE, puzzleSolver, puzzleInput);
+        PartData partTwo = runPart(Part.TWO, puzzleSolver, puzzleInput);
         
         return new RunData(partOne, partTwo);
     }
     
-    private static PartData runPart(Part part, PuzzleSolver solver, List<String> inputLines)
+    /// Executes a part of the puzzle and stores the execution data.
+    ///
+    /// @param part the part to run.
+    /// @param solver the puzzle solver.
+    /// @param puzzleInput the input of the puzzle.
+    /// @return the execution data for the part.
+    private static PartData runPart(Part part, PuzzleSolver solver, List<String> puzzleInput)
     {
         long startTime =  System.nanoTime();
         
         Object partOneResult = switch (part)
         {
-            case Part.ONE -> solver.partOne(inputLines);
-            case Part.TWO -> solver.partTwo(inputLines);
+            case Part.ONE -> solver.partOne(puzzleInput);
+            case Part.TWO -> solver.partTwo(puzzleInput);
         };
         
         long endTime = System.nanoTime();
@@ -56,19 +66,27 @@ public class PuzzleRunner
         return new PartData(partOneResult, executionTimeInMicroseconds);
     }
     
+    /// Prints the execution data for the puzzle.
+    ///
+    /// @param puzzle the puzzle
+    /// @param runData the execution data of the puzzle.
     private static void print(Puzzle puzzle, RunData runData)
     {
         System.out.printf("\n Year: %d Day: %d\n", puzzle.getYear(), puzzle.getDay());
         System.out.println("---------------");
         
         System.out.printf("%-15s", "Part One");
-        printData(runData.partOne.result, runData.partOne.executionTimeInMicroseconds);
+        printPartData(runData.partOne.result, runData.partOne.executionTimeInMicroseconds);
         
         System.out.printf("\n%-15s", "Part Two");
-        printData(runData.partTwo.result, runData.partTwo.executionTimeInMicroseconds);
+        printPartData(runData.partTwo.result, runData.partTwo.executionTimeInMicroseconds);
     }
     
-    private static void printData(Object result, long executionTimeInMicroseconds)
+    /// Prints the data for one part of the execution data.
+    ///
+    /// @param result the solution part output.
+    /// @param executionTimeInMicroseconds the execution time of the solution part.
+    private static void printPartData(Object result, long executionTimeInMicroseconds)
     {
         if (result != null)
         {
