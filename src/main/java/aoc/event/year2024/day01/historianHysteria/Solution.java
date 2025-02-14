f6e925603be0df08135c5b05abe1e4882360584e
Sei-Jin <seijin.tufts@gmail.com>
2025-02-13 22:59:21 -0400

    [2024-1] Refactor and reformat solution
    Formatting now uses the default java formatting. Variables now use var instead of explicit types.

--- src/main/java/aoc/event/year2024/day01/historianHysteria/Solution.java
+++ src/main/java/aoc/event/year2024/day01/historianHysteria/Solution.java
@@ -1,19 +1,23 @@
 package aoc.event.year2024.day01.historianHysteria;
 
+import aoc.PuzzleRunner;
 import aoc.PuzzleSolver;
 
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Solution implements PuzzleSolver
-{
+/// 2024 Day 1 - Historian Hysteria
+public class Solution implements PuzzleSolver {
+
+    /// Pattern for each line of the puzzle input
+    private static final Pattern LINE_PATTERN = Pattern.compile("(\\d+)\\s+(\\d+)");
+
     /// This record class stores the data for the two lists of ids.
     ///
-    /// @param firstList the first list of ids.
+    /// @param firstList  the first list of ids.
     /// @param secondList the second list of ids.
     private record IdLists(List<Integer> firstList, List<Integer> secondList) {}
-    
+
     /// Parses the puzzle input for the ids in each list.
     ///
     /// The puzzle input is in the form:
@@ -22,7 +26,7 @@ public class Solution implements PuzzleSolver
     /// A...B
     /// A...B
     /// A...B
-    /// ```
+    ///```
     ///
     /// Where:
     /// - Each `.` represents a space and should be ignored.
@@ -33,36 +37,28 @@ public class Solution implements PuzzleSolver
     /// and the second id is placed in the second list. Also note that the ids can be longer
     /// than a single digit.
     ///
-    /// @param inputLines the puzzle input.
+    /// @param puzzleInput the puzzle input.
     /// @return a record of the two id lists.
-    private static IdLists getIdLists(List<String> inputLines)
-    {
-        List<Integer> firstList = new ArrayList<>();
-        List<Integer> secondList = new ArrayList<>();
-        
-        String regex = "(\\d+)\\s+(\\d+)";
-        Pattern pattern = Pattern.compile(regex);
-        
-        for (String line : inputLines)
-        {
-            Matcher matcher = pattern.matcher(line);
-            
-            if (matcher.find())
-            {
+    private static IdLists parse(List<String> puzzleInput) {
+        final var firstList = new ArrayList<Integer>();
+        final var secondList = new ArrayList<Integer>();
+
+        for (final var line : puzzleInput) {
+            final var matcher = LINE_PATTERN.matcher(line);
+
+            if (matcher.find()) {
                 firstList.add(Integer.valueOf(matcher.group(1)));
                 secondList.add(Integer.valueOf(matcher.group(2)));
-            }
-            else
-            {
+            } else {
                 throw new IllegalArgumentException(
                         "Encountered unexpected format in puzzle input for line: %s".formatted(line)
                 );
             }
         }
-        
+
         return new IdLists(firstList, secondList);
     }
-    
+
     /// Calculates the total distance between the two lists of ids.
     ///
     /// Time Complexity: O(n log n)
@@ -71,15 +67,14 @@ public class Solution implements PuzzleSolver
     /// Space Complexity: O(n)
     /// - All ids from the puzzle input are parsed and then stored.
     ///
-    /// @param inputLines the puzzle input.
+    /// @param puzzleInput the puzzle input.
     /// @return the total distance between the two lists of ids.
     @Override
-    public Object partOne(List<String> inputLines)
-    {
-        IdLists idLists = getIdLists(inputLines);
+    public Object partOne(List<String> puzzleInput) {
+        final var idLists = parse(puzzleInput);
         return calculateTotalDistance(idLists);
     }
-    
+
     /// Calculates the total distance between the ids of the two lists.
     ///
     /// The total distance is calculated by comparing the ids in each list from smallest to
@@ -87,25 +82,21 @@ public class Solution implements PuzzleSolver
     ///
     /// @param idLists the two ids lists.
     /// @return the total distance between the numbers of the two lists.
-    private static int calculateTotalDistance(IdLists idLists)
-    {
+    private static int calculateTotalDistance(IdLists idLists) {
         Collections.sort(idLists.firstList());
         Collections.sort(idLists.secondList());
-        
-        int totalDistance = 0;
-        
-        for (int index = 0; index < idLists.firstList().size(); index++)
-        {
-            int firstNumber = idLists.firstList().get(index);
-            int secondNumber = idLists.secondList().get(index);
-            
-            int distance = Math.abs(firstNumber - secondNumber);
-            totalDistance += distance;
+
+        var totalDistance = 0;
+
+        for (var index = 0; index < idLists.firstList().size(); index++) {
+            final var first = idLists.firstList().get(index);
+            final var second = idLists.secondList().get(index);
+            totalDistance += Math.abs(first - second);
         }
-        
+
         return totalDistance;
     }
-    
+
     /// Calculates the similarity score between the two lists of ids.
     ///
     /// Time Complexity: O(n)
@@ -114,15 +105,14 @@ public class Solution implements PuzzleSolver
     /// Space Complexity: O(n)
     /// - The entire puzzle input is stored.
     ///
-    /// @param inputLines the puzzle input.
+    /// @param puzzleInput the puzzle input.
     /// @return the similarity score between the two lists of ids.
     @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        IdLists idLists = getIdLists(inputLines);
+    public Object partTwo(List<String> puzzleInput) {
+        final var idLists = parse(puzzleInput);
         return calculateSimilarityScore(idLists);
     }
-    
+
     /// Calculates the similarity score between the two lists.
     ///
     /// To calculate the similarity score between the two lists, the ids in one list are
@@ -134,35 +124,35 @@ public class Solution implements PuzzleSolver
     ///
     /// @param idLists the lists of ids.
     /// @return the similarity score between the two lists.
-    private static int calculateSimilarityScore(IdLists idLists)
-    {
-        Map<Integer, Integer> secondIdListFrequencies = calculateFrequencies(idLists.secondList);
-        
-        int similarityScore = 0;
-        
-        for (int id : idLists.firstList)
-        {
-            int frequency = secondIdListFrequencies.getOrDefault(id, 0);
+    private static int calculateSimilarityScore(IdLists idLists) {
+        final var secondIdListFrequencies = calculateFrequencies(idLists.secondList);
+
+        var similarityScore = 0;
+
+        for (final var id : idLists.firstList) {
+            final var frequency = secondIdListFrequencies.getOrDefault(id, 0);
             similarityScore += id * frequency;
         }
-        
+
         return similarityScore;
     }
-    
+
     /// Calculates a frequency map of the ids in the list.
     ///
     /// @param idList a list of ids.
     /// @return a frequency map of the ids in the list.
-    private static Map<Integer, Integer> calculateFrequencies(List<Integer> idList)
-    {
-        Map<Integer, Integer> idFrequencies = new HashMap<>();
-        
-        for (int id : idList)
-        {
-            int frequency = idFrequencies.getOrDefault(id, 0) + 1;
+    private static Map<Integer, Integer> calculateFrequencies(List<Integer> idList) {
+        final var idFrequencies = new HashMap<Integer, Integer>();
+
+        for (final var id : idList) {
+            final var frequency = idFrequencies.getOrDefault(id, 0) + 1;
             idFrequencies.put(id, frequency);
         }
-        
+
         return idFrequencies;
+    }
+
+    public static void main(String[] args) {
+        PuzzleRunner.runAndPrint(2024, 1);
     }
 }
