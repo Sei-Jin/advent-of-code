4f6e0485e587ffb0d52c31b2e063188c7cefb951
Sei-Jin <seijin.tufts@gmail.com>
2025-02-14 21:38:55 -0400

    [2024-1] Refactor solution
    Also remembered to close the BufferedReader.

--- src/main/java/aoc/event/year2024/day01/historianHysteria/Solution.java
+++ src/main/java/aoc/event/year2024/day01/historianHysteria/Solution.java
@@ -4,6 +4,7 @@ import aoc.Runner;
 import aoc.Solver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -13,13 +14,14 @@ public class Solution implements Solver {
     /// Pattern for each line of the puzzle input
     private static final Pattern LINE_PATTERN = Pattern.compile("(\\d+)\\s+(\\d+)");
 
+    /// The id lists parsed from the puzzle input.
     private static IdLists ID_LISTS;
 
-    /// This record class stores the data for the two lists of ids.
+    /// Stores the data for the two lists of ids.
     ///
-    /// @param firstList  the first list of ids.
-    /// @param secondList the second list of ids.
-    private record IdLists(List<Integer> firstList, List<Integer> secondList) {}
+    /// @param first  the first list of ids.
+    /// @param second the second list of ids.
+    private record IdLists(List<Integer> first, List<Integer> second) {}
 
     /// Parses the puzzle input for the ids in each list.
     ///
@@ -43,16 +45,16 @@ public class Solution implements Solver {
     /// @param reader the puzzle input.
     @Override
     public void parse(BufferedReader reader) {
-        final var firstList = new ArrayList<Integer>();
-        final var secondList = new ArrayList<Integer>();
+        final var first = new ArrayList<Integer>();
+        final var second = new ArrayList<Integer>();
 
         reader.lines()
                 .forEach(line -> {
                     final var matcher = LINE_PATTERN.matcher(line);
 
                     if (matcher.find()) {
-                        firstList.add(Integer.valueOf(matcher.group(1)));
-                        secondList.add(Integer.valueOf(matcher.group(2)));
+                        first.add(Integer.valueOf(matcher.group(1)));
+                        second.add(Integer.valueOf(matcher.group(2)));
                     } else {
                         throw new IllegalArgumentException(
                                 String.format("Unexpected format in input: %s", line)
@@ -60,7 +62,13 @@ public class Solution implements Solver {
                     }
                 });
 
-        ID_LISTS = new IdLists(firstList, secondList);
+        try {
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to close reader" + e);
+        }
+
+        ID_LISTS = new IdLists(first, second);
     }
 
     /// Calculates the total distance between the ids of the two lists.
@@ -77,14 +85,14 @@ public class Solution implements Solver {
     /// @return the total distance between the two lists of ids.
     @Override
     public Object partOne() {
-        Collections.sort(ID_LISTS.firstList());
-        Collections.sort(ID_LISTS.secondList());
+        Collections.sort(ID_LISTS.first());
+        Collections.sort(ID_LISTS.second());
 
         var totalDistance = 0;
 
-        for (var index = 0; index < ID_LISTS.firstList().size(); index++) {
-            final var first = ID_LISTS.firstList().get(index);
-            final var second = ID_LISTS.secondList().get(index);
+        for (var index = 0; index < ID_LISTS.first().size(); index++) {
+            final var first = ID_LISTS.first().get(index);
+            final var second = ID_LISTS.second().get(index);
             totalDistance += Math.abs(first - second);
         }
 
@@ -109,31 +117,31 @@ public class Solution implements Solver {
     /// @return the similarity score between the two lists of ids.
     @Override
     public Object partTwo() {
-        final var secondIdListFrequencies = calculateFrequencies(ID_LISTS.secondList);
+        final var frequencies = calculateFrequencies(ID_LISTS.second);
 
         var similarityScore = 0;
 
-        for (final var id : ID_LISTS.firstList) {
-            final var frequency = secondIdListFrequencies.getOrDefault(id, 0);
+        for (final var id : ID_LISTS.first) {
+            final var frequency = frequencies.getOrDefault(id, 0);
             similarityScore += id * frequency;
         }
 
         return similarityScore;
     }
 
-    /// Calculates a frequency map of the ids in the list.
+    /// Calculates a frequency map of the numbers in the list.
     ///
-    /// @param idList a list of ids.
-    /// @return a frequency map of the ids in the list.
-    private static Map<Integer, Integer> calculateFrequencies(List<Integer> idList) {
-        final var idFrequencies = new HashMap<Integer, Integer>();
+    /// @param list a list.
+    /// @return a frequency map of the numbers in the list.
+    private static Map<Integer, Integer> calculateFrequencies(List<Integer> list) {
+        final var frequencies = new HashMap<Integer, Integer>();
 
-        for (final var id : idList) {
-            final var frequency = idFrequencies.getOrDefault(id, 0) + 1;
-            idFrequencies.put(id, frequency);
+        for (final var number : list) {
+            final var frequency = frequencies.getOrDefault(number, 0) + 1;
+            frequencies.put(number, frequency);
         }
 
-        return idFrequencies;
+        return frequencies;
     }
 
     public static void main(String[] args) {
