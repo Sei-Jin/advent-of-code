6b6d541930bc6c3086d75f81eaf1cec454e9b2bf
Sei-Jin <seijin.tufts@gmail.com>
2025-02-19 18:58:27 -0400

    [2017-2] Update solution
    Solution now uses a constructor to pass in the input.

--- src/main/java/aoc/event/year2017/day02/corruptionChecksum/Solution.java
+++ src/main/java/aoc/event/year2017/day02/corruptionChecksum/Solution.java
@@ -4,42 +4,74 @@ import aoc.Runner;
 import aoc.Solver;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
-/// --- Day 2: Corruption Checksum ---
 public class Solution implements Solver {
     
-    private static final List<List<Integer>> INPUT = new ArrayList<>();
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("(\\d+)");
+    /// Compiles a pattern that matches integers.
+    private static final Pattern PATTERN = Pattern.compile("(\\d+)");
     
-    @Override
-    public void parse(String input) {
-        input.lines()
-                .forEach(line -> {
-                    final var numbers = NUMBER_PATTERN.matcher(line)
-                            .results()
-                            .mapToInt(match -> Integer.parseInt(match.group()))
-                            .boxed()
-                            .toList();
-                    
-                    INPUT.add(numbers);
-                });
+    /// Stores the puzzle input as a list of lists of numbers.
+    private final List<List<Integer>> numberLists;
+    
+    /// Initializes the solution.
+    ///
+    /// @param input the puzzle input.
+    public Solution(String input) {
+        this.numberLists = Collections.unmodifiableList(parse(input));
     }
     
-    /// @return the checksum for the spreadsheet.
+    /// Parses the solution input for a list of number lists.
+    ///
+    /// Each line of the puzzle input is in the form: `###### ...`. Where each `#`
+    /// represents an integer.
+    ///
+    /// @param input the puzzle input.
+    /// @return a list of number lists.
+    private List<List<Integer>> parse(String input) {
+        final var numberLists = new ArrayList<List<Integer>>();
+        
+        input.lines().forEach(line -> {
+            var numberList = PATTERN.matcher(line)
+                    .results()
+                    .mapToInt(match -> Integer.parseInt(match.group()))
+                    .boxed()
+                    .toList();
+            
+            numberLists.add(numberList);
+        });
+        
+        return numberLists;
+    }
+    
+    /// Calculates the sum of the maximum difference for all rows.
+    ///
+    /// @return the sum of the maximum differences for all rows.
     @Override
     public Integer partOne() {
-        return INPUT.stream()
-                .mapToInt(Solution::findDifference)
-                .sum();
+        var sum = 0;
+        
+        for (final var list : numberLists) {
+            sum += findMaximumDifference(list);
+        }
+        
+        return sum;
     }
     
-    private static int findDifference(List<Integer> numbers) {
-        var min = numbers.getFirst();
-        var max = numbers.getFirst();
+    /// Calculates the maximum difference between any two numbers in the given list.
+    ///
+    /// The maximum difference is the difference between the smallest and largest numbers in the
+    /// list.
+    ///
+    /// @param list a list of numbers.
+    /// @return the maximum difference between any two numbers in the list.
+    private static int findMaximumDifference(List<Integer> list) {
+        var min = list.getFirst();
+        var max = list.getFirst();
         
-        for (final var number : numbers) {
+        for (final var number : list) {
             if (min > number) {
                 min = number;
             }
@@ -52,25 +84,37 @@ public class Solution implements Solver {
         return max - min;
     }
     
-    /// @return the sum of each row's divisible result.
+    /// Calculates the total of the divisible quotients from all rows.
+    ///
+    /// @return the sum of the of divisible quotients.
     @Override
     public Integer partTwo() {
-        return INPUT.stream()
-                .mapToInt(Solution::findDivisible)
-                .sum();
+        var sum = 0;
+        
+        for (final var list : numberLists) {
+            sum += findDivisibleQuotient(list);
+        }
+        
+        return sum;
     }
     
-    private static int findDivisible(List<Integer> numbers) {
-        for (int outer = 0; outer < numbers.size(); outer++) {
-            for (int inner = 0; inner < numbers.size(); inner++) {
-                if (outer == inner) {
+    /// Finds the only two numbers in the list that are divisible, and returns their quotient.
+    ///
+    /// Numbers are divisible if and only if they divide evenly without leaving a remainder.
+    ///
+    /// @param list a list of numbers.
+    /// @return the quotient of the only two divisible list.
+    private static int findDivisibleQuotient(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (i == j) {
                     continue;
                 }
                 
-                boolean isDivisible = ((numbers.get(outer) % numbers.get(inner)) == 0);
+                boolean isDivisible = ((list.get(i) % list.get(j)) == 0);
                 
                 if (isDivisible) {
-                    return numbers.get(outer) / numbers.get(inner);
+                    return list.get(i) / list.get(j);
                 }
             }
         }
--- src/test/java/aoc/event/year2017/day02/corruptionChecksum/SolutionTests.java
+++ src/test/java/aoc/event/year2017/day02/corruptionChecksum/SolutionTests.java
@@ -3,90 +3,77 @@ package aoc.event.year2017.day02.corruptionChecksum;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SolutionTests
-{
+public class SolutionTests {
+    
     @Nested
-    class PartOne
-    {
+    class PartOne {
         @Test
-        void exampleSpreadsheetFirstRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("5 1 9 5");
-            assertEquals(8, new Solution().partOne(inputLines));
+        void exampleSpreadsheetFirstRow() {
+            final var input = "5 1 9 5";
+            final var solution = new Solution(input);
+            assertEquals(8, solution.partOne());
         }
         
         @Test
-        void exampleSpreadsheetSecondRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("7 5 3  ");
-            assertEquals(4, new Solution().partOne(inputLines));
+        void exampleSpreadsheetSecondRow() {
+            final var input = "7 5 3  ";
+            final var solution = new Solution(input);
+            assertEquals(4, solution.partOne());
         }
         
         @Test
-        void exampleSpreadsheetThirdRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("2 4 6 8");
-            assertEquals(6, new Solution().partOne(inputLines));
+        void exampleSpreadsheetThirdRow() {
+            final var input = "2 4 6 8";
+            final var solution = new Solution(input);
+            assertEquals(6, solution.partOne());
         }
         
         @Test
-        void exampleSpreadsheet()
-        {
-            List<String> inputLines = new ArrayList<>(Arrays.asList(
-                    "5 1 9 5",
-                    "7 5 3  ",
-                    "2 4 6 8"
-            ));
-            assertEquals(18, new Solution().partOne(inputLines));
+        void exampleSpreadsheet() {
+            final var input = """
+                    5 1 9 5
+                    7 5 3
+                    2 4 6 8
+                    """;
+            final var solution = new Solution(input);
+            assertEquals(18, solution.partOne());
         }
     }
     
-    
     @Nested
-    class PartTwo
-    {
+    class PartTwo {
         @Test
-        void exampleSpreadsheetFirstRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("5 9 2 8");
-            assertEquals(4, new Solution().partTwo(inputLines));
+        void exampleSpreadsheetFirstRow() {
+            final var input = "5 9 2 8";
+            final var solution = new Solution(input);
+            assertEquals(4, solution.partTwo());
         }
         
         @Test
-        void exampleSpreadsheetSecondRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("9 4 7 3");
-            assertEquals(3, new Solution().partTwo(inputLines));
+        void exampleSpreadsheetSecondRow() {
+            final var input = "9 4 7 3";
+            final var solution = new Solution(input);
+            assertEquals(3, solution.partTwo());
         }
         
         @Test
-        void exampleSpreadsheetThirdRow()
-        {
-            List<String> inputLines = new ArrayList<>();
-            inputLines.add("3 8 6 5");
-            assertEquals(2, new Solution().partTwo(inputLines));
+        void exampleSpreadsheetThirdRow() {
+            final var input = "3 8 6 5";
+            final var solution = new Solution(input);
+            assertEquals(2, solution.partTwo());
         }
         
         @Test
-        void exampleSpreadsheet()
-        {
-            List<String> inputLines = new ArrayList<>(Arrays.asList(
-                    "5 9 2 8",
-                    "9 4 7 3",
-                    "3 8 6 5"
-            ));
-            assertEquals(9, new Solution().partTwo(inputLines));
+        void exampleSpreadsheet() {
+            final var input = """
+                    5 9 2 8
+                    9 4 7 3
+                    3 8 6 5
+                    """;
+            final var solution = new Solution(input);
+            assertEquals(9, solution.partTwo());
         }
     }
 }
