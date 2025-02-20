6871379325bb79ce700c8c5b8b8e61e94babcebd
Sei-Jin <seijin.tufts@gmail.com>
2025-02-20 19:52:21 -0400

    [2022-2] Update solution to use constructor

--- src/main/java/aoc/event/year2022/day02/rockPaperScissors/Solution.java
+++ src/main/java/aoc/event/year2022/day02/rockPaperScissors/Solution.java
@@ -1,33 +1,69 @@
 package aoc.event.year2022.day02.rockPaperScissors;
 
-import aoc.DeprecatedSolver;
+import aoc.Runner;
+import aoc.Solver;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Solution implements DeprecatedSolver
-{
-    /**
-     * @param inputLines the puzzle input.
-     * @return the total score if everything goes exactly according to the strategy guide.
-     */
+public class Solution implements Solver {
+    
+    private final List<Draw> draws;
+    
+    public Solution(String input) {
+        draws = parse(input);
+    }
+    
+    private static List<Draw> parse(String input) {
+        final var draws = new ArrayList<Draw>();
+        
+        input.lines().forEach(line -> {
+            final var values =  line.split(" ");
+            
+            final var first = values[0].charAt(0);
+            final var second = values[1].charAt(0);
+            
+            draws.add(new Draw(first, second));
+        });
+        
+        return draws;
+    }
+    
+    /// @return the total score if everything goes exactly according to the strategy guide.
     @Override
-    public Object partOne(List<String> inputLines)
-    {
-        int totalScore = 0;
+    public Object partOne() {
+        var totalScore = 0;
         
-        HashMap<Character, Choice> opponentsPossibleChoices = getOpponentsPossibleChoices();
-        HashMap<Character, Choice> ourPossibleChoices = getOurPossibleChoices();
+        final var opponentsPossibleChoices = getOpponentsPossibleChoices();
+        final var ourPossibleChoices = getOurPossibleChoices();
         
-        for (String line : inputLines)
-        {
-            List<Character> choices = getChoices(line);
+        for (final var draw : draws) {
+            final var opponentsChoice = opponentsPossibleChoices.get(draw.first);
+            final var ourChoice = ourPossibleChoices.get(draw.second);
             
-            Choice opponentsChoice = opponentsPossibleChoices.get(choices.getFirst());
-            Choice ourChoice = ourPossibleChoices.get(choices.getLast());
+            final var outcome = determineOutcome(opponentsChoice, ourChoice);
+            totalScore += outcome.score + ourChoice.score;
+        }
+        
+        return totalScore;
+    }
+    
+    /// @return the total score if everything goes exactly according to the strategy guide with the
+    /// Elf's updated instructions.
+    @Override
+    public Object partTwo() {
+        var totalScore = 0;
+        
+        final var opponentsPossibleChoices = getOpponentsPossibleChoices();
+        final var possibleOutcomes = getPossibleOutcomes();
+        
+        for (final var draw : draws) {
+            final var opponentsChoice = opponentsPossibleChoices.get(draw.first);
+            final var outcome = possibleOutcomes.get(draw.second);
             
-            Outcome outcome = determineOutcome(opponentsChoice, ourChoice);
+            final var ourChoice = determineChoice(opponentsChoice, outcome);
             
             totalScore += outcome.score + ourChoice.score;
         }
@@ -35,85 +71,32 @@ public class Solution implements DeprecatedSolver
         return totalScore;
     }
     
-    
-    /**
-     * @param inputLines the puzzle input.
-     * @return the total score if everything goes exactly according to the strategy guide with the
-     * Elf's updated instructions.
-     */
-    @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        int totalScore = 0;
-        
-        HashMap<Character, Choice> opponentsPossibleChoices = getOpponentsPossibleChoices();
-        HashMap<Character, Outcome> possibleOutcomes = getPossibleOutcomes();
-        
-        for (String line : inputLines)
-        {
-            List<Character> choices = getChoices(line);
-            
-            Choice opponentsChoice = opponentsPossibleChoices.get(choices.getFirst());
-            Outcome outcome = possibleOutcomes.get(choices.getLast());
-            
-            Choice ourChoice = determineChoice(opponentsChoice, outcome);
-            
-            totalScore += outcome.score + ourChoice.score;
-        }
-        
-        return totalScore;
-    }
-    
-    
-    private static List<Character> getChoices(String line)
-    {
-        return Arrays.stream((line.split(" ")))
-                .map(s -> s.charAt(0))
-                .toList();
-    }
-    
-    
-    public static Outcome determineOutcome(Choice opponentsChoice, Choice ourChoice)
-    {
+    public static Outcome determineOutcome(Choice opponentsChoice, Choice ourChoice) {
         Outcome outcome;
         
         if (opponentsChoice == Choice.ROCK && ourChoice == Choice.PAPER
                 || opponentsChoice == Choice.PAPER && ourChoice == Choice.SCISSORS
-                || opponentsChoice == Choice.SCISSORS && ourChoice == Choice.ROCK)
-        {
+                || opponentsChoice == Choice.SCISSORS && ourChoice == Choice.ROCK) {
             outcome = Outcome.WIN;
-        }
-        else if (opponentsChoice == ourChoice)
-        {
+        } else if (opponentsChoice == ourChoice) {
             outcome = Outcome.DRAW;
-        }
-        else
-        {
+        } else {
             outcome = Outcome.LOSS;
         }
         
         return outcome;
     }
     
-    
-    public static Choice determineChoice(Choice opponentsChoice, Outcome outcome)
-    {
-        Choice choice = null;
-        
-        switch (outcome)
-        {
-            case WIN -> choice = opponentsChoice.losesAgainst();
-            case DRAW -> choice = opponentsChoice;
-            case LOSS -> choice = opponentsChoice.winsAgainst();
-        }
-        
-        return choice;
+    public static Choice determineChoice(Choice opponentsChoice, Outcome outcome) {
+        return switch (outcome) {
+            case WIN -> opponentsChoice.losesAgainst();
+            case DRAW -> opponentsChoice;
+            case LOSS -> opponentsChoice.winsAgainst();
+        };
     }
     
-    
-    private static HashMap<Character, Choice> getOpponentsPossibleChoices()
-    {
-        HashMap<Character, Choice> opponentsPossibleChoices = new HashMap<>();
+    private static HashMap<Character, Choice> getOpponentsPossibleChoices() {
+        final var opponentsPossibleChoices = new HashMap<Character, Choice>();
         
         opponentsPossibleChoices.put('A', Choice.ROCK);
         opponentsPossibleChoices.put('B', Choice.PAPER);
@@ -122,10 +105,8 @@ public class Solution implements DeprecatedSolver
         return opponentsPossibleChoices;
     }
     
-    
-    private static HashMap<Character, Choice> getOurPossibleChoices()
-    {
-        HashMap<Character, Choice> ourPossibleChoices = new HashMap<>();
+    private static HashMap<Character, Choice> getOurPossibleChoices() {
+        final var ourPossibleChoices = new HashMap<Character, Choice>();
         
         ourPossibleChoices.put('X', Choice.ROCK);
         ourPossibleChoices.put('Y', Choice.PAPER);
@@ -134,10 +115,8 @@ public class Solution implements DeprecatedSolver
         return ourPossibleChoices;
     }
     
-    
-    private static HashMap<Character, Outcome> getPossibleOutcomes()
-    {
-        HashMap<Character, Outcome> possibleOutcomes = new HashMap<>();
+    private static Map<Character, Outcome> getPossibleOutcomes() {
+        final var possibleOutcomes = new HashMap<Character, Outcome>();
         
         possibleOutcomes.put('X', Outcome.LOSS);
         possibleOutcomes.put('Y', Outcome.DRAW);
@@ -146,43 +125,41 @@ public class Solution implements DeprecatedSolver
         return possibleOutcomes;
     }
     
-    
-    public enum Choice
-    {
+    public enum Choice {
         ROCK(1),
         PAPER(2),
         SCISSORS(3);
         
         final int score;
         
-        Choice(int score)
-        {
+        Choice(int score) {
             this.score = score;
         }
         
-        public Choice losesAgainst()
-        {
+        public Choice losesAgainst() {
             return values()[(this.ordinal() + 1) % (values().length)];
         }
         
-        public Choice winsAgainst()
-        {
+        public Choice winsAgainst() {
             return values()[(values().length + this.ordinal() - 1) % values().length];
         }
     }
     
-    
-    public enum Outcome
-    {
+    public enum Outcome {
         WIN(6),
         LOSS(0),
         DRAW(3);
         
         final int score;
         
-        Outcome(int score)
-        {
+        Outcome(int score) {
             this.score = score;
         }
+    }
+    
+    private record Draw(char first, char second) {}
+    
+    public static void main(String[] args) {
+        Runner.runAndPrint(2022, 2);
     }
 }
