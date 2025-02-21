de7e49e2e383bb76c2dd6474eb250b1a0b4318d7
Sei-Jin <seijin.tufts@gmail.com>
2025-02-20 22:15:09 -0400

    [2022-2] Add tests

--- src/test/java/aoc/event/year2022/day02/rockPaperScissors/SolutionTests.java
+++ src/test/java/aoc/event/year2022/day02/rockPaperScissors/SolutionTests.java
@@ -1,6 +1,20 @@
 package aoc.event.year2022.day02.rockPaperScissors;
 
-public class SolutionTests
-{
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTests {
+    
+    @Test
+    void example() {
+        final var input = """
+                A Y
+                B X
+                C Z
+                """;
+        final var solution = new Solution(input);
+        assertEquals(15, solution.partOne());
+        assertEquals(12, solution.partTwo());
+    }
 }
