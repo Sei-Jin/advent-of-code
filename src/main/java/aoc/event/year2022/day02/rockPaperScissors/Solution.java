186f41bfd789fd93f153cd4472926817437dbfb8
Sei-Jin <seijin.tufts@gmail.com>
2025-02-20 22:46:51 -0400

    [2022-2] Improve comments
    Comments are now only made on the public methods/constructor.

--- src/main/java/aoc/event/year2022/day02/rockPaperScissors/Solution.java
+++ src/main/java/aoc/event/year2022/day02/rockPaperScissors/Solution.java
@@ -9,20 +9,18 @@ import java.util.List;
 
 public class Solution implements Solver {
     
-    /// Stores the data for the rounds.
     private final List<Round> rounds;
     
-    /// Initializes the solution.
+    /// Initializes the solution with the parsed round data.
+    ///
+    /// Each line of the puzzle input is in the form `A B`, where `A` and `B` are characters that
+    /// represent the data for a round.
+    ///
+    /// @param input the puzzle input.
     public Solution(String input) {
         rounds = Collections.unmodifiableList(parse(input));
     }
     
-    /// Parses the input for the round data.
-    ///
-    /// Each line of the puzzle input is in the form `A B`, where `A` and `B` are characters.
-    ///
-    /// @param input the puzzle input.
-    /// @return the data for the rounds.
     private static List<Round> parse(String input) {
         final var draws = new ArrayList<Round>();
         
@@ -38,10 +36,6 @@ public class Solution implements Solver {
         return draws;
     }
     
-    /// Determines which of the opponents choices the given character maps to.
-    ///
-    /// @param character a character representing the opponents choice.
-    /// @return the opponents choice.
     private static Choice determineOpponentsChoice(char character) {
         return switch (character) {
             case 'A' -> Choice.ROCK;
@@ -53,6 +47,10 @@ public class Solution implements Solver {
     
     /// Calculates the total score from all rounds played when given our opponents choices and
     /// our own choices.
+    ///
+    /// Given our choice and the opponents choice we can calculate the outcome for each round.
+    /// The score of a round is calculated by adding up the score values for the outcome and our
+    /// choice.
     ///
     /// @return the total score.
     @Override
@@ -71,10 +69,6 @@ public class Solution implements Solver {
         return totalScore;
     }
     
-    /// Determines which choice the given character maps to.
-    ///
-    /// @param character a character.
-    /// @return our choice.
     private static Choice determineOurChoice(char character) {
         return switch (character) {
             case 'X' -> Choice.ROCK;
@@ -84,11 +78,6 @@ public class Solution implements Solver {
         };
     }
     
-    /// Determines the outcome of the round.
-    ///
-    /// @param opponentsChoice the opponents choice.
-    /// @param ourChoice our choice.
-    /// @return the outcome of the round.
     private static Outcome determineOutcome(Choice opponentsChoice, Choice ourChoice) {
         if (opponentsChoice.losesAgainst() == ourChoice) {
             return Outcome.WIN;
@@ -99,8 +88,12 @@ public class Solution implements Solver {
         }
     }
     
-    /// Calculates the total score from all rounds played when given our opponents choices and the
-    /// round outcomes.
+    /// Calculates the total score from all rounds played when given our opponents choices and
+    /// the round outcomes.
+    ///
+    /// Given our opponents choice and the outcome of the round we can determine which choice we
+    /// should make. The score of a round is calculated by adding up the score values for the
+    /// outcome and our choice.
     ///
     /// @return the total score.
     @Override
@@ -119,10 +112,6 @@ public class Solution implements Solver {
         return totalScore;
     }
     
-    /// Determines the outcome that maps to the given character.
-    ///
-    /// @param character a character.
-    /// @return the outcome.
     private static Outcome determineOutcome(char character) {
         return switch (character) {
             case 'X' -> Outcome.LOSS;
@@ -132,12 +121,6 @@ public class Solution implements Solver {
         };
     }
     
-    /// Determines the choice we should make when knowing our opponents choice and the round
-    /// outcome.
-    ///
-    /// @param opponents the choice our opponent will make.
-    /// @param outcome   the outcome of the round.
-    /// @return the choice we should make to ensure the correct round outcome.
     private static Choice determineOurChoice(Choice opponents, Outcome outcome) {
         return switch (outcome) {
             case WIN -> opponents.losesAgainst();
@@ -146,7 +129,6 @@ public class Solution implements Solver {
         };
     }
     
-    /// Stores the possible choices for a player with their score.
     private enum Choice {
         ROCK(1),
         PAPER(2),
@@ -158,7 +140,6 @@ public class Solution implements Solver {
             this.score = score;
         }
         
-        /// @return the choice that loses against the current choice.
         private Choice losesAgainst() {
             return switch (this) {
                 case ROCK -> PAPER;
@@ -167,7 +148,6 @@ public class Solution implements Solver {
             };
         }
         
-        /// @return the choice that wins against the current choice.
         private Choice winsAgainst() {
             return switch (this) {
                 case ROCK -> SCISSORS;
@@ -177,7 +157,6 @@ public class Solution implements Solver {
         }
     }
     
-    /// Stores the possible outcomes of a round with their score values.
     private enum Outcome {
         WIN(6),
         LOSS(0),
@@ -190,7 +169,6 @@ public class Solution implements Solver {
         }
     }
     
-    /// Stores the data for a round.
     private record Round(char first, char second) {}
     
     /// Runs the solution.
