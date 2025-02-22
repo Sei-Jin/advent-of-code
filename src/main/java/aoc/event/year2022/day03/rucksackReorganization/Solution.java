0ac208e329eace605a6898db9fa41999cce2cf1c
Sei-Jin <seijin.tufts@gmail.com>
2025-02-22 12:24:12 -0400

    [2022-3] Update solution to use constructor
    Solution logic was also improved and simplified.

--- src/main/java/aoc/event/year2022/day03/rucksackReorganization/Solution.java
+++ src/main/java/aoc/event/year2022/day03/rucksackReorganization/Solution.java
@@ -1,121 +1,95 @@
 package aoc.event.year2022.day03.rucksackReorganization;
 
-import aoc.DeprecatedSolver;
+import aoc.Runner;
+import aoc.Solver;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-/**
- * --- Day 3: Rucksack Reorganization ---
- */
-public class Solution implements DeprecatedSolver
-{
-    /**
-     * @param inputLines the puzzle input.
-     * @return the sum of the priorities.
-     */
-    @Override
-    public Object partOne(List<String> inputLines)
-    {
-        int totalItemPriority = 0;
-        
-        for (String line : inputLines)
-        {
-            int midPoint = line.length() / 2;
-            
-            Set<Character> firstHalf = line.substring(0, midPoint)
-                    .chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toSet());
-            
-            Set<Character> secondHalf = line.substring(midPoint)
-                    .chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toSet());
-            
-            char sharedItem = findSharedItem(firstHalf, secondHalf).getFirst();
-            
-            int itemPriority = calculateItemPriority(sharedItem);
-            
-            totalItemPriority += itemPriority;
-        }
-        
-        return totalItemPriority;
+public class Solution implements Solver {
+    
+    private final List<List<Character>> lines;
+    
+    /// Initializes the solution with the parsed data
+    ///
+    /// Each line of the puzzle input is in the form `cccccccc...`, where each `c` an
+    /// alphabetical character from `a-z` that may or may not be capitalized.
+    ///
+    /// @param input the puzzle input.
+    public Solution(String input) {
+        lines = Collections.unmodifiableList(parse(input));
     }
     
-    
-    private static List<Character> findSharedItem(Set<Character> firstSet, Set<Character> secondSet)
-    {
-        firstSet.retainAll(secondSet);
-        return firstSet.stream().toList();
+    private static List<List<Character>> parse(String input) {
+        return input.lines()
+                .map(line -> line.chars()
+                        .mapToObj(c -> (char) c)
+                        .toList())
+                .toList();
     }
     
-    
-    private static int calculateItemPriority(char sharedItem)
-    {
-        int itemPriority = 0;
+    /// Maps lowercase characters to the values 1 to 26 and uppercase characters to the values 27
+    /// to 52.
+    private static int calculateItemPriority(char sharedItem) {
+        var itemPriority = 0;
         
-        if (sharedItem >= 'a' && sharedItem <= 'z')
-        {
+        if (sharedItem >= 'a' && sharedItem <= 'z') {
             itemPriority = sharedItem - 96;
-        }
-        else if (sharedItem >= 'A' && sharedItem <= 'Z')
-        {
+        } else if (sharedItem >= 'A' && sharedItem <= 'Z') {
             itemPriority = sharedItem - 38;
         }
         
         return itemPriority;
     }
     
-    
-    /**
-     * @param inputLines the puzzle input.
-     * @return the sum of the priorities of the badges.
-     */
-    @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        int totalItemPriority = 0;
+    /// Calculates the sum of the priority values for the shared characters.
+    ///
+    /// Each line contains one shared character between the first and second half
+    ///
+    /// @return the sum of the priority values for the shared characters.
+    public Integer partOne() {
+        var totalItemPriority = 0;
         
-        List<Set<Character>> group = new ArrayList<>();
-        
-        for (String line : inputLines)
-        {
-            Set<Character> rucksack = line.chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toSet());
+        for (final var line : lines) {
+            final var midPoint = line.size() / 2;
             
-            group.add(rucksack);
+            final var firstHalf = new HashSet<>(line.subList(0, midPoint));
+            final var secondHalf = new HashSet<>(line.subList(midPoint, line.size()));
             
-            if (group.size() < 3)
-            {
-                continue;
-            }
+            firstHalf.retainAll(secondHalf);
             
-            char sharedItem = findBadge(group).getFirst();
-            
-            int itemPriority = calculateItemPriority(sharedItem);
-            
-            totalItemPriority += itemPriority;
-            
-            group.clear();
+            final var sharedItem = firstHalf.stream().findFirst().orElseThrow();
+            totalItemPriority += calculateItemPriority(sharedItem);
         }
         
         return totalItemPriority;
     }
     
+    /// Calculates the sum of the priority values for the shared characters.
+    ///
+    /// Each three-line group contains one shared character.
+    ///
+    /// @return the sum of the priority values for the shared characters.
+    public Integer partTwo() {
+        var totalItemPriority = 0;
+        
+        for (var i = 0; i < lines.size(); i += 3) {
+            final var first = new HashSet<>(lines.get(i));
+            final var second = new HashSet<>(lines.get(i + 1));
+            final var third = new HashSet<>(lines.get(i + 2));
+            
+            first.retainAll(second);
+            first.retainAll(third);
+            
+            final var sharedItem = first.stream().findFirst().orElseThrow();
+            totalItemPriority += calculateItemPriority(sharedItem);
+        }
+        
+        return totalItemPriority;
+    }
     
-    private static List<Character> findBadge(List<Set<Character>> group)
-    {
-        Set<Character> firstGroup = group.getFirst();
-        Set<Character> secondGroup = group.get(1);
-        Set<Character> thirdGroup = group.get(2);
-        
-        firstGroup.retainAll(secondGroup);
-        firstGroup.retainAll(thirdGroup);
-        
-        return firstGroup.stream().toList();
+    public static void main(String[] args) {
+        Runner.runAndPrint(2022, 3);
     }
 }
--- src/test/java/aoc/event/year2022/day03/rucksackReorganization/SolutionTests.java
+++ src/test/java/aoc/event/year2022/day03/rucksackReorganization/SolutionTests.java
@@ -1,6 +1,23 @@
 package aoc.event.year2022.day03.rucksackReorganization;
 
-public class SolutionTests
-{
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTests {
+    
+    @Test
+    void example() {
+        final var input = """
+                vJrwpWtwJgWrhcsFMMfFFhFp
+                jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                PmmdzqPrVvPwwTWBwg
+                wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                ttgJtRGJQctTZtZT
+                CrZsJsPPZsGzwwsLwLmpwMDw
+                """;
+        final var solution = new Solution(input);
+        assertEquals(157, solution.partOne());
+        assertEquals(70, solution.partTwo());
+    }
 }
