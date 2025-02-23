0af48fd8a888e14802bbc6b63c93170785f3b939
Sei-Jin <seijin.tufts@gmail.com>
2025-02-23 17:58:42 -0400

    [2022-6] Reformat solution

--- src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
+++ src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
@@ -1,14 +1,13 @@
 package aoc.event.year2022.day06.tuningTrouble;
 
-import aoc.Runner;
 import aoc.DeprecatedSolver;
+import aoc.Runner;
 
 import java.util.List;
 
-public class Solution implements DeprecatedSolver
-{
-    public static void main(String[] args)
-    {
+public class Solution implements DeprecatedSolver {
+    
+    public static void main(String[] args) {
         Runner.runAndPrint(2022, 6);
     }
     
@@ -17,18 +16,15 @@ public class Solution implements DeprecatedSolver
     /// The position of the first marker is calculated by finding the first substring of distinct
     /// characters with length `windowSize`.
     ///
-    /// @param line finds the position of the first marker.
+    /// @param line       finds the position of the first marker.
     /// @param windowSize the length of distinct characters.
     /// @return the position of the first marker.
-    private static int findFirstMarker(String line, int windowSize)
-    {
-        for (int i = 0; i < line.length(); i++)
-        {
+    private static int findFirstMarker(String line, int windowSize) {
+        for (int i = 0; i < line.length(); i++) {
             String substring = line.substring(i, i + windowSize);
             boolean containsDuplicate = containsDuplicate(substring);
             
-            if (!containsDuplicate)
-            {
+            if (!containsDuplicate) {
                 return i + windowSize;
             }
         }
@@ -40,14 +36,10 @@ public class Solution implements DeprecatedSolver
     ///
     /// @param string a string.
     /// @return true if the string contains a duplicate character, or false otherwise.
-    private static boolean containsDuplicate(String string)
-    {
-        for (int i = 0; i < string.length() - 1; i++)
-        {
-            for (int j = i + 1; j < string.length(); j++)
-            {
-                if (string.charAt(i) == string.charAt(j))
-                {
+    private static boolean containsDuplicate(String string) {
+        for (int i = 0; i < string.length() - 1; i++) {
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(i) == string.charAt(j)) {
                     return true;
                 }
             }
@@ -64,8 +56,7 @@ public class Solution implements DeprecatedSolver
     /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
     /// @return the position of the first start-of-packet marker.
     @Override
-    public Object partOne(List<String> puzzleInput)
-    {
+    public Object partOne(List<String> puzzleInput) {
         String line = puzzleInput.getFirst();
         return findFirstMarker(line, 4);
     }
@@ -78,8 +69,7 @@ public class Solution implements DeprecatedSolver
     /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
     /// @return the position of the first start-of-message marker.
     @Override
-    public Object partTwo(List<String> puzzleInput)
-    {
+    public Object partTwo(List<String> puzzleInput) {
         String line = puzzleInput.getFirst();
         return findFirstMarker(line, 14);
     }
