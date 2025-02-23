9124c225b6740addff48304573c94b86d96775a1
Sei-Jin <seijin.tufts@gmail.com>
2025-02-23 16:18:15 -0400

    [2022-5] Update test cases to use constructor

--- src/test/java/aoc/event/year2022/day05/supplyStacks/SolutionTests.java
+++ src/test/java/aoc/event/year2022/day05/supplyStacks/SolutionTests.java
@@ -2,31 +2,25 @@ package aoc.event.year2022.day05.supplyStacks;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SolutionTests
-{
+public class SolutionTests {
+    
     @Test
-    void example()
-    {
-        List<String> input = new ArrayList<>(
-                Arrays.stream("""
-                    [D]   \s
-                [N] [C]   \s
-                [Z] [M] [P]
-                 1   2   3\s
-                
-                move 1 from 2 to 1
-                move 3 from 1 to 3
-                move 2 from 2 to 1
-                move 1 from 1 to 2
-                """.split("\n")).toList()
-        );
-        assertEquals("CMZ", new Solution().partOne(input));
-        assertEquals("MCD", new Solution().partTwo(input));
+    void example() {
+        final var input = """
+                   [D]   \s
+               [N] [C]   \s
+               [Z] [M] [P]
+                1   2   3\s
+               
+               move 1 from 2 to 1
+               move 3 from 1 to 3
+               move 2 from 2 to 1
+               move 1 from 1 to 2
+               """;
+        final var solution = new Solution(input);
+        assertEquals("CMZ", solution.partOne());
+        assertEquals("MCD", solution.partTwo());
     }
 }
