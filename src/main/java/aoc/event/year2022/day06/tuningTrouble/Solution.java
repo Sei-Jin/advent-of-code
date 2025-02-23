8b478abb12c8743ef55602e99435e03cd5851e6b
Sei-Jin <seijin.tufts@gmail.com>
2025-02-23 18:10:06 -0400

    [2022-6] Update solution to use constructor

--- src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
+++ src/main/java/aoc/event/year2022/day06/tuningTrouble/Solution.java
@@ -1,11 +1,15 @@
 package aoc.event.year2022.day06.tuningTrouble;
 
-import aoc.DeprecatedSolver;
+import aoc.Solver;
 import aoc.Runner;
 
-import java.util.List;
-
-public class Solution implements DeprecatedSolver {
+public class Solution implements Solver {
+    
+    private final String buffer;
+    
+    public Solution(String input) {
+        this.buffer = input;
+    }
     
     /// Finds the position of the first marker.
     ///
@@ -16,9 +20,9 @@ public class Solution implements DeprecatedSolver {
     /// @param windowSize the length of distinct characters.
     /// @return the position of the first marker.
     private static int findFirstMarker(String line, int windowSize) {
-        for (int i = 0; i < line.length(); i++) {
-            String substring = line.substring(i, i + windowSize);
-            boolean containsDuplicate = containsDuplicate(substring);
+        for (var i = 0; i < line.length(); i++) {
+            final var substring = line.substring(i, i + windowSize);
+            final var containsDuplicate = containsDuplicate(substring);
             
             if (!containsDuplicate) {
                 return i + windowSize;
@@ -33,8 +37,8 @@ public class Solution implements DeprecatedSolver {
     /// @param string a string.
     /// @return true if the string contains a duplicate character, or false otherwise.
     private static boolean containsDuplicate(String string) {
-        for (int i = 0; i < string.length() - 1; i++) {
-            for (int j = i + 1; j < string.length(); j++) {
+        for (var i = 0; i < string.length() - 1; i++) {
+            for (var j = i + 1; j < string.length(); j++) {
                 if (string.charAt(i) == string.charAt(j)) {
                     return true;
                 }
@@ -49,12 +53,10 @@ public class Solution implements DeprecatedSolver {
     /// The start-of-packet marker starts after the first substring of length 4 with all distinct
     /// characters.
     ///
-    /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
     /// @return the position of the first start-of-packet marker.
     @Override
-    public Object partOne(List<String> puzzleInput) {
-        String line = puzzleInput.getFirst();
-        return findFirstMarker(line, 4);
+    public Object partOne() {
+        return findFirstMarker(buffer, 4);
     }
     
     /// Calculates the position of the first start-of-message marker.
@@ -62,12 +64,10 @@ public class Solution implements DeprecatedSolver {
     /// The start-of-message marker starts after the first substring of length 14 with all distinct
     /// characters.
     ///
-    /// @param puzzleInput the puzzle input. The puzzle input should contain a single string.
     /// @return the position of the first start-of-message marker.
     @Override
-    public Object partTwo(List<String> puzzleInput) {
-        String line = puzzleInput.getFirst();
-        return findFirstMarker(line, 14);
+    public Object partTwo() {
+        return findFirstMarker(buffer, 14);
     }
     
     public static void main(String[] args) {
