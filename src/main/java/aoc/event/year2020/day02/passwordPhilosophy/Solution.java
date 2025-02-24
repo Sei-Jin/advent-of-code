f5d4591dfdb00707396d9844c46a8398d75b77a2
Sei-Jin <seijin.tufts@gmail.com>
2025-02-24 18:38:56 -0400

    [2020-2] Reformat solution

--- src/main/java/aoc/event/year2020/day02/passwordPhilosophy/Solution.java
+++ src/main/java/aoc/event/year2020/day02/passwordPhilosophy/Solution.java
@@ -5,22 +5,15 @@ import aoc.DeprecatedSolver;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * --- Day 2: Password Philosophy ---
- */
-public class Solution implements DeprecatedSolver
-{
-    /**
-     * @param inputLines the puzzle input.
-     * @return the number of valid passwords according to the set policies.
-     */
+public class Solution implements DeprecatedSolver {
+    
+    /// @param inputLines the puzzle input.
+    /// @return the number of valid passwords according to the set policies.
     @Override
-    public Object partOne(List<String> inputLines)
-    {
+    public Object partOne(List<String> inputLines) {
         int validPasswords = 0;
         
-        for (String line : inputLines)
-        {
+        for (String line : inputLines) {
             List<String> lineValues = getLineValues(line);
             
             int minimumCount = Integer.parseInt(lineValues.get(0));
@@ -30,16 +23,13 @@ public class Solution implements DeprecatedSolver
             
             int letterCount = 0;
             
-            for (char character : password.toCharArray())
-            {
-                if (character == policyLetter)
-                {
+            for (char character : password.toCharArray()) {
+                if (character == policyLetter) {
                     letterCount++;
                 }
             }
             
-            if (letterCount >= minimumCount && letterCount <= maximumCount)
-            {
+            if (letterCount >= minimumCount && letterCount <= maximumCount) {
                 validPasswords++;
             }
         }
@@ -48,23 +38,18 @@ public class Solution implements DeprecatedSolver
     }
     
     
-    /**
-     * @param inputLines the puzzle input.
-     * @return the number of valid passwords according to the new interpretation of the policies.
-     */
+    /// @param inputLines the puzzle input.
+    /// @return the number of valid passwords according to the new interpretation of the policies.
     @Override
-    public Object partTwo(List<String> inputLines)
-    {
+    public Object partTwo(List<String> inputLines) {
         int validPasswords = 0;
         
-        for (String line : inputLines)
-        {
+        for (String line : inputLines) {
             List<String> lineValues = getLineValues(line);
             
             boolean oneLetterPresent = isOneLetterPresent(lineValues);
             
-            if (oneLetterPresent)
-            {
+            if (oneLetterPresent) {
                 validPasswords++;
             }
         }
@@ -73,15 +58,13 @@ public class Solution implements DeprecatedSolver
     }
     
     
-    private static List<String> getLineValues(String line)
-    {
+    private static List<String> getLineValues(String line) {
         return Arrays.stream(line.split("(-| |: )"))
                 .toList();
     }
     
     
-    private static boolean isOneLetterPresent(List<String> lineValues)
-    {
+    private static boolean isOneLetterPresent(List<String> lineValues) {
         int firstPosition = Integer.parseInt(lineValues.get(0));
         int secondPosition = Integer.parseInt(lineValues.get(1));
         char policyLetter = lineValues.get(2).charAt(0);
@@ -89,13 +72,11 @@ public class Solution implements DeprecatedSolver
         
         boolean oneLetterPresent = false;
         
-        if (policyLetter == password.charAt(firstPosition - 1))
-        {
+        if (policyLetter == password.charAt(firstPosition - 1)) {
             oneLetterPresent = !oneLetterPresent;
         }
         
-        if (policyLetter == password.charAt(secondPosition - 1))
-        {
+        if (policyLetter == password.charAt(secondPosition - 1)) {
             oneLetterPresent = !oneLetterPresent;
         }
         
