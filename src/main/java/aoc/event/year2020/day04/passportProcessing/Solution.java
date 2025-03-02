4e7ea368edfa08e79e8f8d6bc7d6bb64d5d2a35b
Sei-Jin <seijin.tufts@gmail.com>
2025-03-01 21:33:49 -0400

    [2020-4] Solve part two

--- src/main/java/aoc/event/year2020/day04/passportProcessing/Solution.java
+++ src/main/java/aoc/event/year2020/day04/passportProcessing/Solution.java
@@ -4,6 +4,7 @@ import aoc.Runner;
 import aoc.Solver;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Solution implements Solver {
     
@@ -18,13 +19,11 @@ public class Solution implements Solver {
     private Set<String> createMandatoryFields() {
         final var fields = new HashSet<String>();
         
-        fields.add("byr");
-        fields.add("iyr");
-        fields.add("eyr");
-        fields.add("hgt");
-        fields.add("hcl");
-        fields.add("ecl");
-        fields.add("pid");
+        for (final var field : Field.values()) {
+            if (field.mandatory) {
+                fields.add(field.key);
+            }
+        }
         
         return fields;
     }
@@ -66,7 +65,152 @@ public class Solution implements Solver {
     
     @Override
     public Integer partTwo() {
-        return 0;
+        var validCount = 0;
+        
+        for (final var fieldMap : fieldMaps) {
+            if (!fieldMap.keySet().containsAll(mandatoryFields)) {
+                continue;
+            }
+            
+            if (hasValidFields(fieldMap)) {
+                validCount++;
+            }
+        }
+        
+        return validCount;
+    }
+    
+    private static boolean hasValidFields(Map<String, String> fieldMap) {
+        boolean validity = true;
+        
+        validity &= Field.validBirthYear(fieldMap.get(Field.BIRTH_YEAR.key));
+        validity &= Field.validIssueYear(fieldMap.get(Field.ISSUE_YEAR.key));
+        validity &= Field.validExpirationYear(fieldMap.get(Field.EXPIRATION_YEAR.key));
+        validity &= Field.validHeight(fieldMap.get(Field.HEIGHT.key));
+        validity &= Field.validHairColor(fieldMap.get(Field.HAIR_COLOR.key));
+        validity &= Field.validEyeColor(fieldMap.get(Field.EYE_COLOR.key));
+        validity &= Field.validPassportId(fieldMap.get(Field.PASSPORT_ID.key));
+        
+        return validity;
+    }
+    
+    private enum Field {
+        BIRTH_YEAR("byr", true),
+        ISSUE_YEAR("iyr", true),
+        EXPIRATION_YEAR("eyr", true),
+        HEIGHT("hgt", true),
+        HAIR_COLOR("hcl", true),
+        EYE_COLOR("ecl", true),
+        PASSPORT_ID("pid", true),
+        COUNTRY_ID("cid", false);
+        
+        private final String key;
+        private final boolean mandatory;
+        
+        Field(String key, boolean mandatory) {
+            this.key = key;
+            this.mandatory = mandatory;
+        }
+        
+        private static final Pattern HEIGHT_PATTERN = Pattern.compile("^(\\d+)(\\w+)$");
+        private static final Pattern HAIR_COLOR_PATTERN = Pattern.compile("^#[a-f0-9]{6}$");
+        private static final Pattern PASSPORT_ID_PATTERN = Pattern.compile("^[0-9]{9}$");
+        
+        private static boolean validBirthYear(String birthYear) {
+            if (notNumeric(birthYear)) {
+                return false;
+            }
+            
+            final var year = Integer.parseInt(birthYear);
+            return year >= 1920 && year <= 2002;
+        }
+        
+        private static boolean validIssueYear(String issueYear) {
+            if (notNumeric(issueYear)) {
+                return false;
+            }
+            
+            final var year = Integer.parseInt(issueYear);
+            return year >= 2010 && year <= 2020;
+        }
+        
+        private static boolean validExpirationYear(String expirationYear) {
+            if (notNumeric(expirationYear)) {
+                return false;
+            }
+            
+            final var year = Integer.parseInt(expirationYear);
+            return year >= 2020 && year <= 2030;
+        }
+        
+        private static boolean validHeight(String height) {
+            final var matcher = HEIGHT_PATTERN.matcher(height);
+            
+            if (!matcher.find()) {
+                return false;
+            }
+            
+            final var magnitude = Integer.parseInt(matcher.group(1));
+            final var units = matcher.group(2);
+            
+            if (units.equals("cm")) {
+                return magnitude >= 150 && magnitude <= 193;
+            } else if (units.equals("in")) {
+                return magnitude >= 59 && magnitude <= 76;
+            }
+            
+            return false;
+        }
+        
+        private static boolean validHairColor(String hairColor) {
+            return HAIR_COLOR_PATTERN.matcher(hairColor).find();
+        }
+        
+        private static boolean validEyeColor(String eyeColor) {
+            return EyeColour.createColourSet().contains(eyeColor);
+        }
+        
+        private static boolean validPassportId(String id) {
+            return PASSPORT_ID_PATTERN.matcher(id).find();
+        }
+        
+        private static boolean notNumeric(String string) {
+            for (int i = 0; i < string.length(); i++) {
+                final var character = string.charAt(i);
+                
+                if (!Character.isDigit(character)) {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+    
+    private enum EyeColour {
+        AMBER("amb"),
+        BLUE("blu"),
+        BROWN("brn"),
+        GREY("gry"),
+        GREEN("grn"),
+        HAZEL("hzl"),
+        OTHER("oth");
+        
+        private final String colour;
+        
+        EyeColour(String colour) {
+            this.colour = colour;
+        }
+        
+        private static Set<String> createColourSet() {
+            final var colours = new HashSet<String>();
+            
+            for (final var value : EyeColour.values()) {
+                colours.add(value.colour);
+            }
+            
+            return colours;
+        }
     }
     
     public static void main(String[] args) {
