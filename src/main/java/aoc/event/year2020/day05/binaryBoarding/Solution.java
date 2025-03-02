f0856067102d26cfa0f6529ec93887667f05b311
Sei-Jin <seijin.tufts@gmail.com>
2025-03-02 12:16:46 -0400

    [2020-5] Add part one solution

--- src/main/java/aoc/event/year2020/day05/binaryBoarding/Solution.java
+++ src/main/java/aoc/event/year2020/day05/binaryBoarding/Solution.java
@@ -1,20 +1,108 @@
 package aoc.event.year2020.day05.binaryBoarding;
 
-import aoc.DeprecatedSolver;
+import aoc.Runner;
+import aoc.Solver;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
-public class Solution implements DeprecatedSolver
+public class Solution implements Solver
 {
-    @Override
-    public Object partOne(List<String> inputLines)
-    {
-        return null;
+    private static final Pattern BOARDING_SEQUENCE = Pattern.compile("^([BF]+)([LR]+)$");
+    
+    private final List<BoardingSequence> boardingSequences;
+    
+    public Solution(String input) {
+        boardingSequences = parse(input);
+    }
+    
+    private static List<BoardingSequence> parse(String input) {
+        final var boardingSequences = new ArrayList<BoardingSequence>();
+        final var lines = input.lines().toList();
+        
+        for (final var line : lines) {
+            final var matcher = BOARDING_SEQUENCE.matcher(line);
+            
+            if (matcher.find()) {
+                final var row = matcher.group(1);
+                final var column = matcher.group(2);
+                
+                final var rowSequence = new ArrayList<Character>();
+                final var columnSequence = new ArrayList<Character>();
+                
+                for (int i = 0; i < row.length(); i++) {
+                    rowSequence.add(row.charAt(i));
+                }
+                
+                for (int i = 0; i < column.length(); i++) {
+                    columnSequence.add(column.charAt(i));
+                }
+                
+                final var boardingSequence = new BoardingSequence(
+                    Collections.unmodifiableList(rowSequence),
+                    Collections.unmodifiableList(columnSequence)
+                );
+                
+                boardingSequences.add(boardingSequence);
+            }
+        }
+        
+        return Collections.unmodifiableList(boardingSequences);
+    }
+    
+    private static int binarySearch(List<Character> sequence, int n) {
+        var left = 0;
+        var right = n - 1;
+        
+        for (final var character : sequence) {
+            final var middle = (left + right) / 2;
+            
+            if (character == 'F' || character == 'L') {
+                // Lower half
+                right = middle;
+            } else if (character == 'B' || character == 'R') {
+                // Upper half
+                left = middle + 1;
+            }
+        }
+        
+        final var lastCharacter = sequence.getLast();
+        var value = 0;
+        
+        if (lastCharacter == 'F' || lastCharacter == 'L') {
+            value = Math.min(left, right);
+        } else if (lastCharacter == 'B' || lastCharacter == 'R') {
+            value = Math.max(left, right);
+        }
+        
+        return value;
     }
     
     @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        return null;
+    public Integer partOne() {
+        var highestSeatId = 0;
+        
+        for (final var sequence : boardingSequences) {
+            final var row = binarySearch(sequence.rowSequence, 128);
+            final var column = binarySearch(sequence.columnSequence, 8);
+            
+            final var seatId = row * 8 + column;
+            highestSeatId = Math.max(highestSeatId, seatId);
+        }
+        
+        return highestSeatId;
+    }
+    
+    @Override
+    public Integer partTwo() {
+        return 0;
+    }
+    
+    private record BoardingSequence(List<Character> rowSequence, List<Character> columnSequence) {}
+    
+    public static void main(String[] args) {
+        Runner.runAndPrint(2020, 5);
     }
 }
--- src/test/java/aoc/event/year2020/day05/binaryBoarding/SolutionTests.java
+++ src/test/java/aoc/event/year2020/day05/binaryBoarding/SolutionTests.java
@@ -1,5 +1,36 @@
 package aoc.event.year2020.day05.binaryBoarding;
 
-public class SolutionTests
-{
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTests {
+    
+    @Test
+    void exampleDefault() {
+        final var input = "FBFBBFFRLR";
+        final var solution = new Solution(input);
+        assertEquals(357, solution.partOne());
+    }
+    
+    @Test
+    void example1() {
+        final var input = "BFFFBBFRRR";
+        final var solution = new Solution(input);
+        assertEquals(567, solution.partOne());
+    }
+    
+    @Test
+    void example2() {
+        final var input = "FFFBBBFRRR";
+        final var solution = new Solution(input);
+        assertEquals(119, solution.partOne());
+    }
+    
+    @Test
+    void example3() {
+        final var input = "BBFFBBFRLL";
+        final var solution = new Solution(input);
+        assertEquals(820, solution.partOne());
+    }
 }
