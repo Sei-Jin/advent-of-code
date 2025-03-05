f3e53163f92d2f9d04482c5b4966d2d697d4dc15
Sei-Jin <seijin.tufts@gmail.com>
2025-03-05 11:53:59 -0400

    [2016-1] 10x performance increase for part two
    A custom point class with an overridden hashcode and equals method is used. Comparing the points directly instead of converting them to strings first is a far more efficient approach.

--- src/main/java/aoc/event/year2016/day01/noTimeForATaxicab/Solution.java
+++ src/main/java/aoc/event/year2016/day01/noTimeForATaxicab/Solution.java
@@ -3,10 +3,10 @@ package aoc.event.year2016.day01.noTimeForATaxicab;
 import aoc.Runner;
 import aoc.Solver;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 /// --- Day 1: No Time for a Taxicab ---
@@ -34,81 +34,56 @@ public class Solution implements Solver {
         return instructions;
     }
     
-    private record Instruction(Turn turn, int distance) {}
-    
     /// @return the number of blocks from the starting position to the Easter Bunny HQ.
     @Override
     public Integer partOne() {
-        Point point = new Point();
-        Direction currentDirection = Direction.NORTH;
+        final var current = new Point();
+        var direction = Direction.NORTH;
         
         for (final var instruction : instructions) {
-            currentDirection = changeDirection(currentDirection, instruction.turn);
+            direction = direction.turn(instruction.turn);
             
-            switch (currentDirection) {
-                case NORTH -> point.y += instruction.distance;
-                case EAST -> point.x += instruction.distance;
-                case SOUTH -> point.y -= instruction.distance;
-                case WEST -> point.x -= instruction.distance;
+            switch (direction) {
+                case NORTH -> current.y += instruction.distance;
+                case EAST -> current.x += instruction.distance;
+                case SOUTH -> current.y -= instruction.distance;
+                case WEST -> current.x -= instruction.distance;
             }
         }
         
-        return Math.abs(point.x) + Math.abs(point.y);
+        return Math.abs(current.x) + Math.abs(current.y);
     }
     
     /// @return the distance from the starting location to the first location visited twice or `-1` if there
     ///         were no locations visited twice.
     @Override
     public Integer partTwo() {
-        Point point = new Point();
-        Direction currentDirection = Direction.NORTH;
+        final var current = new Point();
+        var direction = Direction.NORTH;
         
-        HashSet<String> pointsVisited = new HashSet<>();
-        pointsVisited.add(point.toString());
+        final var points = new HashSet<Point>();
+        points.add(current);
         
         for (final var instruction : instructions) {
-            currentDirection = changeDirection(currentDirection, instruction.turn);
+            direction = direction.turn(instruction.turn);
             
-            for (int blocksTravelled = 0; blocksTravelled < instruction.distance; blocksTravelled++) {
-                switch (currentDirection) {
-                    case NORTH -> point.y++;
-                    case EAST -> point.x++;
-                    case SOUTH -> point.y--;
-                    case WEST -> point.x--;
+            for (var i = 0; i < instruction.distance; i++) {
+                switch (direction) {
+                    case NORTH -> current.y++;
+                    case EAST -> current.x++;
+                    case SOUTH -> current.y--;
+                    case WEST -> current.x--;
                 }
                 
-                if (!pointsVisited.contains(point.toString())) {
-                    pointsVisited.add(point.toString());
+                if (!points.contains(current)) {
+                    points.add(new Point(current.x, current.y));
                 } else {
-                    return Math.abs(point.x) + Math.abs(point.y);
+                    return Math.abs(current.x) + Math.abs(current.y);
                 }
             }
         }
         
-        // There were no locations visited twice
         return -1;
-    }
-    
-    private static Direction changeDirection(Direction currentDirection, Turn turningDirection) {
-        if (turningDirection == Turn.RIGHT) {
-            currentDirection = switch (currentDirection) {
-                case NORTH -> Direction.EAST;
-                case EAST -> Direction.SOUTH;
-                case SOUTH -> Direction.WEST;
-                case WEST -> Direction.NORTH;
-            };
-        }
-        
-        if (turningDirection == Turn.LEFT) {
-            currentDirection = switch (currentDirection) {
-                case NORTH -> Direction.WEST;
-                case WEST -> Direction.SOUTH;
-                case SOUTH -> Direction.EAST;
-                case EAST -> Direction.NORTH;
-            };
-        }
-        
-        return currentDirection;
     }
     
     private enum Turn {
@@ -128,7 +103,55 @@ public class Solution implements Solver {
         NORTH,
         EAST,
         SOUTH,
-        WEST
+        WEST;
+        
+        private Direction turn(Turn turn) {
+            if (turn == Turn.RIGHT) {
+                return switch (this) {
+                    case NORTH -> Direction.EAST;
+                    case EAST -> Direction.SOUTH;
+                    case SOUTH -> Direction.WEST;
+                    case WEST -> Direction.NORTH;
+                };
+            } else if (turn == Turn.LEFT) {
+                return switch (this) {
+                    case NORTH -> Direction.WEST;
+                    case WEST -> Direction.SOUTH;
+                    case SOUTH -> Direction.EAST;
+                    case EAST -> Direction.NORTH;
+                };
+            }
+            
+            return this;
+        }
+    }
+    
+    private record Instruction(Turn turn, int distance) {}
+    
+    private class Point {
+        int x;
+        int y;
+        
+        public Point() {
+            this.x = 0;
+            this.y = 0;
+        }
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Point point)) return false;
+            return x == point.x && y == point.y;
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
     
     public static void main(String[] args) {
