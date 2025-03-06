d6e5d7c997b5e993bfde0c53ebefd4b61211a83d
Sei-Jin <seijin.tufts@gmail.com>
2025-03-05 21:45:28 -0400

    [2016-6] Update test cases to use constructor

--- src/test/java/aoc/event/year2016/day06/signalsAndNoise/SolutionTests.java
+++ src/test/java/aoc/event/year2016/day06/signalsAndNoise/SolutionTests.java
@@ -2,35 +2,32 @@ package aoc.event.year2016.day06.signalsAndNoise;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SolutionTests
-{
+public class SolutionTests {
+    
     @Test
-    void example()
-    {
-        List<String> input = Arrays.stream("""
-                eedadn
-                drvtee
-                eandsr
-                raavrd
-                atevrs
-                tsrnev
-                sdttsa
-                rasrtv
-                nssdts
-                ntnada
-                svetve
-                tesnvt
-                vntsnd
-                vrdear
-                dvrsen
-                enarar
-                """.split("\n")).toList();
-        assertEquals("easter", new Solution().partOne(input));
-        assertEquals("advent", new Solution().partTwo(input));
+    void example() {
+        final var input = """
+            eedadn
+            drvtee
+            eandsr
+            raavrd
+            atevrs
+            tsrnev
+            sdttsa
+            rasrtv
+            nssdts
+            ntnada
+            svetve
+            tesnvt
+            vntsnd
+            vrdear
+            dvrsen
+            enarar
+            """;
+        final var solution = new Solution(input);
+        assertEquals("easter", solution.partOne());
+        assertEquals("advent", solution.partTwo());
     }
 }
