0e84a47e0d46119f5ba49dbc47ed56dbe674b1c3
Sei-Jin <seijin.tufts@gmail.com>
2025-03-05 22:39:54 -0400

    [2015-3] Update test cases to use constructor

--- src/test/java/aoc/event/year2015/day03/perfectlySphericalHousesInAVacuum/SolutionTests.java
+++ src/test/java/aoc/event/year2015/day03/perfectlySphericalHousesInAVacuum/SolutionTests.java
@@ -2,42 +2,39 @@ package aoc.event.year2015.day03.perfectlySphericalHousesInAVacuum;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTests {
     
     @Test
     void headEast() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add(">");
-        assertEquals(2, new Solution().partOne(inputLines));
-        assertEquals(2, new Solution().partTwo(inputLines));
+        final var input = ">";
+        final var solution = new Solution(input);
+        assertEquals(2, solution.partOne());
+        assertEquals(2, solution.partTwo());
     }
     
     @Test
     void cycleThroughDirections() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("^>v<");
-        assertEquals(4, new Solution().partOne(inputLines));
-        assertEquals(3, new Solution().partTwo(inputLines));
+        final var input = "^>v<";
+        final var solution = new Solution(input);
+        assertEquals(4, solution.partOne());
+        assertEquals(3, solution.partTwo());
     }
     
     @Test
     void northAndSouthAlternating() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("^v^v^v^v^v");
-        assertEquals(2, new Solution().partOne(inputLines));
-        assertEquals(11, new Solution().partTwo(inputLines));
+        final var input = "^v^v^v^v^v";
+        final var solution = new Solution(input);
+        assertEquals(2, solution.partOne());
+        assertEquals(11, solution.partTwo());
     }
     
     @Test
     void northAndSouth() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("^v");
-        assertEquals(2, new Solution().partOne(inputLines));
-        assertEquals(3, new Solution().partTwo(inputLines));
+        final var input = "^v";
+        final var solution = new Solution(input);
+        assertEquals(2, solution.partOne());
+        assertEquals(3, solution.partTwo());
     }
 }
