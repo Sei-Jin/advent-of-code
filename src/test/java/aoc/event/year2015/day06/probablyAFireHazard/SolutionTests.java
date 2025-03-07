2cd820733d6ea2b67f463dbcb5b732bd9fd73d7b
Sei-Jin <seijin.tufts@gmail.com>
2025-03-07 11:04:39 -0400

    [2015-6] Update test cases to use constructor

--- src/test/java/aoc/event/year2015/day06/probablyAFireHazard/SolutionTests.java
+++ src/test/java/aoc/event/year2015/day06/probablyAFireHazard/SolutionTests.java
@@ -2,60 +2,61 @@ package aoc.event.year2015.day06.probablyAFireHazard;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTests {
     
     @Test
     void turnOnEverything() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("turn on 0,0 through 999,999");
-        assertEquals(1000000, new Solution().partOne(inputLines));
-        assertEquals(1000000, new Solution().partTwo(inputLines));
+        final var input = "turn on 0,0 through 999,999";
+        final var solution = new Solution(input);
+        assertEquals(1000000, solution.partOne());
+        assertEquals(1000000, solution.partTwo());
     }
     
     @Test
     void toggleFirstRow() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("toggle 0,0 through 999,0");
-        assertEquals(1000, new Solution().partOne(inputLines));
-        assertEquals(2000, new Solution().partTwo(inputLines));
+        final var input = "toggle 0,0 through 999,0";
+        final var solution = new Solution(input);
+        assertEquals(1000, solution.partOne());
+        assertEquals(2000, solution.partTwo());
     }
     
     @Test
     void turnOffMiddleFour() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("turn off 499,499 through 500,500");
-        assertEquals(0, new Solution().partOne(inputLines));
-        assertEquals(0, new Solution().partTwo(inputLines));
+        final var input = "turn off 499,499 through 500,500";
+        final var solution = new Solution(input);
+        assertEquals(0, solution.partOne());
+        assertEquals(0, solution.partTwo());
     }
     
     @Test
     void turnOnEverythingThenTurnOffMiddleFour() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("turn on 0,0 through 999,999");
-        inputLines.add("turn off 499,499 through 500,500");
-        assertEquals(999996, new Solution().partOne(inputLines));
-        assertEquals(999996, new Solution().partTwo(inputLines));
+        final var input = """
+            turn on 0,0 through 999,999
+            turn off 499,499 through 500,500
+            """;
+        final var solution = new Solution(input);
+        assertEquals(999996, solution.partOne());
+        assertEquals(999996, solution.partTwo());
     }
     
     @Test
     void turnOnFirstTwice() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("turn on 0,0 through 0,0");
-        inputLines.add("turn on 0,0 through 0,0");
-        assertEquals(1, new Solution().partOne(inputLines));
-        assertEquals(2, new Solution().partTwo(inputLines));
+        final var input = """
+            turn on 0,0 through 0,0
+            turn on 0,0 through 0,0
+            """;
+        final var solution = new Solution(input);
+        assertEquals(1, solution.partOne());
+        assertEquals(2, solution.partTwo());
     }
     
     @Test
     void toggleEverything() {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("toggle 0,0 through 999,999");
-        assertEquals(1000000, new Solution().partOne(inputLines));
-        assertEquals(2000000, new Solution().partTwo(inputLines));
+        final var input = "toggle 0,0 through 999,999";
+        final var solution = new Solution(input);
+        assertEquals(1000000, solution.partOne());
+        assertEquals(2000000, solution.partTwo());
     }
 }
