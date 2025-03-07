d79b4484aa8fc7011a3203129a9b514db37add2e
Sei-Jin <seijin.tufts@gmail.com>
2025-03-07 10:43:23 -0400

    [2015-6] Update solution to use constructor

--- src/main/java/aoc/event/year2015/day06/probablyAFireHazard/Solution.java
+++ src/main/java/aoc/event/year2015/day06/probablyAFireHazard/Solution.java
@@ -1,79 +1,30 @@
 package aoc.event.year2015.day06.probablyAFireHazard;
 
-import aoc.DeprecatedSolver;
+import aoc.Runner;
+import aoc.Solver;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution implements DeprecatedSolver {
+public class Solution implements Solver {
     
-    /// @param inputLines the puzzle input.
-    /// @return the number of lit lights after following the instructions.
-    @Override
-    public Object partOne(List<String> inputLines) {
-        boolean[][] lightGrid = new boolean[1000][1000];
-        
-        for (String line : inputLines) {
-            Instruction instruction = getInstruction(line);
-            
-            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
-                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
-                    switch (instruction.operation) {
-                        case ON -> lightGrid[row][column] = true;
-                        case OFF -> lightGrid[row][column] = false;
-                        case TOGGLE -> lightGrid[row][column] = !lightGrid[row][column];
-                    }
-                }
-            }
-        }
-        
-        int totalLightsTurnedOn = 0;
-        
-        for (int row = 0; row < lightGrid.length; row++) {
-            for (int column = 0; column < lightGrid[0].length; column++) {
-                if (lightGrid[row][column]) {
-                    totalLightsTurnedOn++;
-                }
-            }
-        }
-        
-        return totalLightsTurnedOn;
+    private final List<Instruction> instructions;
+    
+    public Solution(String input) {
+        instructions = parse(input);
     }
     
-    /// @param inputLines the puzzle input.
-    /// @return the total brightness of all lights combined after following Santa's instructions.
-    @Override
-    public Object partTwo(List<String> inputLines) {
-        int[][] lightGrid = new int[1000][1000];
+    private static List<Instruction> parse(String input) {
+        final var instructions = new ArrayList<Instruction>();
         
-        for (String line : inputLines) {
-            Instruction instruction = getInstruction(line);
-            
-            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
-                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
-                    switch (instruction.operation) {
-                        case ON -> lightGrid[row][column]++;
-                        case OFF -> {
-                            if (lightGrid[row][column] > 0) {
-                                lightGrid[row][column]--;
-                            }
-                        }
-                        case TOGGLE -> lightGrid[row][column] += 2;
-                    }
-                }
-            }
+        for (final var line : input.lines().toList()) {
+            final var instruction = getInstruction(line);
+            instructions.add(instruction);
         }
         
-        int totalBrightness = 0;
-        
-        for (int row = 0; row < lightGrid.length; row++) {
-            for (int column = 0; column < lightGrid[0].length; column++) {
-                totalBrightness += lightGrid[row][column];
-            }
-        }
-        
-        return totalBrightness;
+        return instructions;
     }
     
     private static Instruction getInstruction(String line) {
@@ -103,6 +54,68 @@ public class Solution implements DeprecatedSolver {
         return new Instruction(operation, topCorner, bottomCorner);
     }
     
+    /// @return the number of lit lights after following the instructions.
+    @Override
+    public Object partOne() {
+        boolean[][] lightGrid = new boolean[1000][1000];
+        
+        for (final var instruction : instructions) {
+            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
+                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
+                    switch (instruction.operation) {
+                        case ON -> lightGrid[row][column] = true;
+                        case OFF -> lightGrid[row][column] = false;
+                        case TOGGLE -> lightGrid[row][column] = !lightGrid[row][column];
+                    }
+                }
+            }
+        }
+        
+        int totalLightsTurnedOn = 0;
+        
+        for (int row = 0; row < lightGrid.length; row++) {
+            for (int column = 0; column < lightGrid[0].length; column++) {
+                if (lightGrid[row][column]) {
+                    totalLightsTurnedOn++;
+                }
+            }
+        }
+        
+        return totalLightsTurnedOn;
+    }
+    
+    /// @return the total brightness of all lights combined after following Santa's instructions.
+    @Override
+    public Object partTwo() {
+        int[][] lightGrid = new int[1000][1000];
+        
+        for (final var instruction : instructions) {
+            for (int row = instruction.topCorner.y; row <= instruction.bottomCorner.y; row++) {
+                for (int column = instruction.topCorner.x; column <= instruction.bottomCorner.x; column++) {
+                    switch (instruction.operation) {
+                        case ON -> lightGrid[row][column]++;
+                        case OFF -> {
+                            if (lightGrid[row][column] > 0) {
+                                lightGrid[row][column]--;
+                            }
+                        }
+                        case TOGGLE -> lightGrid[row][column] += 2;
+                    }
+                }
+            }
+        }
+        
+        int totalBrightness = 0;
+        
+        for (int row = 0; row < lightGrid.length; row++) {
+            for (int column = 0; column < lightGrid[0].length; column++) {
+                totalBrightness += lightGrid[row][column];
+            }
+        }
+        
+        return totalBrightness;
+    }
+    
     private record Instruction(Operation operation, Point topCorner, Point bottomCorner) {}
     
     private enum Operation {
@@ -119,5 +132,9 @@ public class Solution implements DeprecatedSolver {
         private String getValue() {
             return value;
         }
+    }
+    
+    public static void main(String[] args) {
+        Runner.runAndPrint(2015, 6);
     }
 }
