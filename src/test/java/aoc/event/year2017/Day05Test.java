53941675ce6ee472bfd3ffa420975e08f8ec45a8
Sei-Jin <seijin.tufts@gmail.com>
2025-03-20 11:30:47 -0300

    [2017-5] Add test case

--- src/test/java/aoc/event/year2017/Day05Test.java
+++ src/test/java/aoc/event/year2017/Day05Test.java
@@ -1,6 +1,22 @@
 package aoc.event.year2017;
 
-public class Day05Test
-{
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day05Test {
+    
+    @Test
+    void example() {
+        final var input = """
+            0
+            3
+            0
+            1
+            -3
+            """;
+        final var solution = new Day05(input);
+        assertEquals(5, solution.partOne());
+        assertEquals(10, solution.partTwo());
+    }
 }
