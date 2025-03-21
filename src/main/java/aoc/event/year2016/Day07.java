ce2f08f1e5072b36fc3d5503abedbfe23abd99c7
Sei-Jin <seijin.tufts@gmail.com>
2025-03-21 20:06:27 -0300

    [2017-7] Extract parse method

--- src/main/java/aoc/event/year2016/Day07.java
+++ src/main/java/aoc/event/year2016/Day07.java
@@ -8,46 +8,55 @@ import java.util.stream.Gatherers;
 
 public class Day07 implements Solver<Integer> {
     
-    private static List<String> lines;
+    private static List<Sequence> sequences;
     
     public Day07(String input) {
-        lines = input.lines().toList();
+        sequences = parse(input);
+    }
+    
+    private static List<Sequence> parse(String input) {
+        return input
+            .lines()
+            .map(line -> {
+                final var outside = new ArrayList<Character>();
+                final var inside = new ArrayList<Character>();
+                
+                var isInside = false;
+                
+                for (int i = 0; i < line.length(); i++) {
+                    final var character = line.charAt(i);
+                    
+                    if (character == '[') {
+                        isInside = true;
+                    } else if (character == ']') {
+                        isInside = false;
+                    } else {
+                        if (isInside) {
+                            inside.add(character);
+                        } else {
+                            outside.add(character);
+                        }
+                    }
+                }
+                
+                return new Sequence(outside, inside);
+            })
+            .toList();
     }
     
     @Override
     public Integer partOne() {
         var count = 0;
         
-        for (final var line : lines) {
-            final var outside = new ArrayList<Character>();
-            final var inside = new ArrayList<Character>();
-            
-            var isInside = false;
-            
-            for (int i = 0; i < line.length(); i++) {
-                final var character = line.charAt(i);
-                
-                if (character == '[') {
-                    isInside = true;
-                } else if (character == ']') {
-                    isInside = false;
-                } else {
-                    if (isInside) {
-                        inside.add(character);
-                    } else {
-                        outside.add(character);
-                    }
-                }
-            }
-            
-            final var outsideSequence = outside
+        for (final var pair : sequences) {
+            final var outsideSequence = pair.outside()
                 .stream()
                 .gather(Gatherers.windowSliding(4))
                 .anyMatch(i ->
                     i.getFirst() == i.get(3) && i.get(1) == i.get(2) && i.getFirst() != i.get(1)
                 );
             
-            final var insideSequence = inside
+            final var insideSequence = pair.inside()
                 .stream()
                 .gather(Gatherers.windowSliding(4))
                 .anyMatch(i ->
@@ -66,29 +75,8 @@ public class Day07 implements Solver<Integer> {
     public Integer partTwo() {
         var count = 0;
         
-        for (final var line : lines) {
-            final var outside = new ArrayList<Character>();
-            final var inside = new ArrayList<Character>();
-            
-            var isInside = false;
-            
-            for (int i = 0; i < line.length(); i++) {
-                final var character = line.charAt(i);
-                
-                if (character == '[') {
-                    isInside = true;
-                } else if (character == ']') {
-                    isInside = false;
-                } else {
-                    if (isInside) {
-                        inside.add(character);
-                    } else {
-                        outside.add(character);
-                    }
-                }
-            }
-            
-            final var outsideSequences = outside
+        for (final var pair : sequences) {
+            final var outsideSequences = pair.outside()
                 .stream()
                 .gather(Gatherers.windowSliding(3))
                 .filter(i -> i.getFirst() == i.get(2) && i.getFirst() != i.get(1))
@@ -106,7 +94,7 @@ public class Day07 implements Solver<Integer> {
                 invertedOutsideSequences.add(inverted);
             }
             
-            final var supportsSsl = inside
+            final var supportsSsl = pair.inside()
                 .stream()
                 .gather(Gatherers.windowSliding(3))
                 .anyMatch(i -> invertedOutsideSequences.contains(i.subList(0, i.size())));
@@ -118,4 +106,6 @@ public class Day07 implements Solver<Integer> {
         
         return count;
     }
+    
+    private record Sequence(ArrayList<Character> outside, ArrayList<Character> inside) {}
 }
