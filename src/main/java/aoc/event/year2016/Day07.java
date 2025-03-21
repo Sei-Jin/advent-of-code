236abe33fbf6cd7d563fbcb2945363b90b330bb4
Sei-Jin <seijin.tufts@gmail.com>
2025-03-21 19:59:56 -0300

    [2017-7] Complete part two

--- src/main/java/aoc/event/year2016/Day07.java
+++ src/main/java/aoc/event/year2016/Day07.java
@@ -31,12 +31,12 @@ public class Day07 implements Solver<Integer> {
                     isInside = true;
                 } else if (character == ']') {
                     isInside = false;
-                }
-                
-                if (isInside) {
-                    inside.add(character);
                 } else {
-                    outside.add(character);
+                    if (isInside) {
+                        inside.add(character);
+                    } else {
+                        outside.add(character);
+                    }
                 }
             }
             
@@ -64,6 +64,58 @@ public class Day07 implements Solver<Integer> {
     
     @Override
     public Integer partTwo() {
-        return 0;
+        var count = 0;
+        
+        for (final var line : lines) {
+            final var outside = new ArrayList<Character>();
+            final var inside = new ArrayList<Character>();
+            
+            var isInside = false;
+            
+            for (int i = 0; i < line.length(); i++) {
+                final var character = line.charAt(i);
+                
+                if (character == '[') {
+                    isInside = true;
+                } else if (character == ']') {
+                    isInside = false;
+                } else {
+                    if (isInside) {
+                        inside.add(character);
+                    } else {
+                        outside.add(character);
+                    }
+                }
+            }
+            
+            final var outsideSequences = outside
+                .stream()
+                .gather(Gatherers.windowSliding(3))
+                .filter(i -> i.getFirst() == i.get(2) && i.getFirst() != i.get(1))
+                .toList();
+            
+            final var invertedOutsideSequences = new ArrayList<List<Character>>();
+            
+            for (final var list : outsideSequences) {
+                final var inverted = new ArrayList<Character>();
+                
+                inverted.add(list.get(1));
+                inverted.add(list.get(0));
+                inverted.add(list.get(1));
+                
+                invertedOutsideSequences.add(inverted);
+            }
+            
+            final var supportsSsl = inside
+                .stream()
+                .gather(Gatherers.windowSliding(3))
+                .anyMatch(i -> invertedOutsideSequences.contains(i.subList(0, i.size())));
+            
+            if (supportsSsl) {
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
--- src/test/java/aoc/event/year2016/Day07Test.java
+++ src/test/java/aoc/event/year2016/Day07Test.java
@@ -1,36 +1,73 @@
 package aoc.event.year2016;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day07Test {
     
-    @Test
-    void example1() {
-        final var input = "abba[mnop]qrst";
-        final var solution = new Day07(input);
-        assertEquals(1, solution.partOne());
+    @Nested
+    public class partOne {
+        
+        @Test
+        void example1() {
+            final var input = "abba[mnop]qrst";
+            final var solution = new Day07(input);
+            assertEquals(1, solution.partOne());
+        }
+        
+        @Test
+        void example2() {
+            final var input = "abcd[bddb]xyyx";
+            final var solution = new Day07(input);
+            assertEquals(0, solution.partOne());
+        }
+        
+        @Test
+        void example3() {
+            final var input = "aaaa[qwer]tyui";
+            final var solution = new Day07(input);
+            assertEquals(0, solution.partOne());
+        }
+        
+        @Test
+        void example4() {
+            final var input = "ioxxoj[asdfgh]zxcvbn";
+            final var solution = new Day07(input);
+            assertEquals(1, solution.partOne());
+        }
     }
     
-    @Test
-    void example2() {
-        final var input = "abcd[bddb]xyyx";
-        final var solution = new Day07(input);
-        assertEquals(0, solution.partOne());
-    }
-    
-    @Test
-    void example3() {
-        final var input = "aaaa[qwer]tyui";
-        final var solution = new Day07(input);
-        assertEquals(0, solution.partOne());
-    }
-    
-    @Test
-    void example4() {
-        final var input = "ioxxoj[asdfgh]zxcvbn";
-        final var solution = new Day07(input);
-        assertEquals(1, solution.partOne());
+    @Nested
+    public class partTwo {
+        
+        @Test
+        void example1() {
+            final var input = "aba[bab]xyz";
+            final var solution = new Day07(input);
+            assertEquals(1, solution.partTwo());
+        }
+        
+        @Test
+        void example2() {
+            final var input = "xyx[xyx]xyx";
+            final var solution = new Day07(input);
+            assertEquals(0, solution.partTwo());
+        }
+        
+        @Test
+        void example3() {
+            final var input = "aaa[kek]eke";
+            final var solution = new Day07(input);
+            assertEquals(1, solution.partTwo());
+        }
+        
+        @Test
+        void example4() {
+            final var input = "zazbz[bzb]cdb";
+            final var solution = new Day07(input);
+            assertEquals(1, solution.partTwo());
+        }
     }
 }
