4ebe58d761340fc4374f5f7365d83a7075cc9b17
Sei-Jin <seijin.tufts@gmail.com>
2025-03-22 14:08:40 -0300

    [2016-09] Add extra test cases

--- src/test/java/aoc/event/year2016/Day09Test.java
+++ src/test/java/aoc/event/year2016/Day09Test.java
@@ -47,4 +47,60 @@ class Day09Test {
         final var solution = new Day09(input);
         assertEquals(18, solution.partOne());
     }
+    
+    @Test
+    void custom1() {
+        final var input = "(6x1)(1x3)A(1x3)";
+        final var solution = new Day09(input);
+        assertEquals(6, solution.partOne());
+    }
+    
+    @Test
+    void custom2() {
+        final var input = "A(1x5)(2x3)BCD";
+        final var solution = new Day09(input);
+        assertEquals(13, solution.partOne());
+    }
+    
+    @Test
+    void custom3() {
+        final var input = "(2x3)A(2x2)BCD";
+        final var solution = new Day09(input);
+        assertEquals(13, solution.partOne());
+    }
+    
+    @Test
+    void custom4() {
+        final var input = "(3x4)A(2x2)(2x3)B(1x3)C";
+        final var solution = new Day09(input);
+        assertEquals(26, solution.partOne());
+    }
+    
+    @Test
+    void custom5() {
+        final var input = "(3x3)A";
+        final var solution = new Day09(input);
+        assertEquals(3, solution.partOne());
+    }
+    
+    @Test
+    void custom6() {
+        final var input = "A(3x3)";
+        final var solution = new Day09(input);
+        assertEquals(1, solution.partOne());
+    }
+    
+    @Test
+    void custom7() {
+        final var input = "X(3x3)A(2x2)BC(2x2)EF";
+        final var solution = new Day09(input);
+        assertEquals(19, solution.partOne());
+    }
+    
+    @Test
+    void custom8() {
+        final var input = "ABC(2x3)DEFG";
+        final var solution = new Day09(input);
+        assertEquals(11, solution.partOne());
+    }
 }
