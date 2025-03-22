655c5e9151e492646ee6a04c61728d25637a6249
Sei-Jin <seijin.tufts@gmail.com>
2025-03-22 15:19:11 -0300

    [2016-09] Complete part one

--- src/main/java/aoc/event/year2016/Day09.java
+++ src/main/java/aoc/event/year2016/Day09.java
@@ -2,12 +2,7 @@ package aoc.event.year2016;
 
 import aoc.Solver;
 
-import java.util.Arrays;
-import java.util.regex.Pattern;
-
 public class Day09 implements Solver<Integer> {
-    
-    private static final Pattern SEQUENCE = Pattern.compile("\\((\\d)x(\\d+)\\)");
     
     private static String line;
     
@@ -17,52 +12,32 @@ public class Day09 implements Solver<Integer> {
     
     @Override
     public Integer partOne() {
-        final var counts = new int[line.length()];
-        Arrays.fill(counts, 1);
-        
-        final var markers = SEQUENCE.matcher(line)
-            .results()
-            .map(result -> {
-                final var length = Integer.parseInt(result.group(1));
-                final var repetitions = Integer.parseInt(result.group(2));
-                final var start = result.start();
-                final var end = result.end() - 1;
-                
-                return new Marker(length, repetitions, start, end);
-            })
-            .toList();
-        
+        var total = 0;
         var current = 0;
         
-        for (final var marker : markers) {
-            if (marker.start >= current) {
-                for (int i = marker.start(); i <= marker.end(); i++) {
-                    counts[i] = 0;
-                }
+        while (current < line.length()) {
+            if (line.charAt(current) != '(') {
+                current++;
+                total++;
+            } else {
+                final var closing = current + line.substring(current).indexOf(')');
                 
-                current = marker.end() + 1;
-                final var maxIndex = current + marker.length();
+                final var values = line.substring(current + 1, closing).split("x");
+                final var length = Integer.parseInt(values[0]);
+                final var repetitions = Integer.parseInt(values[1]);
                 
-                while (current < maxIndex && current < counts.length) {
-                    counts[current] *= marker.repetitions();
-                    current++;
-                }
+                final var maxLength = Math.min(length, line.length() - closing - 1);
+                
+                total += maxLength * repetitions;
+                current = closing + maxLength + 1;
             }
         }
         
-        var sum = 0;
-        
-        for (final var count : counts) {
-            sum += count;
-        }
-        
-        return sum;
+        return total;
     }
     
     @Override
     public Integer partTwo() {
         return 0;
     }
-    
-    private record Marker(int length, int repetitions, int start, int end) {}
 }
