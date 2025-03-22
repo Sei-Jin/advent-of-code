7ab6e316894c7bf672771d528ef1f56f0b9bc807
Sei-Jin <seijin.tufts@gmail.com>
2025-03-22 15:56:30 -0300

    [2016-09] Complete part two

--- src/main/java/aoc/event/year2016/Day09.java
+++ src/main/java/aoc/event/year2016/Day09.java
@@ -2,7 +2,7 @@ package aoc.event.year2016;
 
 import aoc.Solver;
 
-public class Day09 implements Solver<Integer> {
+public class Day09 implements Solver<Long> {
     
     private static String line;
     
@@ -11,7 +11,7 @@ public class Day09 implements Solver<Integer> {
     }
     
     @Override
-    public Integer partOne() {
+    public Long partOne() {
         var total = 0;
         var current = 0;
         
@@ -33,11 +33,59 @@ public class Day09 implements Solver<Integer> {
             }
         }
         
-        return total;
+        return (long) total;
     }
     
     @Override
-    public Integer partTwo() {
-        return 0;
+    public Long partTwo() {
+        final var counts = new int[line.length()];
+        
+        var inside = false;
+        
+        for (int i = 0; i < counts.length; i++) {
+            if (line.charAt(i) == '(') {
+                inside = true;
+            } else if (line.charAt(i) == ')') {
+                inside = false;
+                counts[i] = 0;
+                continue;
+            }
+            
+            if (inside) {
+                counts[i] = 0;
+            } else {
+                counts[i] = 1;
+            }
+        }
+        
+        var current = 0;
+        
+        while (current < line.length()) {
+            if (line.charAt(current) != '(') {
+                current++;
+            } else {
+                final var closing = current + line.substring(current).indexOf(')');
+                
+                final var values = line.substring(current + 1, closing).split("x");
+                final var length = Integer.parseInt(values[0]);
+                final var repetitions = Integer.parseInt(values[1]);
+                
+                final var maxLength = Math.min(length, line.length() - closing - 1);
+                
+                for (int i = closing; i <= closing + maxLength; i++) {
+                    counts[i] *= repetitions;
+                }
+                
+                current = closing + 1;
+            }
+        }
+        
+        var total = 0L;
+        
+        for (final var count : counts) {
+            total += count;
+        }
+        
+        return total;
     }
 }
--- src/test/java/aoc/event/year2016/Day09Test.java
+++ src/test/java/aoc/event/year2016/Day09Test.java
@@ -10,97 +10,113 @@ class Day09Test {
     void example1() {
         final var input = "ADVENT";
         final var solution = new Day09(input);
-        assertEquals(6, solution.partOne());
+        assertEquals(6L, solution.partOne());
     }
     
     @Test
     void example2() {
         final var input = "A(1x5)BC";
         final var solution = new Day09(input);
-        assertEquals(7, solution.partOne());
+        assertEquals(7L, solution.partOne());
     }
     
     @Test
     void example3() {
         final var input = "(3x3)XYZ";
         final var solution = new Day09(input);
-        assertEquals(9, solution.partOne());
+        assertEquals(9L, solution.partOne());
+        assertEquals(9L, solution.partTwo());
     }
     
     @Test
     void example4() {
         final var input = "A(2x2)BCD(2x2)EFG";
         final var solution = new Day09(input);
-        assertEquals(11, solution.partOne());
+        assertEquals(11L, solution.partOne());
     }
     
     @Test
     void example5() {
         final var input = "(6x1)(1x3)A";
         final var solution = new Day09(input);
-        assertEquals(6, solution.partOne());
+        assertEquals(6L, solution.partOne());
     }
     
     @Test
     void example6() {
         final var input = "X(8x2)(3x3)ABCY";
         final var solution = new Day09(input);
-        assertEquals(18, solution.partOne());
+        assertEquals(18L, solution.partOne());
+        assertEquals(20L, solution.partTwo());
     }
     
     @Test
     void custom1() {
         final var input = "(6x1)(1x3)A(1x3)";
         final var solution = new Day09(input);
-        assertEquals(6, solution.partOne());
+        assertEquals(6L, solution.partOne());
     }
     
     @Test
     void custom2() {
         final var input = "A(1x5)(2x3)BCD";
         final var solution = new Day09(input);
-        assertEquals(13, solution.partOne());
+        assertEquals(13L, solution.partOne());
     }
     
     @Test
     void custom3() {
         final var input = "(2x3)A(2x2)BCD";
         final var solution = new Day09(input);
-        assertEquals(13, solution.partOne());
+        assertEquals(13L, solution.partOne());
     }
     
     @Test
     void custom4() {
         final var input = "(3x4)A(2x2)(2x3)B(1x3)C";
         final var solution = new Day09(input);
-        assertEquals(26, solution.partOne());
+        assertEquals(26L, solution.partOne());
     }
     
     @Test
     void custom5() {
         final var input = "(3x3)A";
         final var solution = new Day09(input);
-        assertEquals(3, solution.partOne());
+        assertEquals(3L, solution.partOne());
     }
     
     @Test
     void custom6() {
         final var input = "A(3x3)";
         final var solution = new Day09(input);
-        assertEquals(1, solution.partOne());
+        assertEquals(1L, solution.partOne());
     }
     
     @Test
     void custom7() {
         final var input = "X(3x3)A(2x2)BC(2x2)EF";
         final var solution = new Day09(input);
-        assertEquals(19, solution.partOne());
+        assertEquals(19L, solution.partOne());
     }
     
     @Test
     void custom8() {
         final var input = "ABC(2x3)DEFG";
         final var solution = new Day09(input);
-        assertEquals(11, solution.partOne());
+        assertEquals(11L, solution.partOne());
+    }
+    
+    @Test
+    void examplePartTwo1() {
+        final var input = "(27x12)(20x12)(13x14)(7x10)(1x12)A";
+        final var solution = new Day09(input);
+        assertEquals(241920L, solution.partTwo());
+    }
+    
+    @Test
+    void examplePartTwo2() {
+        final var input = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN";
+        final var solution = new Day09(input);
+        assertEquals(445L, solution.partTwo());
     }
 }
