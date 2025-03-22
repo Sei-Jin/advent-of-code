96eab52a08ddd616f725ea2c8e229afc95878ef3
Sei-Jin <seijin.tufts@gmail.com>
2025-03-21 22:58:09 -0300

    [2017-7] Update solution

--- src/main/java/aoc/event/year2016/Day07.java
+++ src/main/java/aoc/event/year2016/Day07.java
@@ -30,12 +30,12 @@ public class Day07 implements Solver<Integer> {
                         isInside = true;
                     } else if (character == ']') {
                         isInside = false;
+                    }
+                    
+                    if (isInside) {
+                        inside.add(character);
                     } else {
-                        if (isInside) {
-                            inside.add(character);
-                        } else {
-                            outside.add(character);
-                        }
+                        outside.add(character);
                     }
                 }
                 
@@ -56,7 +56,7 @@ public class Day07 implements Solver<Integer> {
             .count();
     }
     
-    private static boolean hasAbba(ArrayList<Character> sequence) {
+    private static boolean hasAbba(List<Character> sequence) {
         return sequence
             .stream()
             .gather(Gatherers.windowSliding(4))
@@ -70,28 +70,29 @@ public class Day07 implements Solver<Integer> {
         var count = 0;
         
         for (final var pair : sequences) {
-            final var outsideSequences = pair.outside()
+            final var outsideAbas = pair.outside()
                 .stream()
                 .gather(Gatherers.windowSliding(3))
-                .filter(i -> i.getFirst() == i.get(2) && i.getFirst() != i.get(1))
+                .filter(i -> i.get(0) == i.get(2) && i.get(0) != i.get(1))
                 .toList();
             
-            final var invertedOutsideSequences = new ArrayList<List<Character>>();
-            
-            for (final var list : outsideSequences) {
-                final var inverted = new ArrayList<Character>();
-                
-                inverted.add(list.get(1));
-                inverted.add(list.get(0));
-                inverted.add(list.get(1));
-                
-                invertedOutsideSequences.add(inverted);
-            }
+            final var outsideBabs = outsideAbas
+                .stream()
+                .map(list -> {
+                    final var inverted = new ArrayList<Character>();
+                    
+                    inverted.add(list.get(1));
+                    inverted.add(list.get(0));
+                    inverted.add(list.get(1));
+                    
+                    return inverted;
+                })
+                .toList();
             
             final var supportsSsl = pair.inside()
                 .stream()
                 .gather(Gatherers.windowSliding(3))
-                .anyMatch(i -> invertedOutsideSequences.contains(i.subList(0, i.size())));
+                .anyMatch(i -> outsideBabs.contains(i.subList(0, i.size())));
             
             if (supportsSsl) {
                 count++;
@@ -101,5 +102,5 @@ public class Day07 implements Solver<Integer> {
         return count;
     }
     
-    private record Sequence(ArrayList<Character> outside, ArrayList<Character> inside) {}
+    private record Sequence(List<Character> outside, List<Character> inside) {}
 }
