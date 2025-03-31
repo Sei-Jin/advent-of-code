fceaee7ccc5a4fce53a56e8fdd62948cbc48357f
Sei-Jin <seijin.tufts@gmail.com>
2025-03-31 19:39:31 -0300

    [2019-03] Update test cases to use constructor

--- src/test/java/aoc/event/year2019/Day03Test.java
+++ src/test/java/aoc/event/year2019/Day03Test.java
@@ -2,55 +2,51 @@ package aoc.event.year2019;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day03Test
-{
+public class Day03Test {
+    
     @Test
-    void exampleOne()
-    {
-        List<String> inputLines = new ArrayList<>(List.of(
-                "R8,U5,L5,D3",
-                "U7,R6,D4,L4"
-        ));
-        assertEquals(6, new Day03().partOne(inputLines));
-        assertEquals(30, new Day03().partTwo(inputLines));
+    void example1() {
+        final var input = """
+            R8,U5,L5,D3
+            U7,R6,D4,L4
+            """;
+        final var solution = new Day03(input);
+        assertEquals(6, solution.partOne());
+        assertEquals(30, solution.partTwo());
     }
     
     @Test
-    void exampleTwo()
-    {
-        List<String> inputLines = new ArrayList<>(List.of(
-                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
-                "U62,R66,U55,R34,D71,R55,D58,R83"
-        ));
-        assertEquals(159, new Day03().partOne(inputLines));
-        assertEquals(610, new Day03().partTwo(inputLines));
+    void example2() {
+        final var input = """
+            R75,D30,R83,U83,L12,D49,R71,U7,L72
+            U62,R66,U55,R34,D71,R55,D58,R83
+            """;
+        final var solution = new Day03(input);
+        assertEquals(159, solution.partOne());
+        assertEquals(610, solution.partTwo());
     }
     
     @Test
-    void exampleThree()
-    {
-        List<String> inputLines = new ArrayList<>(List.of(
-                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-        ));
-        assertEquals(135, new Day03().partOne(inputLines));
-        assertEquals(410, new Day03().partTwo(inputLines));
+    void example3() {
+        final var input = """
+            R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+            """;
+        final var solution = new Day03(input);
+        assertEquals(135, solution.partOne());
+        assertEquals(410, solution.partTwo());
     }
     
     @Test
-    void tinyWireIntersection()
-    {
-        List<String> inputLines = new ArrayList<>(Arrays.asList(
-                "U2,R2",
-                "R2,U2"
-        ));
-        assertEquals(4, new Day03().partOne(inputLines));
-        assertEquals(8, new Day03().partTwo(inputLines));
+    void tinyWireIntersection() {
+        final var input = """
+            U2,R2
+            R2,U2
+            """;
+        final var solution = new Day03(input);
+        assertEquals(4, solution.partOne());
+        assertEquals(8, solution.partTwo());
     }
 }
