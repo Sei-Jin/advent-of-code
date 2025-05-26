aaae85fbc51d18c16ff38c497244e8618087e739
Sei-Jin <seijin.tufts@gmail.com>
2025-05-26 19:35:40 -0300

    [2024-03] Update solution

--- src/main/java/aoc/event/year2024/Day03.java
+++ src/main/java/aoc/event/year2024/Day03.java
@@ -1,180 +1,81 @@
 package aoc.event.year2024;
 
-import aoc.DeprecatedSolver;
+import aoc.Solver;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
-public class Day03 implements DeprecatedSolver
-{
-    /// This enum stores the regular expression for each type of instruction.
-    enum Instruction
-    {
+/// # [2024-03: Mull It Over](https://adventofcode.com/2024/day/3)
+public class Day03 implements Solver<Integer, Integer> {
+    
+    private static final Pattern inputPattern = Pattern.compile(
+        Instruction.MULTIPLY.getRegex()
+            + "|" + Instruction.DO.getRegex()
+            + "|" + Instruction.DO_NOT.getRegex()
+    );
+    private static final Pattern numberPattern = Pattern.compile("\\d+");
+    private final List<String> instructions;
+    
+    public Day03(String input) {
+        instructions = parse(input);
+    }
+    
+    private static List<String> parse(String input) {
+        return inputPattern
+            .matcher(input)
+            .results()
+            .map(MatchResult::group)
+            .toList();
+    }
+    
+    private static int calculateProduct(String instruction) {
+        return numberPattern
+            .matcher(instruction)
+            .results()
+            .mapToInt(result -> Integer.parseInt(result.group()))
+            .reduce(1, (a, b) -> a * b);
+    }
+    
+    @Override
+    public Integer partOne() {
+        return instructions
+            .stream()
+            .mapToInt(Day03::calculateProduct)
+            .sum();
+    }
+    
+    @Override
+    public Integer partTwo() {
+        var sum = 0;
+        var enableMultiply = true;
+        
+        for (var instruction : instructions) {
+            if (instruction.matches(Instruction.DO_NOT.getRegex())) {
+                enableMultiply = false;
+            } else if (instruction.matches(Instruction.DO.getRegex())) {
+                enableMultiply = true;
+            } else if (enableMultiply) {
+                sum += calculateProduct(instruction);
+            }
+        }
+        
+        return sum;
+    }
+    
+    private enum Instruction {
         MULTIPLY("mul\\(\\d+,\\d+\\)"),
         DO("do\\(\\)"),
         DO_NOT("don't\\(\\)");
         
         private final String regex;
         
-        Instruction(String regex)
-        {
+        Instruction(String regex) {
             this.regex = regex;
         }
         
-        public String getRegex()
-        {
+        public String getRegex() {
             return regex;
         }
-    }
-    
-    /// Creates a pattern matcher for the given section of the program.
-    ///
-    /// The puzzle input represents a program with corrupted memory and contains a series of
-    /// multiplication instructions to follow. A multiplication instruction is represented as
-    /// `mul(X,Y)` and multiplies the two numbers `X` and `Y`.
-    ///
-    /// The pattern that is matched depends on if all multiplications are enabled or not. If not all
-    /// multiplications are enabled then `do` and `don't` instructions should also be matched.
-    ///
-    /// @param programSection a section of the program.
-    /// @param allMultiplicationsEnabled true if all multiplication instructions are
-    ///         enabled, false if otherwise.
-    /// @return a pattern matcher for the section of the program.
-    private static Matcher createPatternMatcher(
-            String programSection,
-            boolean allMultiplicationsEnabled)
-    {
-        StringBuilder regex = new StringBuilder(Instruction.MULTIPLY.getRegex());
-        
-        if (!allMultiplicationsEnabled)
-        {
-            regex.append("|");
-            regex.append(Instruction.DO_NOT.getRegex());
-            regex.append("|");
-            regex.append(Instruction.DO.getRegex());
-        }
-        
-        Pattern pattern = Pattern.compile(regex.toString());
-        return pattern.matcher(programSection);
-    }
-    
-    /// Returns the list of valid instructions from a section of the program.
-    ///
-    /// A valid instruction is any instruction that is matched by the pattern matcher.
-    ///
-    /// @param patternMatcher a matcher for valid instructions.
-    /// @return the list of valid instructions.
-    private static List<String> parseValidInstructions(Matcher patternMatcher)
-    {
-        List<String> instructions = new ArrayList<>();
-        
-        while (patternMatcher.find())
-        {
-            String instruction = patternMatcher.group();
-            instructions.add(instruction);
-        }
-        
-        return instructions;
-    }
-    
-    /// Calculates the product of a multiplication instruction.
-    ///
-    /// A multiplication instruction is in the form `mul(X,Y)`, where `X` and `Y` are the two
-    /// numbers to be multiplied together.
-    ///
-    /// @param instruction a multiplication instruction.
-    /// @return the product of the instruction.
-    private static int calculateProduct(String instruction)
-    {
-        int product = 1;
-        
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(instruction);
-        
-        while (matcher.find())
-        {
-            int factor = Integer.parseInt(matcher.group());
-            product *= factor;
-        }
-        
-        return product;
-    }
-    
-    /// Calculates the sum of all multiplication instructions in program memory.
-    ///
-    /// Time Complexity: O(n)
-    /// - One pass is completed over the puzzle input to find and store the instructions. Then each
-    /// instruction is executed sequentially.
-    ///
-    /// Space Complexity: O(n)
-    /// - All instructions from the puzzle input are stored. If the entire puzzle input consists of
-    /// instructions, then the entire puzzle input will be stored.
-    ///
-    /// @param programMemory the puzzle input.
-    /// @return the total sum of all multiplication instructions.
-    @Override
-    public Object partOne(List<String> programMemory)
-    {
-        int sumOfProducts = 0;
-        
-        for (String programSection : programMemory)
-        {
-            Matcher patternMatcher = createPatternMatcher(programSection, true);
-            List<String> instructions = parseValidInstructions(patternMatcher);
-            
-            for (String instruction : instructions)
-            {
-                sumOfProducts += calculateProduct(instruction);
-            }
-        }
-        
-        return sumOfProducts;
-    }
-    
-    /// Calculates the sum of all ***enabled*** multiplication instructions in program memory.
-    ///
-    /// - The program starts with instructions enabled.
-    /// - A `don't` instruction disables the following instructions.
-    /// - A `do` instruction enables the following instructions.
-    ///
-    /// Time Complexity: O(n)
-    /// - Same as part one.
-    ///
-    /// Space Complexity: O(n)
-    /// - Same as part one.
-    ///
-    /// @param programMemory the puzzle input.
-    /// @return the total sum of all enabled multiplication instructions.
-    @Override
-    public Object partTwo(List<String> programMemory)
-    {
-        int sumOfProducts = 0;
-        boolean enableMultiplications = true;
-        
-        for (String programSection : programMemory)
-        {
-            Matcher patternMatcher = createPatternMatcher(programSection, false);
-            List<String> instructions = parseValidInstructions(patternMatcher);
-            
-            for (String instruction : instructions)
-            {
-                if (instruction.matches(Instruction.DO_NOT.regex))
-                {
-                    enableMultiplications = false;
-                }
-                else if (instruction.matches(Instruction.DO.regex))
-                {
-                    enableMultiplications = true;
-                }
-                else if (enableMultiplications)
-                {
-                    sumOfProducts += calculateProduct(instruction);
-                }
-            }
-        }
-        
-        return sumOfProducts;
     }
 }
--- src/test/java/aoc/event/year2024/Day03Test.java
+++ src/test/java/aoc/event/year2024/Day03Test.java
@@ -1,33 +1,22 @@
 package aoc.event.year2024;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day03Test
-{
-    @Nested
-    class PartOne
-    {
-        @Test
-        void example()
-        {
-            String programMemory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-            assertEquals(161, new Day03().partOne(Collections.singletonList(programMemory)));
-        }
+public class Day03Test {
+    
+    @Test
+    void examplePart1() {
+        var input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        var solution = new Day03(input);
+        assertEquals(161, solution.partOne());
     }
     
-    @Nested
-    class PartTwo
-    {
-        @Test
-        void example()
-        {
-            String programMemory = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-            assertEquals(48, new Day03().partTwo(Collections.singletonList(programMemory)));
-        }
+    @Test
+    void examplePart2() {
+        var input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        var solution = new Day03(input);
+        assertEquals(48, solution.partTwo());
     }
 }
