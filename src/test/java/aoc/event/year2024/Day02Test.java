b89ba53398028f21f9eb6565029fe8764fee82c4
Sei-Jin <seijin.tufts@gmail.com>
2025-05-26 18:24:13 -0300

    [2024-02] Update test cases

--- src/test/java/aoc/event/year2024/Day02Test.java
+++ src/test/java/aoc/event/year2024/Day02Test.java
@@ -2,73 +2,70 @@ package aoc.event.year2024;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day02Test
-{
+public class Day02Test {
+    
     @Test
-    void exampleReport1()
-    {
-        List<String> report  = new ArrayList<>(List.of("7 6 4 2 1"));
-        assertEquals(1, new Day02().partOne(report));
-        assertEquals(1, new Day02().partTwo(report));
+    void exampleReport1() {
+        var input = "7 6 4 2 1";
+        var solution = new Day02(input);
+        assertEquals(1, solution.partOne());
+        assertEquals(1, solution.partTwo());
     }
     
     @Test
-    void exampleReport2()
-    {
-        List<String> report  = new ArrayList<>(List.of("1 2 7 8 9"));
-        assertEquals(0, new Day02().partOne(report));
-        assertEquals(0, new Day02().partTwo(report));
+    void exampleReport2() {
+        var input = "1 2 7 8 9";
+        var solution = new Day02(input);
+        assertEquals(0, solution.partOne());
+        assertEquals(0, solution.partTwo());
     }
     
     @Test
-    void exampleReport3()
-    {
-        List<String> report  = new ArrayList<>(List.of("9 7 6 2 1"));
-        assertEquals(0, new Day02().partOne(report));
-        assertEquals(0, new Day02().partTwo(report));
+    void exampleReport3() {
+        var input = "9 7 6 2 1";
+        var solution = new Day02(input);
+        assertEquals(0, solution.partOne());
+        assertEquals(0, solution.partTwo());
     }
     
     @Test
-    void exampleReport4()
-    {
-        List<String> report  = new ArrayList<>(List.of("1 3 2 4 5"));
-        assertEquals(0, new Day02().partOne(report));
-        assertEquals(1, new Day02().partTwo(report));
+    void exampleReport4() {
+        var input = "1 3 2 4 5";
+        var solution = new Day02(input);
+        assertEquals(0, solution.partOne());
+        assertEquals(1, solution.partTwo());
     }
     
     @Test
-    void exampleReport5()
-    {
-        List<String> report  = new ArrayList<>(List.of("8 6 4 4 1"));
-        assertEquals(0, new Day02().partOne(report));
-        assertEquals(1, new Day02().partTwo(report));
+    void exampleReport5() {
+        var input = "8 6 4 4 1";
+        var solution = new Day02(input);
+        assertEquals(0, solution.partOne());
+        assertEquals(1, solution.partTwo());
     }
     
     @Test
-    void exampleReport6()
-    {
-        List<String> report  = new ArrayList<>(List.of("1 3 6 7 9"));
-        assertEquals(1, new Day02().partOne(report));
-        assertEquals(1, new Day02().partTwo(report));
+    void exampleReport6() {
+        var input = "1 3 6 7 9";
+        var solution = new Day02(input);
+        assertEquals(1, solution.partOne());
+        assertEquals(1, solution.partTwo());
     }
     
     @Test
-    void allExampleReports()
-    {
-        List<String> reports  = new ArrayList<>(List.of(
-                "7 6 4 2 1",
-                "1 2 7 8 9",
-                "9 7 6 2 1",
-                "1 3 2 4 5",
-                "8 6 4 4 1",
-                "1 3 6 7 9"
-        ));
-        assertEquals(2, new Day02().partOne(reports));
-        assertEquals(4, new Day02().partTwo(reports));
+    void allExampleReports() {
+        var input = """
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+            """;
+        var solution = new Day02(input);
+        assertEquals(2, solution.partOne());
+        assertEquals(4, solution.partTwo());
     }
 }
