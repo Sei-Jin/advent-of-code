40330fa6f5f3c2c430e8c802c2c8b177f8483799
Sei-Jin <seijin.tufts@gmail.com>
2025-05-26 18:24:49 -0300

    [2024-01] Update test case

--- src/test/java/aoc/event/year2024/Day01Test.java
+++ src/test/java/aoc/event/year2024/Day01Test.java
@@ -4,21 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day01Test
-{
+public class Day01Test {
+    
     @Test
-    void exampleList()
-    {
-        final var input = """
-                3   4
-                4   3
-                2   5
-                1   3
-                3   9
-                3   3
-                """;
-        
-        final var solution = new Day01(input);
+    void exampleList() {
+        var input = """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+            """;
+        var solution = new Day01(input);
         assertEquals(11, solution.partOne());
         assertEquals(31, solution.partTwo());
     }
