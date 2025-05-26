326a8b964be8cf3b1a520f9e7c0621dc75740044
Sei-Jin <seijin.tufts@gmail.com>
2025-05-25 23:58:54 -0300

    [2024-01] Improve solution

--- src/main/java/aoc/event/year2024/Day01.java
+++ src/main/java/aoc/event/year2024/Day01.java
@@ -3,138 +3,72 @@ package aoc.event.year2024;
 import aoc.Solver;
 
 import java.util.*;
-import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
-/// 2024 Day 1 - Historian Hysteria
-public class Day01 implements Solver {
+/// # [2024-01: Historian Hysteria](https://adventofcode.com/2024/day/1)
+public class Day01 implements Solver<Integer, Integer> {
     
-    /// Pattern for each line of the puzzle input
-    private static final Pattern LINE_PATTERN = Pattern.compile("(\\d+)\\s+(\\d+)");
+    private final IdLists idLists;
     
-    /// Stores the id lists.
-    private static IdLists idLists;
-    
-    /// Initializes the solution with the parsed puzzle input.
     public Day01(String input) {
         idLists = parse(input);
     }
     
-    /// Parses the puzzle input for the ids in each list.
-    ///
-    /// The puzzle input is in the form:
-    ///
-    /// ```
-    /// A...B
-    /// A...B
-    /// A...B
-    ///```
-    ///
-    /// Where:
-    /// - Each `.` represents a space and should be ignored.
-    /// - Each `A` represents an id in the first list.
-    /// - Each `B` represents an id in the second list.
-    ///
-    /// The puzzle input is parsed line by line, where the first id is placed in the first list
-    /// and the second id is placed in the second list. Also note that the ids can be longer
-    /// than a single digit.
-    ///
-    /// @param input the puzzle input.
     private IdLists parse(String input) {
         var first = new ArrayList<Integer>();
         var second = new ArrayList<Integer>();
         
-        input.lines().forEach(line -> {
-            var matcher = LINE_PATTERN.matcher(line);
-            
-            if (matcher.find()) {
-                first.add(Integer.valueOf(matcher.group(1)));
-                second.add(Integer.valueOf(matcher.group(2)));
-            } else {
-                throw new IllegalArgumentException(
-                        String.format("Unexpected format in input: %s", line)
-                );
-            }
-        });
+        input
+            .lines()
+            .forEach(line -> {
+                var parts = line.split("\\s+");
+                first.add(Integer.parseInt(parts[0]));
+                second.add(Integer.parseInt(parts[1]));
+            });
         
-        return new IdLists(first, second);
+        return new IdLists(
+            Collections.unmodifiableList(first),
+            Collections.unmodifiableList(second)
+        );
     }
     
-    /// Calculates the total distance between the ids of the two lists.
-    ///
-    /// The total distance is calculated by comparing the ids in each list from smallest to
-    /// largest, finding the difference between them, and adding it to a running total.
-    ///
-    /// Time Complexity: O(n log n)
-    /// - The id lists are sorted when calculating the total distance.
-    ///
-    /// Space Complexity: O(n)
-    /// - All ids from the puzzle input are parsed and then stored.
-    ///
-    /// @return the total distance between the two lists of ids.
     @Override
     public Integer partOne() {
-        Collections.sort(idLists.first());
-        Collections.sort(idLists.second());
+        var mutableFirst = new ArrayList<>(idLists.first());
+        var mutableSecond = new ArrayList<>(idLists.second());
         
-        var totalDistance = 0;
+        Collections.sort(mutableFirst);
+        Collections.sort(mutableSecond);
         
-        for (var index = 0; index < idLists.first().size(); index++) {
-            var first = idLists.first().get(index);
-            var second = idLists.second().get(index);
-            totalDistance += Math.abs(first - second);
-        }
-        
-        return totalDistance;
+        return IntStream.range(0, mutableFirst.size())
+            .map(i -> {
+                var first = mutableFirst.get(i);
+                var second = mutableSecond.get(i);
+                return Math.abs(first - second);
+            })
+            .sum();
     }
     
-    /// Calculates the similarity score between the two lists of ids.
-    ///
-    /// To calculate the similarity score between the two lists, the ids in one list are
-    /// multiplied by their frequency in the other list and added to a running total.
-    ///
-    /// Here each id in the first list is multiplied by its frequency in the second list, but
-    /// the calculation could have been done the other way around, where each id in the second
-    /// list is multiplied by its frequency in the first.
-    ///
-    /// Time Complexity: O(n)
-    /// - All methods are done in linear or constant time.
-    ///
-    /// Space Complexity: O(n)
-    /// - The entire puzzle input is stored.
-    ///
-    /// @return the similarity score between the two lists of ids.
     @Override
     public Integer partTwo() {
-        var frequencies = calculateFrequencies(idLists.second);
-        
-        var similarityScore = 0;
-        
-        for (var id : idLists.first) {
-            var frequency = frequencies.getOrDefault(id, 0);
-            similarityScore += id * frequency;
-        }
-        
-        return similarityScore;
+        var secondFrequencies = calculateFrequencies(idLists.second());
+        return idLists.first()
+            .stream()
+            .mapToInt(id -> {
+                var frequency = secondFrequencies.getOrDefault(id, 0);
+                return id * frequency;
+            })
+            .sum();
     }
     
-    /// Calculates a frequency map of the numbers in the list.
-    ///
-    /// @param list a list.
-    /// @return a frequency map of the numbers in the list.
     private static Map<Integer, Integer> calculateFrequencies(List<Integer> list) {
         var frequencies = new HashMap<Integer, Integer>();
-        
         for (var number : list) {
             var frequency = frequencies.getOrDefault(number, 0) + 1;
             frequencies.put(number, frequency);
         }
-        
         return frequencies;
     }
     
-    /// Stores the data for the two lists of ids.
-    ///
-    /// @param first  the first list of ids.
-    /// @param second the second list of ids.
     private record IdLists(List<Integer> first, List<Integer> second) {}
 }
