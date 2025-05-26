e196c90c6acb1815da497eb06649be784c201e05
Sei-Jin <seijin.tufts@gmail.com>
2025-05-26 18:24:20 -0300

    [2024-02] Complete part 2

--- src/main/java/aoc/event/year2024/Day02.java
+++ src/main/java/aoc/event/year2024/Day02.java
@@ -2,9 +2,11 @@ package aoc.event.year2024;
 
 import aoc.Solver;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Gatherers;
+import java.util.stream.IntStream;
 
 /// # [2024-02: Red-Nosed Reports](https://adventofcode.com/2024/day/2)
 public class Day02 implements Solver<Integer, Integer> {
@@ -30,32 +32,43 @@ public class Day02 implements Solver<Integer, Integer> {
     public Integer partOne() {
         return (int) reports
             .stream()
-            .filter(report -> {
-                var allIncreasing = report
-                    .stream()
-                    .gather(Gatherers.windowSliding(2))
-                    .noneMatch(list -> list.get(0) >= list.get(1));
-                
-                var allDecreasing = report
-                    .stream()
-                    .gather(Gatherers.windowSliding(2))
-                    .noneMatch(list -> list.get(0) <= list.get(1));
-                
-                var safeDifference = report
-                    .stream()
-                    .gather(Gatherers.windowSliding(2))
-                    .noneMatch(list -> {
-                        var difference = Math.abs(list.get(0) - list.get(1));
-                        return (difference < 1 || difference > 3);
-                    });
-                
-                return (allDecreasing || allIncreasing) && safeDifference;
-            })
+            .filter(Day02::safe)
             .count();
+    }
+    
+    private static boolean safe(List<Integer> report) {
+        var allIncreasing = report
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .noneMatch(list -> list.get(0) >= list.get(1));
+        
+        var allDecreasing = report
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .noneMatch(list -> list.get(0) <= list.get(1));
+        
+        var safeDifference = report
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .noneMatch(list -> {
+                var difference = Math.abs(list.get(0) - list.get(1));
+                return (difference < 1 || difference > 3);
+            });
+        
+        return (allDecreasing || allIncreasing) && safeDifference;
     }
     
     @Override
     public Integer partTwo() {
-        return 0;
+        return (int) reports
+            .stream()
+            .filter(report ->
+                IntStream.range(0, report.size())
+                    .anyMatch(i -> {
+                        var reportCopy = new ArrayList<>(report);
+                        reportCopy.remove(i);
+                        return safe(reportCopy);
+                    }))
+            .count();
     }
 }
