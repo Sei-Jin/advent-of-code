27fdd347ce7ae72c0d25ead6c52f2455ac16e144
Sei-Jin <seijin.tufts@gmail.com>
2025-05-28 16:18:39 -0300

    [2024-4] Update solution

--- src/main/java/aoc/event/year2024/Day04.java
+++ src/main/java/aoc/event/year2024/Day04.java
@@ -1,142 +1,107 @@
 package aoc.event.year2024;
 
-import aoc.DeprecatedSolver;
+import aoc.Solver;
 
-import java.util.List;
+import java.util.Arrays;
 
 /// # [2024-04: Ceres Search](https://adventofcode.com/2024/day/4)
-public class Day04 implements DeprecatedSolver
-{
-    /// The target string.
-    final String TARGET = "XMAS";
+public class Day04 implements Solver<Integer, Integer> {
     
-    /// Counts the number of strings in the word search that match the string `XMAS`.
-    ///
-    /// The string `XMAS` can be horizontal, vertical or diagonal in the word search.
-    ///
-    /// @param inputLines the puzzle input.
-    /// @return the number of strings that match `XMAS` in the word search.
-    @Override
-    public Object partOne(List<String> inputLines)
-    {
-        char[][] wordSearch = getWordSearch(inputLines);
-        
-        int totalMatchingStrings = 0;
-        
-        for (int rowIndex = 0; rowIndex < wordSearch.length; rowIndex++)
-        {
-            for (int columnIndex = 0; columnIndex < wordSearch[rowIndex].length; columnIndex++)
-            {
-                totalMatchingStrings += countMatchingStringsAtPosition(rowIndex, columnIndex, wordSearch);
-            }
-        }
-        
-        return totalMatchingStrings;
+    private static char[][] wordSearch;
+    private static final String TARGET = "XMAS";
+    private static final Direction[] possibleDirections = {
+        new Direction(1, 1),
+        new Direction(0, 1),
+        new Direction(-1, 1),
+        new Direction(-1, 0),
+        new Direction(-1, -1),
+        new Direction(0, -1),
+        new Direction(1, -1),
+        new Direction(1, 0),
+    };
+    
+    public Day04(String input) {
+        wordSearch = parse(input);
     }
     
-    /// Converts the puzzle input into a two-dimensional array of characters.
-    ///
-    /// @param inputLines the puzzle input
-    /// @return the puzzle input as a two-dimensional array of characters.
-    private static char[][] getWordSearch(List<String> inputLines)
-    {
-        char[][] wordSearch = new char[inputLines.size()][inputLines.getFirst().length()];
+    private static char[][] parse(String input) {
+        var lines = input.lines().toList();
         
-        for (int row = 0; row < inputLines.size(); row++)
-        {
-            for (int column = 0; column < inputLines.getFirst().length(); column++)
-            {
-                wordSearch[row][column] = inputLines.get(row).charAt(column);
+        var rowSize = lines.size();
+        var columnSize = lines.getFirst().length();
+        
+        char[][] wordSearch = new char[rowSize][columnSize];
+        for (int row = 0; row < rowSize; row++) {
+            for (int column = 0; column < columnSize; column++) {
+                wordSearch[row][column] = lines.get(row).charAt(column);
             }
         }
-        
         return wordSearch;
     }
     
+    /// Counts the number of strings in the word search that match the target string.
+    ///
+    /// The target string can be horizontal, vertical or diagonal in the word search.
+    @Override
+    public Integer partOne() {
+        int sum = 0;
+        for (int row = 0; row < wordSearch.length; row++) {
+            for (int column = 0; column < wordSearch[row].length; column++) {
+                var position = new Position(row, column);
+                sum += countMatchingInAllDirections(position, wordSearch);
+            }
+        }
+        return sum;
+    }
+    
     /// Calculates the number of matching strings in all 8 directions from the current position.
-    ///
-    /// @param rowIndex the row of the current position.
-    /// @param columnIndex the column of the current position.
-    /// @param wordSearch the word search.
-    /// @return the number of matching strings in any direction at the current position.
-    private int countMatchingStringsAtPosition(int rowIndex, int columnIndex, char[][] wordSearch)
-    {
-        int matchingStringsAtPosition = 0;
-        
-        for (int rowDirection = -1; rowDirection <= 1; rowDirection++)
-        {
-            for (int columnDirection = -1; columnDirection <= 1; columnDirection++)
-            {
-                if (rowDirection == 0 && columnDirection == 0)
-                {
-                    continue;
-                }
-                
-                if (isValidDirection(rowIndex, columnIndex, rowDirection, columnDirection, wordSearch))
-                {
-                    if (isMatchingInDirection(rowIndex, columnIndex, rowDirection, columnDirection, wordSearch))
-                    {
-                        matchingStringsAtPosition++;
-                    }
-                }
-            }
-        }
-        
-        return matchingStringsAtPosition;
+    private int countMatchingInAllDirections(Position position, char[][] wordSearch) {
+        return (int) Arrays
+            .stream(possibleDirections)
+            .map(direction -> new Search(position, direction))
+            .filter(search -> isValidDirection(search, wordSearch))
+            .filter(search -> isMatching(search, wordSearch))
+            .count();
     }
     
-    /// Returns true if the word can fit on the word search grid in current direction, false if otherwise.
-    ///
-    /// The word can fit on the word search grid if the last character will be within the bounds of the grid in
-    /// the current direction.
-    ///
-    /// @param rowIndex the row of the current position.
-    /// @param columnIndex the column of the current position.
-    /// @param rowDirection the current direction row-wise.
-    /// @param columnDirection the current direction column-wise.
-    /// @param wordSearch the word search.
-    /// @return true if the word can fit in the current direction, false if otherwise.
-    private boolean isValidDirection(int rowIndex, int columnIndex, int rowDirection, int columnDirection, char[][] wordSearch)
-    {
-        int lastRowIndex = rowIndex + rowDirection * (TARGET.length() - 1);
-        int lastColumnIndex = columnIndex + columnDirection * (TARGET.length() - 1);
+    /// The current direction is valid if the word will stay within the bounds of the word search.
+    private boolean isValidDirection(Search search, char[][] wordSearch) {
+        var position = search.position();
+        var direction = search.direction();
         
-        return (lastRowIndex >= 0) &&
-                (lastRowIndex < wordSearch.length) &&
-                (lastColumnIndex >= 0) &&
-                (lastColumnIndex < wordSearch[rowIndex].length);
+        int endRow = position.row() + direction.row() * (TARGET.length() - 1);
+        int endColumn = position.column() + direction.column() * (TARGET.length() - 1);
+        
+        return (endRow >= 0)
+            && (endRow < wordSearch.length)
+            && (endColumn >= 0)
+            && (endColumn < wordSearch[position.row()].length);
     }
     
-    /// Returns true if the word matches the target string `XMAS` in the current direction, false if otherwise.
-    ///
-    /// @param rowIndex the row of the current position.
-    /// @param columnIndex the column of the current position.
-    /// @param rowDirection the current direction row-wise.
-    /// @param columnDirection the current direction column-wise.
-    /// @param wordSearch the word search.
-    /// @return true if the word matches, false if otherwise.
-    boolean isMatchingInDirection(int rowIndex, int columnIndex, int rowDirection, int columnDirection, char[][] wordSearch)
-    {
-        boolean matchingString = true;
+    /// Determines if the word matches the target string.
+    boolean isMatching(Search search, char[][] wordSearch) {
+        var position = search.position();
+        var direction = search.direction();
         
-        for (int wordIndex = 0; wordIndex < TARGET.length(); wordIndex++)
-        {
-            int newRowIndex = rowIndex + wordIndex * rowDirection;
-            int newColumnIndex = columnIndex + wordIndex * columnDirection;
+        for (int i = 0; i < TARGET.length(); i++) {
+            int newRow = position.row() + i * direction.row();
+            int newColumn = position.column() + i * direction.column();
             
-            if (wordSearch[newRowIndex][newColumnIndex] != TARGET.charAt(wordIndex))
-            {
-                matchingString = false;
-                break;
+            if (wordSearch[newRow][newColumn] != TARGET.charAt(i)) {
+                return false;
             }
         }
-        
-        return matchingString;
+        return true;
     }
     
     @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        return null;
+    public Integer partTwo() {
+        return 0;
     }
+    
+    private record Position(int row, int column) {}
+    
+    private record Direction(int row, int column) {}
+    
+    private record Search(Position position, Direction direction) {}
 }
--- src/test/java/aoc/event/year2024/Day04Test.java
+++ src/test/java/aoc/event/year2024/Day04Test.java
@@ -2,31 +2,26 @@ package aoc.event.year2024;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day04Test
-{
+public class Day04Test {
+    
     @Test
-    void example()
-    {
-        List<String> input = new ArrayList<>(Arrays.stream(
-                """
-                MMMSXXMASM
-                MSAMXMSMSA
-                AMXSXMAAMM
-                MSAMASMSMX
-                XMASAMXAMM
-                XXAMMXXAMA
-                SMSMSASXSS
-                SAXAMASAAA
-                MAMMMXMMMM
-                MXMXAXMASX
-                """.split("\n")).toList()
-        );
-        assertEquals(18, new Day04().partOne(input));
+    void example() {
+        var input = """
+            MMMSXXMASM
+            MSAMXMSMSA
+            AMXSXMAAMM
+            MSAMASMSMX
+            XMASAMXAMM
+            XXAMMXXAMA
+            SMSMSASXSS
+            SAXAMASAAA
+            MAMMMXMMMM
+            MXMXAXMASX
+            """;
+        var solution = new Day04(input);
+        assertEquals(18, solution.partOne());
+        assertEquals(9, solution.partTwo());
     }
 }
