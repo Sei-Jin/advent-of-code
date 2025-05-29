1f134c035eaf820f05f1637cecf7428c4e13cf37
Sei-Jin <seijin.tufts@gmail.com>
2025-05-28 22:14:39 -0300

    [2024-05] Refactor solution

--- src/main/java/aoc/event/year2024/Day05.java
+++ src/main/java/aoc/event/year2024/Day05.java
@@ -1,209 +1,131 @@
 package aoc.event.year2024;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /// # [2024-05: Print Queue](https://adventofcode.com/2024/day/5)
-public class Day05 implements DeprecatedSolver2 {
+public class Day05 implements Solver<Integer, Integer> {
     
-    private final Map<Integer, Set<Integer>> orderingRules;
+    private final Map<Integer, Set<Integer>> rules;
     private final List<List<Integer>> updates;
     
     public Day05(String input) {
         var lines = input.lines().toList();
-        var emptyLineIndex = getEmptyLineIndex(lines);
+        var emptyLineIndex = findEmptyLineIndex(lines);
         
-        var orderingRulesInput = lines.subList(0, emptyLineIndex);
+        var rulesInput = lines.subList(0, emptyLineIndex);
         var updatesInput = lines.subList(emptyLineIndex + 1, lines.size());
         
-        orderingRules = parseOrderingRules(orderingRulesInput);
+        rules = parseRules(rulesInput);
         updates = parseUpdates(updatesInput);
     }
     
-    /// Finds the index of the first empty line in the puzzle input if it exists.
-    ///
-    /// @param inputLines the puzzle input.
-    /// @return the index of the first empty line in the puzzle input.
-    /// @throws IllegalArgumentException if the puzzle input does not contain any empty lines.
-    private static int getEmptyLineIndex(List<String> inputLines) {
-        for (var index = 0; index < inputLines.size(); index++) {
-            if (inputLines.get(index).isEmpty()) {
-                return index;
-            }
-        }
-        
-        throw new IllegalArgumentException("The puzzle input does not contain any empty lines.");
+    private static int findEmptyLineIndex(List<String> lines) {
+        return IntStream.range(0, lines.size())
+            .filter(i -> lines.get(i).isEmpty())
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("No empty lines found."));
     }
     
-    /// Parses the first section of the puzzle input for the ordering rules.
-    ///
-    /// @param orderingRulesInput the first section of the puzzle input, containing the ordering
-    /// @return the ordering rules.
-    private static Map<Integer, Set<Integer>> parseOrderingRules(
-        List<String> orderingRulesInput
-    ) {
-        var orderingRules = new HashMap<Integer, Set<Integer>>();
-        
-        for (var rule : orderingRulesInput) {
-            var pageNumbers = Arrays.stream(rule.split("\\|"))
+    private static Map<Integer, Set<Integer>> parseRules(List<String> rulesInput) {
+        var rules = new HashMap<Integer, Set<Integer>>();
+        for (var rule : rulesInput) {
+            var parts = Arrays.stream(rule.split("\\|"))
                 .mapToInt(Integer::parseInt)
-                .boxed()
-                .toList();
+                .toArray();
             
-            var precedingNumber = pageNumbers.getFirst();
-            var succeedingNumber = pageNumbers.getLast();
+            var predecessor = parts[0];
+            var successor = parts[1];
             
-            if (orderingRules.containsKey(precedingNumber)) {
-                orderingRules.get(precedingNumber).add(succeedingNumber);
+            if (rules.containsKey(predecessor)) {
+                rules.get(predecessor).add(successor);
             } else {
-                var succeedingNumbers = new HashSet<Integer>();
-                succeedingNumbers.add(succeedingNumber);
-                orderingRules.put(precedingNumber, succeedingNumbers);
+                var successors = new HashSet<Integer>();
+                successors.add(successor);
+                rules.put(predecessor, successors);
             }
         }
-        
-        return orderingRules;
+        return rules;
     }
     
-    /// Parses the second section of the puzzle input for the list of updates.
-    ///
-    /// @param updatesInput the second section of the puzzle input, containing the list of updates.
-    /// @return the list of updates.
     private static List<List<Integer>> parseUpdates(List<String> updatesInput) {
-        var updates = new ArrayList<List<Integer>>();
-        
-        for (var input : updatesInput) {
-            var update = Arrays.stream(input.split(","))
-                .mapToInt(Integer::parseInt)
-                .boxed()
-                .collect(Collectors.toList());
-            
-            updates.add(update);
-        }
-        
-        return updates;
+        return updatesInput
+            .stream()
+            .map(input ->
+                Arrays
+                    .stream(input.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList()))
+            .toList();
     }
     
-    /// Calculates the sum of all the middle page numbers in the list of updates.
-    ///
-    /// @param updates a list of updates.
-    /// @return the sum of all the middle page numbers in the list of updates.
-    private static int calculateMiddlePageNumberSum(List<List<Integer>> updates) {
-        var sumMiddlePageNumbers = 0;
-        
-        for (var update : updates) {
-            int middleIndex = update.size() / 2;
-            sumMiddlePageNumbers += update.get(middleIndex);
-        }
-        
-        return sumMiddlePageNumbers;
-    }
-    
-    /// Calculates the sum of the middle page numbers for valid updates.
-    ///
-    /// @return the sum of the middle page numbers for valid updates.
-    @Override
-    public Integer partOne() {
-        var validUpdates = selectUpdates(updates, orderingRules, true);
-        return calculateMiddlePageNumberSum(validUpdates);
-    }
-    
-    /// Creates a list of either all the valid, or all the invalid updates in `updates`.
-    ///
-    /// If `selectValidUpdates` is true then all valid updates in `updates` are added to the list.
-    /// If false, invalid updates are added instead.
-    ///
-    /// @param updates            a list of updates.
-    /// @param orderingRules      rules the updates should follow.
-    /// @param selectValidUpdates determines if valid or invalid updates are selected.
-    /// @return a list of either all the valid updates or all the invalid updates.
-    private static List<List<Integer>> selectUpdates(
-        List<List<Integer>> updates,
-        Map<Integer, Set<Integer>> orderingRules,
-        boolean selectValidUpdates
-    ) {
-        var selectedUpdates = new ArrayList<List<Integer>>();
-        
-        for (var update : updates) {
-            var isValidUpdate = isValidUpdate(update, orderingRules);
-            
-            if (selectValidUpdates == isValidUpdate) {
-                selectedUpdates.add(update);
-            }
-        }
-        
-        return selectedUpdates;
-    }
-    
-    /// Determines if an update follows the ordering rules or not.
-    ///
-    /// An update is valid if its page numbers follow the ordering rules, and is invalid if its
-    /// follow the ordering rules.
-    ///
-    /// @param update        an update. Each update contains a list of page numbers.
-    /// @param orderingRules rules the ordering of the page numbers should follow.
-    /// @return true if the update follows the ordering rules, or false otherwise.
-    private static boolean isValidUpdate(
-        List<Integer> update,
-        Map<Integer, Set<Integer>> orderingRules
-    ) {
-        var previousPageNumbers = new HashSet<Integer>();
-        
-        for (var pageNumber : update) {
-            if (orderingRules.containsKey(pageNumber)) {
-                var intersection = new HashSet<>(orderingRules.get(pageNumber));
-                intersection.retainAll(previousPageNumbers);
-                
-                if (!intersection.isEmpty()) {
-                    return false;
+    private static boolean isValidUpdate(List<Integer> update, Map<Integer, Set<Integer>> rules) {
+        var seen = new HashSet<Integer>();
+        for (var current : update) {
+            var successors = rules.get(current);
+            if (successors != null) {
+                for (var element : seen) {
+                    if (successors.contains(element)) {
+                        return false;
+                    }
                 }
             }
-            
-            previousPageNumbers.add(pageNumber);
+            seen.add(current);
         }
-        
         return true;
     }
     
+    private static int sumMiddle(List<List<Integer>> updates) {
+        return updates
+            .stream()
+            .map(update -> {
+                int middle = update.size() / 2;
+                return update.get(middle);
+            })
+            .reduce(0, Integer::sum);
+    }
+    
+    /// Calculates the sum of the middle page numbers for valid updates.
+    @Override
+    public Integer partOne() {
+        var validUpdates = updates
+            .stream()
+            .filter(update -> isValidUpdate(update, rules))
+            .toList();
+        return sumMiddle(validUpdates);
+    }
+    
     /// Calculates the sum of the middle page numbers for the corrected invalid updates.
-    ///
-    /// @return the sum of the middle page numbers for the corrected invalid updates.
     @Override
     public Integer partTwo() {
-        var invalidUpdates = selectUpdates(updates, orderingRules, false);
-        correctPageNumberOrder(invalidUpdates, orderingRules);
-        return calculateMiddlePageNumberSum(invalidUpdates);
-    }
-    
-    /// Sorts the page numbers in the updates according to the ordering rules.
-    ///
-    /// @param updates       a list of updates.
-    /// @param orderingRules rules that the page numbers should be ordered by.
-    private static void correctPageNumberOrder(
-        List<List<Integer>> updates,
-        Map<Integer, Set<Integer>> orderingRules
-    ) {
-        var comparator = getComparator(orderingRules);
+        var filteredUpdates = updates
+            .stream()
+            .filter(update -> !isValidUpdate(update, rules))
+            .toList();
         
-        for (var update : updates) {
+        var comparator = createComparator(rules);
+        for (var update : filteredUpdates) {
             update.sort(comparator);
         }
+        
+        return sumMiddle(filteredUpdates);
     }
     
-    /// Creates a custom comparator that follows the ordering rules.
-    ///
-    /// @param orderingRules rules that the page numbers should be ordered by.
-    /// @return a custom comparator that follows the ordering rules.
-    private static Comparator<Integer> getComparator(Map<Integer, Set<Integer>> orderingRules) {
-        return (predecessor, successor) ->
-        {
-            if (orderingRules.getOrDefault(predecessor, new HashSet<>()).contains(successor)) {
-                return 1;
-            } else if (predecessor.equals(successor)) {
+    private static Comparator<Integer> createComparator(Map<Integer, Set<Integer>> rules) {
+        return (predecessor, successor) -> {
+            if (predecessor.equals(successor)) {
                 return 0;
-            } else {
+            }
+            
+            var set = rules.get(predecessor);
+            if (set.contains(successor)) {
+                return 1;
+            }
+            else {
                 return -1;
             }
         };
