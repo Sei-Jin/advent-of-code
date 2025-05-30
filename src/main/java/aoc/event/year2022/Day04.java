bb21ee3c98cdef289791fd56264b673bc44a78eb
Sei-Jin <seijin.tufts@gmail.com>
2025-05-30 13:43:29 -0300

    [2022-04] Update solution

--- src/main/java/aoc/event/year2022/Day04.java
+++ src/main/java/aoc/event/year2022/Day04.java
@@ -1,93 +1,74 @@
 package aoc.event.year2022;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
 /// # [2022-04: Camp Cleanup](https://adventofcode.com/2022/day/4)
-public class Day04 implements DeprecatedSolver2 {
+public class Day04 implements Solver<Integer, Integer> {
     
-    private static Pattern RANGE_PATTERN = Pattern.compile(
-            "^(\\d+)-(\\d+),(\\d+)-(\\d+)$"
+    private static final Pattern RANGE_PATTERN = Pattern.compile(
+        "^(\\d+)-(\\d+),(\\d+)-(\\d+)$"
     );
+    private final List<RangePair> rangePairs;
     
-    private List<RangePair> rangePairs;
-    
-    /// Initializes the solution with the parsed input data.
-    ///
-    /// The puzzle input contains two ranges on each line, such as `2-4,4-6`. In this example the
-    /// first range would be `2-4` and the second range would be `4-6`.
     public Day04(String input) {
-        rangePairs = Collections.unmodifiableList(parse(input));
+        rangePairs = parse(input);
     }
     
     private static List<RangePair> parse(String input) {
-        return input.lines().map(line -> {
-            var matcher = RANGE_PATTERN.matcher(line);
-            
-            if (matcher.find()) {
-                var firstRange = new Range(
+        return input
+            .lines()
+            .map(line -> {
+                var matcher = RANGE_PATTERN.matcher(line);
+                if (matcher.find()) {
+                    var firstRange = new Range(
                         Integer.parseInt(matcher.group(1)),
                         Integer.parseInt(matcher.group(2))
-                );
-                
-                var secondRange = new Range(
+                    );
+                    var secondRange = new Range(
                         Integer.parseInt(matcher.group(3)),
                         Integer.parseInt(matcher.group(4))
-                );
-                
-                return new RangePair(firstRange, secondRange);
-            } else {
-                throw new IllegalArgumentException(
+                    );
+                    return new RangePair(firstRange, secondRange);
+                }
+                else {
+                    throw new IllegalArgumentException(
                         "Error: Unexpected input format encountered with line value: " + line
-                );
-            }
-        }).toList();
+                    );
+                }
+            }).toList();
     }
     
     /// Calculates the number of fully contained range pairs.
     public Integer partOne() {
-        var count = 0;
-        
-        for (var pair : rangePairs) {
-            if (hasFullyContainedRanges(pair)) {
-                count++;
-            }
-        }
-        
-        return count;
+        return (int) rangePairs
+            .stream()
+            .filter(Day04::hasFullyContainedRanges)
+            .count();
     }
     
     private static boolean hasFullyContainedRanges(RangePair pair) {
         var first = pair.first;
         var second = pair.second;
-        
         var firstContainsSecond = (first.start <= second.start && first.end >= second.end);
         var secondContainsFirst = (second.start <= first.start && second.end >= first.end);
-        
         return (firstContainsSecond || secondContainsFirst);
     }
     
     /// Calculates the number of overlapping range pairs.
     public Integer partTwo() {
-        var count = 0;
-        
-        for (var pair : rangePairs) {
-            if (hasOverlappingRanges(pair)) {
-                count++;
-            }
-        }
-        
-        return count;
+        return (int) rangePairs
+            .stream()
+            .filter(Day04::hasOverlappingRanges)
+            .count();
     }
     
     /// The ranges are overlapping if one of them starts before the other ends
     private static boolean hasOverlappingRanges(RangePair pair) {
         var first = pair.first;
         var second = pair.second;
-        
         return (first.start <= second.end && second.start <= first.end);
     }
     
--- src/test/java/aoc/event/year2022/Day04Test.java
+++ src/test/java/aoc/event/year2022/Day04Test.java
@@ -8,26 +8,26 @@ public class Day04Test {
     
     @Test
     void example() {
-        final var input = """
-                2-4,6-8
-                2-3,4-5
-                5-7,7-9
-                2-8,3-7
-                6-6,4-6
-                2-6,4-8
-                """;
-        final var solution = new Day04(input);
+        var input = """
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+            """;
+        var solution = new Day04(input);
         assertEquals(2, solution.partOne());
         assertEquals(4, solution.partTwo());
     }
     
     @Test
     void checkOverlapping() {
-        final var input = """
-                5-5,4-6
-                4-6,5-5
-                """;
-        final var solution = new Day04(input);
+        var input = """
+            5-5,4-6
+            4-6,5-5
+            """;
+        var solution = new Day04(input);
         assertEquals(2, solution.partTwo());
     }
 }
