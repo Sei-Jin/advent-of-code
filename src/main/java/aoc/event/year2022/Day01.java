a7ae2e8d397cf55e35a867123e89c4251633b977
Sei-Jin <seijin.tufts@gmail.com>
2025-05-30 12:50:23 -0300

    [2022-01] Update solution

--- src/main/java/aoc/event/year2022/Day01.java
+++ src/main/java/aoc/event/year2022/Day01.java
@@ -1,105 +1,52 @@
 package aoc.event.year2022;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 /// # [2022-01: Calorie Counting](https://adventofcode.com/2022/day/1)
-public class Day01 implements DeprecatedSolver2 {
+public class Day01 implements Solver<Integer, Integer> {
     
-    /// Stores the sublist sums.
     private final List<Integer> sublistSums;
     
-    /// Initializes the solution.
     public Day01(String input) {
-        final var lists = Collections.unmodifiableList(parse(input));
-        sublistSums = Collections.unmodifiableList(calculateSums(lists));
+        sublistSums = parse(input);
     }
     
-    /// Parses the puzzle input for the list of lists of numbers.
-    ///
-    /// An example puzzle input format is:
-    ///
-    /// ```
-    /// 6000
-    /// 3000
-    /// 1000
-    /// ....
-    /// 2000
-    /// ....
-    /// 5000
-    /// 5000
-    /// ```
-    ///
-    /// Here there are three sub-lists:
-    /// - The first sub-list contains three items with values of `6000`, `3000`, and `1000`.
-    /// - The second sub-list contains a single item with a value of `2000`.
-    /// - The third sub-list contains two items with values of `5000` and `5000`.
-    ///
-    /// @param input the puzzle input.
-    /// @return a list of sub-lists of values.
-    private static List<List<Integer>> parse(String input) {
-        var lists = new ArrayList<List<Integer>>();
-        lists.add(new ArrayList<>());
-        
-        input.lines().forEach(line -> {
+    private static List<Integer> parse(String input) {
+        var sums = new ArrayList<Integer>();
+        int sum = 0;
+        for (var line : input.lines().toList()) {
             if (line.isEmpty()) {
-                lists.add(new ArrayList<>());
-            } else {
-                lists.getLast().add(Integer.parseInt(line));
+                sums.add(sum);
+                sum = 0;
             }
-        });
-        
-        return lists;
-    }
-    
-    /// Calculates the sum of each sublist.
-    ///
-    /// @param lists the input sub-lists.
-    /// @return a list of the sums of each sublist.
-    private static List<Integer> calculateSums(List<List<Integer>> lists) {
-        final var sums = new ArrayList<Integer>();
-        
-        for (final var list : lists) {
-            var sum = 0;
-            
-            for (final var number : list) {
-                sum += number;
+            else {
+                sum += Integer.parseInt(line);
             }
-            
-            sums.add(sum);
         }
-        
+        sums.add(sum);
         return sums;
     }
     
     /// Calculates the max sublist sum.
-    ///
-    /// - Time Complexity: `O(n)` - `n` is the number of lines in the puzzle input.
-    /// - Space Complexity: `O(n)` - `n` is the number of lines in the puzzle input.
-    ///
-    /// @return the max sublist sum.
     @Override
     public Integer partOne() {
-        return sublistSums.stream()
-                .reduce(0, Integer::max);
+        return sublistSums
+            .stream()
+            .reduce(0, Integer::max);
     }
     
     /// Calculates the sum of the three largest sublist sums.
-    ///
-    /// - Time Complexity: `O(m log m)` - `m` is the number of sublist sums.
-    /// - Space Complexity: `O(n)` - `n` is the number of lines in the puzzle input.
-    ///
-    /// @return the sum of the three largest sublist sums.
     @Override
     public Integer partTwo() {
-        return sublistSums.stream()
-                .sorted(Comparator.reverseOrder())
-                .limit(3)
-                .mapToInt(Integer::intValue)
-                .sum();
+        return sublistSums
+            .stream()
+            .sorted(Comparator.reverseOrder())
+            .limit(3)
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 }
--- src/test/java/aoc/event/year2022/Day01Test.java
+++ src/test/java/aoc/event/year2022/Day01Test.java
@@ -8,23 +8,23 @@ public class Day01Test {
     
     @Test
     void example() {
-        final var input = """
-                1000
-                2000
-                3000
-                
-                4000
-                
-                5000
-                6000
-                
-                7000
-                8000
-                9000
-                
-                10000
-                """;
-        final var solution = new Day01(input);
+        var input = """
+            1000
+            2000
+            3000
+            
+            4000
+            
+            5000
+            6000
+            
+            7000
+            8000
+            9000
+            
+            10000
+            """;
+        var solution = new Day01(input);
         assertEquals(24000, solution.partOne());
         assertEquals(45000, solution.partTwo());
     }
