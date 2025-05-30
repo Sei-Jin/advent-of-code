810e753bac8b50efa8561c9ea6be0c93bfd78679
Sei-Jin <seijin.tufts@gmail.com>
2025-05-30 18:04:04 -0300

    [2021-01] Update solution

--- src/main/java/aoc/event/year2021/Day01.java
+++ src/main/java/aoc/event/year2021/Day01.java
@@ -1,87 +1,48 @@
 package aoc.event.year2021;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Gatherers;
 
 /// # [2021-01: Sonar Sweep](https://adventofcode.com/2021/day/1)
-public class Day01 implements DeprecatedSolver2 {
+public class Day01 implements Solver<Integer, Integer> {
     
-    /// Defines the size of the measurement windows.
-    private static final int WINDOW_SIZE = 3;
-    
-    /// Stores the depth measurements.
     private final List<Integer> depths;
     
-    /// Initializes the solution with the puzzle input.
     public Day01(String input) {
-        this.depths = Collections.unmodifiableList(parse(input));
+        depths = parse(input);
     }
     
-    /// Parses the puzzle input for a list of depth measurements.
-    ///
-    /// The puzzle input is in the form:
-    ///
-    /// ```
-    /// #
-    /// #
-    /// #
-    /// ```
-    ///
-    /// Where each `#` is a number.
-    ///
-    /// @param input the puzzle input.
-    /// @return a list of the depths measurements.
     private static List<Integer> parse(String input) {
-        final var depths = new ArrayList<Integer>();
-        
-        input.lines().forEach(line -> depths.add(Integer.valueOf(line)));
-        
-        return depths;
+        return input
+            .lines()
+            .map(Integer::valueOf)
+            .toList();
     }
     
     /// Calculates the amount of numbers that are larger than the previous in the list.
-    ///
-    /// @return the total numbers that are larger than their predecessor in the list.
     @Override
     public Integer partOne() {
-        var increases = 0;
-        var previousDepth = depths.getFirst();
-        
-        for (final var currentDepth : depths.subList(1, depths.size())) {
-            if (previousDepth < currentDepth) {
-                increases++;
-            }
-            
-            previousDepth = currentDepth;
-        }
-        
-        return increases;
+        return (int) depths
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .filter(window -> window.get(1) > window.get(0))
+            .count();
     }
     
-    /// Calculates the number of measurement windows with a larger sum than the previous.
-    ///
-    /// @return the number of measurement windows with a larger sum than the previous.
+    /// Calculates the number of 3-number measurement windows with a larger sum than the previous.
     @Override
     public Integer partTwo() {
-        var increases = 0;
-        var previousSum = depths.get(0) + depths.get(1) + depths.get(2);
-        
-        for (var i = 0; i < depths.size() - WINDOW_SIZE; i++) {
-            final var previousMeasurement = depths.get(i);
-            final var nextMeasurement = depths.get(i + WINDOW_SIZE);
-            
-            final var currentSum = previousSum - previousMeasurement + nextMeasurement;
-            
-            if (previousSum < currentSum) {
-                increases++;
-            }
-            
-            previousSum = currentSum;
-        }
-        
-        return increases;
+        return (int) depths
+            .stream()
+            .gather(Gatherers.windowSliding(4))
+            .filter(window -> {
+                var middle = window.get(1) + window.get(2);
+                var previousSum = window.get(0) + middle;
+                var nextSum = middle + window.get(3);
+                return nextSum > previousSum;
+            })
+            .count();
     }
 }
--- src/test/java/aoc/event/year2021/Day01Test.java
+++ src/test/java/aoc/event/year2021/Day01Test.java
@@ -5,22 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test {
+    
     @Test
     void example() {
-        final var input = """
-                199
-                200
-                208
-                210
-                200
-                207
-                240
-                269
-                260
-                263
-                """;
-        
-        final var solution = new Day01(input);
+        var input = """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """;
+        var solution = new Day01(input);
         assertEquals(7, solution.partOne());
         assertEquals(5, solution.partTwo());
     }
