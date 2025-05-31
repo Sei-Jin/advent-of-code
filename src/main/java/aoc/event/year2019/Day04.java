72c8298a951748aaab4208b5b9a191f239b7c58d
Sei-Jin <seijin.tufts@gmail.com>
2025-05-31 17:21:52 -0300

    [2021-04] Update solution and solve part 2

--- src/main/java/aoc/event/year2019/Day04.java
+++ src/main/java/aoc/event/year2019/Day04.java
@@ -1,82 +1,100 @@
 package aoc.event.year2019;
 
-import aoc.DeprecatedSolver;
+import aoc.Solver;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.stream.Gatherers;
+import java.util.stream.IntStream;
 
 /// # [2019-04: Secure Container](https://adventofcode.com/2019/day/4)
-public class Day04 implements DeprecatedSolver
-{
-    @Override
-    public Object partOne(List<String> inputLines)
-    {
-        List<Integer> rangeNumbers = Arrays.stream(inputLines
-                .getFirst()
-                .split("-"))
-                .map(Integer::parseInt)
-                .toList();
-        
-        int startNumber = rangeNumbers.getFirst();
-        int endNumber = rangeNumbers.getLast();
-        
-        int validPasswords = 0;
-        
-        for (int number = startNumber; number <= endNumber; number++)
-        {
-            List<Integer> digits = getDigits(number);
-            
-            if (validPassword(digits))
-            {
-                validPasswords++;
-            }
-        }
-        
-        return validPasswords;
+public class Day04 implements Solver<Integer, Integer> {
+    
+    private final Range range;
+    
+    public Day04(String input) {
+        var parts = input.split("-");
+        int start = Integer.parseInt(parts[0]);
+        int end = Integer.parseInt(parts[1]);
+        range = new Range(start, end);
     }
     
-    
-    private List<Integer> getDigits(int number)
-    {
-        List<Integer> digits = new LinkedList<>();
-        
-        while (number > 0)
-        {
+    private static Deque<Integer> toDigits(int number) {
+        var digits = new ArrayDeque<Integer>();
+        while (number > 0) {
             digits.addFirst(number % 10);
             number = number / 10;
         }
-        
         return digits;
     }
     
-    
-    private static boolean validPassword(List<Integer> digits)
-    {
-        boolean containsDouble = false;
-        
-        for (int index = 0; index < digits.size() - 1; index++)
-        {
-            // Do the digits decrease from left to right?
-            if (digits.get(index) > digits.get(index + 1))
-            {
-                return false;
-            }
-            
-            if (Objects.equals(digits.get(index), digits.get(index + 1)))
-            {
-                containsDouble = true;
-            }
-        }
-        
-        return containsDouble;
+    private static boolean isNonDecreasing(Deque<Integer> digits) {
+        return digits
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .allMatch(window -> window.get(0) <= window.get(1));
     }
-    
     
     @Override
-    public Object partTwo(List<String> inputLines)
-    {
-        return null;
+    public Integer partOne() {
+        return (int) IntStream
+            .rangeClosed(range.start(), range.end())
+            .boxed()
+            .map(Day04::toDigits)
+            .filter(Day04::isNonDecreasing)
+            .filter(Day04::containsPair)
+            .count();
     }
+    
+    private static boolean containsPair(Deque<Integer> digits) {
+        return digits
+            .stream()
+            .gather(Gatherers.windowSliding(2))
+            .anyMatch(window -> Objects.equals(window.get(0), window.get(1)));
+    }
+    
+    @Override
+    public Integer partTwo() {
+        return (int) IntStream
+            .rangeClosed(range.start(), range.end())
+            .boxed()
+            .map(Day04::toDigits)
+            .filter(Day04::isNonDecreasing)
+            .filter(Day04::containsStandalonePair)
+            .count();
+    }
+    
+    private static boolean containsStandalonePair(Deque<Integer> digits) {
+        var middle = digits
+            .stream()
+            .gather(Gatherers.windowSliding(4))
+            .anyMatch(window -> {
+                var isPair = Objects.equals(window.get(1), window.get(2));
+                var partOfLargerGroup =
+                    Objects.equals(window.get(0), window.get(1))
+                        || Objects.equals(window.get(2), window.get(3));
+                return isPair && !partOfLargerGroup;
+            });
+        // Edge cases. Check if the first or last two digits form a standalone pair.
+        var start = false;
+        var end = false;
+        if (digits.size() >= 3) {
+            start = hasEdgePair(digits.iterator());
+            end = hasEdgePair(digits.reversed().iterator());
+        }
+        return middle || start || end;
+    }
+    
+    private static boolean hasEdgePair(Iterator<Integer> it) {
+        var first = it.next();
+        var second = it.next();
+        var third = it.next();
+        var isPair = Objects.equals(second, first);
+        var partOfLargerGroup = Objects.equals(third, second);
+        return isPair && !partOfLargerGroup;
+    }
+    
+    private record Range(int start, int end) {}
 }
--- src/test/java/aoc/event/year2019/Day04Test.java
+++ src/test/java/aoc/event/year2019/Day04Test.java
@@ -1,43 +1,66 @@
 package aoc.event.year2019;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day04Test
-{
-    @Test
-    void repeatingDigits()
-    {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("111111-111111");
-        assertEquals(1, new Day04().partOne(inputLines));
+public class Day04Test {
+    
+    @Nested
+    class Part1 {
+        
+        @Test
+        void repeatingDigits() {
+            var input = "111111-111111";
+            var solution = new Day04(input);
+            assertEquals(1, solution.partOne());
+        }
+        
+        @Test
+        void decreasingPairOfDigits() {
+            var input = "223450-223450";
+            var solution = new Day04(input);
+            assertEquals(0, solution.partOne());
+        }
+        
+        @Test
+        void increasingDigits() {
+            var input = "123789-123789";
+            var solution = new Day04(input);
+            assertEquals(0, solution.partOne());
+        }
+        
+        @Test
+        void smallRange() {
+            var input = "111110-111120";
+            var solution = new Day04(input);
+            assertEquals(9, solution.partOne());
+        }
     }
     
-    @Test
-    void decreasingPairOfDigits()
-    {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("223450-223450");
-        assertEquals(0, new Day04().partOne(inputLines));
-    }
-    
-    @Test
-    void increasingDigits()
-    {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("123789-123789");
-        assertEquals(0, new Day04().partOne(inputLines));
-    }
-    
-    @Test
-    void smallRange()
-    {
-        List<String> inputLines = new ArrayList<>();
-        inputLines.add("111110-111120");
-        assertEquals(9, new Day04().partOne(inputLines));
+    @Nested
+    class Part2 {
+        
+        @Test
+        void example1() {
+            var input = "112233-112233";
+            var solution = new Day04(input);
+            assertEquals(1, solution.partTwo());
+        }
+        
+        @Test
+        void example2() {
+            var input = "123444-123444";
+            var solution = new Day04(input);
+            assertEquals(0, solution.partTwo());
+        }
+        
+        @Test
+        void example3() {
+            var input = "111122-111122";
+            var solution = new Day04(input);
+            assertEquals(1, solution.partTwo());
+        }
     }
 }
