79b77b91b3cf8473904e5571e60b1ca157c483e4
Sei-Jin <seijin.tufts@gmail.com>
2025-06-02 17:11:51 -0300

    [2019-01] Update solution

--- src/main/java/aoc/event/year2019/Day01.java
+++ src/main/java/aoc/event/year2019/Day01.java
@@ -1,27 +1,23 @@
 package aoc.event.year2019;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.Collections;
 import java.util.List;
 
 /// # [2019-01: The Tyranny of the Rocket Equation](https://adventofcode.com/2019/day/1)
-public class Day01 implements DeprecatedSolver2 {
+public class Day01 implements Solver<Integer, Integer> {
     
     private final List<Integer> moduleMasses;
     
-    /// Initializes the solution with the parsed puzzle input.
-    ///
-    /// The puzzle input contains one number per line.
     public Day01(String input) {
-        moduleMasses = Collections.unmodifiableList(parse(input));
+        moduleMasses = parse(input);
     }
     
     private static List<Integer> parse(String input) {
         return input.lines()
-                .mapToInt(Integer::parseInt)
-                .boxed()
-                .toList();
+            .mapToInt(Integer::parseInt)
+            .boxed()
+            .toList();
     }
     
     private static int calculateFuelMass(int mass) {
@@ -31,35 +27,28 @@ public class Day01 implements DeprecatedSolver2 {
     /// Calculates the sum fuel masses required.
     @Override
     public Integer partOne() {
-        var sum = 0;
-        
-        for (final var moduleMass : moduleMasses) {
-            final var fuelMass = calculateFuelMass(moduleMass);
-            sum += fuelMass;
-        }
-        
-        return sum;
+        return moduleMasses
+            .stream()
+            .mapToInt(Day01::calculateFuelMass)
+            .sum();
     }
     
     /// Calculates the sum of the fuel required when also taking into consideration the mass of the
     /// added fuel itself.
     @Override
     public Integer partTwo() {
-        var sum = 0;
-        
-        for (final var moduleMass : moduleMasses) {
-            sum += calculateFuelMassRecursively(moduleMass);
-        }
-        
-        return sum;
+        return moduleMasses
+            .stream()
+            .mapToInt(Day01::calculateFuelMassRecursively)
+            .sum();
     }
     
     private static int calculateFuelMassRecursively(int mass) {
-        final var fuelMass = calculateFuelMass(mass);
-        
+        var fuelMass = calculateFuelMass(mass);
         if (fuelMass <= 0) {
             return 0;
-        } else {
+        }
+        else {
             return fuelMass + calculateFuelMassRecursively(fuelMass);
         }
     }
--- src/test/java/aoc/event/year2019/Day01Test.java
+++ src/test/java/aoc/event/year2019/Day01Test.java
@@ -8,32 +8,32 @@ public class Day01Test {
     
     @Test
     void example1() {
-        final var input = "12";
-        final var solution = new Day01(input);
+        var input = "12";
+        var solution = new Day01(input);
         assertEquals(2, solution.partOne());
         assertEquals(2, solution.partTwo());
     }
     
     @Test
     void example2() {
-        final var input = "14";
-        final var solution = new Day01(input);
+        var input = "14";
+        var solution = new Day01(input);
         assertEquals(2, solution.partOne());
         assertEquals(2, solution.partTwo());
     }
     
     @Test
     void example3() {
-        final var input = "1969";
-        final var solution = new Day01(input);
+        var input = "1969";
+        var solution = new Day01(input);
         assertEquals(654, solution.partOne());
         assertEquals(966, solution.partTwo());
     }
     
     @Test
     void example4() {
-        final var input = "100756";
-        final var solution = new Day01(input);
+        var input = "100756";
+        var solution = new Day01(input);
         assertEquals(33583, solution.partOne());
         assertEquals(50346, solution.partTwo());
     }
