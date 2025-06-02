bc94058c5ca255382820f26201d9b3d610559ea9
Sei-Jin <seijin.tufts@gmail.com>
2025-06-02 17:51:07 -0300

    [2019-02] Update solution

--- src/main/java/aoc/event/year2019/Day02.java
+++ src/main/java/aoc/event/year2019/Day02.java
@@ -1,6 +1,6 @@
 package aoc.event.year2019;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 /// # [2019-02: 1202 Program Alarm](https://adventofcode.com/2019/day/2)
-public class Day02 implements DeprecatedSolver2 {
+public class Day02 implements Solver<Integer, Integer> {
     
     private final List<Integer> initialState;
     
@@ -17,23 +17,51 @@ public class Day02 implements DeprecatedSolver2 {
     }
     
     private static List<Integer> parse(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays
+            .stream(input.split(","))
             .mapToInt(Integer::parseInt)
             .boxed()
             .toList();
     }
     
+    public static List<Integer> runProgram(List<Integer> intcode) {
+        for (var position = 0; position < intcode.size(); position += 4) {
+            var opcode = intcode.get(position);
+            switch (opcode) {
+                case 1 -> add(intcode, position);
+                case 2 -> multiply(intcode, position);
+                case 99 -> position = intcode.size();
+            }
+        }
+        return intcode;
+    }
+    
+    private static void add(List<Integer> intcode, int position) {
+        var positions = determinePositions(intcode, position);
+        var sum = intcode.get(positions.first) + intcode.get(positions.second);
+        intcode.set(positions.output, sum);
+    }
+    
+    private static void multiply(List<Integer> intcode, int position) {
+        var positions = determinePositions(intcode, position);
+        var product = intcode.get(positions.first) * intcode.get(positions.second);
+        intcode.set(positions.output, product);
+    }
+    
+    private static Positions determinePositions(List<Integer> intcode, int position) {
+        var first = intcode.get(position + 1);
+        var second = intcode.get(position + 2);
+        var output = intcode.get(position + 3);
+        return new Positions(first, second, output);
+    }
+    
     /// @return the value left at position 0 after the program halts.
     @Override
     public Integer partOne() {
-        final var intcode = new ArrayList<>(initialState);
-        
+        var intcode = new ArrayList<>(initialState);
         intcode.set(1, 12);
         intcode.set(2, 2);
-        
-        runProgram(intcode);
-        
-        return intcode.getFirst();
+        return runProgram(intcode).getFirst();
     }
     
     /// @return the input noun and verb that cause the program to produce the output 19690720, or
@@ -42,54 +70,15 @@ public class Day02 implements DeprecatedSolver2 {
     public Integer partTwo() {
         for (var noun = 0; noun <= 99; noun++) {
             for (var verb = 0; verb <= 99; verb++) {
-                final var intcode = new ArrayList<>(initialState);
-                
+                var intcode = new ArrayList<>(initialState);
                 intcode.set(1, noun);
                 intcode.set(2, verb);
-                
-                runProgram(intcode);
-                
-                if (intcode.getFirst() == 19690720) {
+                if (runProgram(intcode).getFirst() == 19690720) {
                     return 100 * noun + verb;
                 }
             }
         }
-        
         return -1;
-    }
-    
-    private static void runProgram(List<Integer> intcode) {
-        for (var position = 0; position < intcode.size(); position += 4) {
-            final var opcode = intcode.get(position);
-            
-            switch (opcode) {
-                case 1 -> add(intcode, position);
-                case 2 -> multiply(intcode, position);
-                case 99 -> position = intcode.size();
-            }
-        }
-    }
-    
-    private static void add(List<Integer> intcode, int position) {
-        final var positions = determinePositions(intcode, position);
-        final var sum = intcode.get(positions.first) + intcode.get(positions.second);
-        
-        intcode.set(positions.output, sum);
-    }
-    
-    private static void multiply(List<Integer> intcode, int position) {
-        final var positions = determinePositions(intcode, position);
-        final var product = intcode.get(positions.first) * intcode.get(positions.second);
-        
-        intcode.set(positions.output, product);
-    }
-    
-    private static Positions determinePositions(List<Integer> intcode, int position) {
-        final var first = intcode.get(position + 1);
-        final var second = intcode.get(position + 2);
-        final var output = intcode.get(position + 3);
-        
-        return new Positions(first, second, output);
     }
     
     private record Positions(Integer first, Integer second, Integer output) {}
--- src/test/java/aoc/event/year2019/Day02Test.java
+++ src/test/java/aoc/event/year2019/Day02Test.java
@@ -1,5 +1,50 @@
 package aoc.event.year2019;
 
-public class Day02Test {
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day02Test {
+    
+    @SafeVarargs
+    public static <T> List<T> mutableListOf(T... elements) {
+        var list = new ArrayList<T>(elements.length);
+        list.addAll(Arrays.asList(elements));
+        return list;
+    }
+    
+    @Test
+    void example1() {
+        var input = mutableListOf(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50);
+        assertEquals(List.of(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50),
+            Day02.runProgram(input));
+    }
+    
+    @Test
+    void example2() {
+        var input = mutableListOf(1, 0, 0, 0, 99);
+        assertEquals(List.of(2, 0, 0, 0, 99), Day02.runProgram(input));
+    }
+    
+    @Test
+    void example3() {
+        var input = mutableListOf(2, 3, 0, 3, 99);
+        assertEquals(List.of(2, 3, 0, 6, 99), Day02.runProgram(input));
+    }
+    
+    @Test
+    void example4() {
+        var input = mutableListOf(2, 4, 4, 5, 99, 0);
+        assertEquals(List.of(2, 4, 4, 5, 99, 9801), Day02.runProgram(input));
+    }
+    
+    @Test
+    void example5() {
+        var input = mutableListOf(1, 1, 1, 4, 99, 5, 6, 0, 99);
+        assertEquals(List.of(30, 1, 1, 4, 2, 5, 6, 0, 99), Day02.runProgram(input));
+    }
 }
