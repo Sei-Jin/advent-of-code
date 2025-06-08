66857d32c526ab8d00dcbcb53bab17936e644ef4
Sei-Jin <seijin.tufts@gmail.com>
2025-06-08 12:56:27 -0300

    [2016-03] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2016/Day03.java
+++ src/main/java/aoc/event/year2016/Day03.java
@@ -1,86 +1,68 @@
 package aoc.event.year2016;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /// # [2016-03: Squares With Three Sides](https://adventofcode.com/2016/day/3)
-public class Day03 implements DeprecatedSolver2 {
+public class Day03 implements Solver<Integer, Integer> {
     
-    private final List<List<Integer>> numberLists;
+    private static final int TRIANGLE_SIDES = 3;
+    private final List<List<Integer>> lists;
     
     public Day03(String input) {
-        numberLists = parse(input);
+        lists = parse(input);
     }
     
     private static List<List<Integer>> parse(String input) {
-        final var numberLists = new ArrayList<List<Integer>>();
-        
-        for (final var line : input.lines().toList()) {
-            final var numbers = Arrays.stream(line.trim().split("\\s+"))
+        return input
+            .lines()
+            .map(String::stripLeading)
+            .map(line -> Arrays
+                .stream(line.split("\\s+"))
                 .map(Integer::parseInt)
-                .toList();
-            
-            numberLists.add(numbers);
-        }
-        
-        return numberLists;
+                .toList())
+            .toList();
     }
     
-    /// Determines whether a triangle could be formed with the given `sideLengths` or not. A triangle is
-    /// considered possible when the sum of any two sides are larger than the remaining side.
-    ///
-    /// @param sideLengths the side lengths of a triangle.
-    /// @return true if the side lengths are possible, or false otherwise.
-    private static boolean possibleTriangle(List<Integer> sideLengths) {
-        if (sideLengths.size() != 3) {
+    private static boolean isPossibleTriangle(List<Integer> sideLengths) {
+        if (sideLengths.size() != TRIANGLE_SIDES) {
             throw new IllegalArgumentException("There must be exactly 3 sides to form a triangle.");
         }
-        
-        final var side1 = sideLengths.get(0);
-        final var side2 = sideLengths.get(1);
-        final var side3 = sideLengths.get(2);
-        
+        var side1 = sideLengths.get(0);
+        var side2 = sideLengths.get(1);
+        var side3 = sideLengths.get(2);
         return (side1 + side2 > side3) &&
             (side1 + side3 > side2) &&
             (side2 + side3 > side1);
     }
-
-    /// @return the number of possible triangles.
+    
+    /// Counts the number of possible triangles with sidelengths listed horizontally.
     @Override
-    public Object partOne() {
-        var possibleTriangles = 0;
-        
-        for (final var list : numberLists) {
-            if (possibleTriangle(list)) {
-                possibleTriangles++;
-            }
-        }
-        
-        return possibleTriangles;
+    public Integer partOne() {
+        return (int) lists
+            .stream()
+            .filter(Day03::isPossibleTriangle)
+            .count();
     }
-
-    /// @return the number of possible triangles in the vertical groups.
+    
+    /// Counts the number of possible triangles with sidelengths listed vertically.
     @Override
-    public Object partTwo() {
-        var possibleTriangles = 0;
-        
-        for (var row = 0; row < numberLists.size(); row += 3) {
-            for (var column = 0; column < numberLists.getFirst().size(); column++) {
-                final var triangle = Arrays.asList(
-                    numberLists.get(row).get(column),
-                    numberLists.get(row + 1).get(column),
-                    numberLists.get(row + 2).get(column)
+    public Integer partTwo() {
+        var count = 0;
+        for (var x = 0; x < lists.size(); x += TRIANGLE_SIDES) {
+            for (var y = 0; y < lists.getFirst().size(); y++) {
+                var triangle = List.of(
+                    lists.get(x).get(y),
+                    lists.get(x + 1).get(y),
+                    lists.get(x + 2).get(y)
                 );
-                
-                if (possibleTriangle(triangle)) {
-                    possibleTriangles++;
+                if (isPossibleTriangle(triangle)) {
+                    count++;
                 }
             }
         }
-        
-        return possibleTriangles;
+        return count;
     }
 }
--- src/test/java/aoc/event/year2016/Day03Test.java
+++ src/test/java/aoc/event/year2016/Day03Test.java
@@ -12,29 +12,29 @@ public class Day03Test {
         
         @Test
         void possibleTriangle1() {
-            final var input = "5 10 10";
-            final var solution = new Day03(input);
+            var input = "5 10 10";
+            var solution = new Day03(input);
             assertEquals(1, solution.partOne());
         }
         
         @Test
         void possibleTriangle2() {
-            final var input = "5 10 14";
-            final var solution = new Day03(input);
+            var input = "5 10 14";
+            var solution = new Day03(input);
             assertEquals(1, solution.partOne());
         }
         
         @Test
         void impossibleTriangle1() {
-            final var input = "5 10 25";
-            final var solution = new Day03(input);
+            var input = "5 10 25";
+            var solution = new Day03(input);
             assertEquals(0, solution.partOne());
         }
         
         @Test
         void impossibleTriangle2() {
-            final var input = "15 10 5";
-            final var solution = new Day03(input);
+            var input = "15 10 5";
+            var solution = new Day03(input);
             assertEquals(0, solution.partOne());
         }
     }
@@ -44,45 +44,45 @@ public class Day03Test {
         
         @Test
         void noPossibleTriangles() {
-            final var input = """
+            var input = """
                 101 301 1500
                 102 700 501
                 300 302 502
                 """;
-            final var solution = new Day03(input);
+            var solution = new Day03(input);
             assertEquals(0, solution.partTwo());
         }
         
         @Test
         void onePossibleTriangle() {
-            final var input = """
+            var input = """
                 101 301 501
                 102 302 502
                 300 700 503
                 """;
-            final var solution = new Day03(input);
+            var solution = new Day03(input);
             assertEquals(1, solution.partTwo());
         }
         
         @Test
         void twoPossibleTriangles() {
-            final var input = """
+            var input = """
                 101 301 501
                 102 302 502
                 300 303 503
                 """;
-            final var solution = new Day03(input);
+            var solution = new Day03(input);
             assertEquals(2, solution.partTwo());
         }
         
         @Test
         void threePossibleTriangles() {
-            final var input = """
+            var input = """
                 101 301 501
                 102 302 502
                 103 303 503
                 """;
-            final var solution = new Day03(input);
+            var solution = new Day03(input);
             assertEquals(3, solution.partTwo());
         }
     }
