757b4dcd3479ea18d043e83c99d68afb593a3783
Sei-Jin <seijin.tufts@gmail.com>
2025-06-07 22:08:31 -0300

    [2017-02] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2017/Day02.java
+++ src/main/java/aoc/event/year2017/Day02.java
@@ -1,124 +1,78 @@
 package aoc.event.year2017;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
 /// # [2017-02: Corruption Checksum](https://adventofcode.com/2017/day/2)
-public class Day02 implements DeprecatedSolver2 {
+public class Day02 implements Solver<Integer, Integer> {
     
-    /// Compiles a pattern that matches integers.
     private static final Pattern PATTERN = Pattern.compile("(\\d+)");
-    
-    /// Stores the puzzle input as a list of lists of numbers.
     private final List<List<Integer>> numberLists;
     
-    /// Initializes the solution.
-    ///
-    /// @param input the puzzle input.
     public Day02(String input) {
-        this.numberLists = Collections.unmodifiableList(parse(input));
+        numberLists = parse(input);
     }
     
-    /// Parses the solution input for a list of number lists.
-    ///
-    /// Each line of the puzzle input is in the form: `###### ...`. Where each `#`
-    /// represents an integer.
-    ///
-    /// @param input the puzzle input.
-    /// @return a list of number lists.
     private List<List<Integer>> parse(String input) {
-        final var numberLists = new ArrayList<List<Integer>>();
-        
-        input.lines().forEach(line -> {
-            var numberList = PATTERN.matcher(line)
-                    .results()
-                    .mapToInt(match -> Integer.parseInt(match.group()))
-                    .boxed()
-                    .toList();
-            
-            numberLists.add(numberList);
-        });
-        
-        return numberLists;
+        return input
+            .lines()
+            .map(line -> PATTERN
+                .matcher(line)
+                .results()
+                .mapToInt(match -> Integer.parseInt(match.group()))
+                .boxed()
+                .toList())
+            .toList();
     }
     
-    /// Calculates the sum of the maximum difference for all rows.
-    ///
-    /// @return the sum of the maximum differences for all rows.
+    /// Calculates the sum of the maximum differences for all rows.
     @Override
     public Integer partOne() {
-        var sum = 0;
-        
-        for (final var list : numberLists) {
-            sum += findMaximumDifference(list);
-        }
-        
-        return sum;
+        return numberLists
+            .stream()
+            .mapToInt(Day02::findMaximumDifference)
+            .sum();
     }
     
-    /// Calculates the maximum difference between any two numbers in the given list.
-    ///
-    /// The maximum difference is the difference between the smallest and largest numbers in the
-    /// list.
-    ///
-    /// @param list a list of numbers.
-    /// @return the maximum difference between any two numbers in the list.
     private static int findMaximumDifference(List<Integer> list) {
+        if (list.isEmpty()) {
+            return 0;
+        }
         var min = list.getFirst();
         var max = list.getFirst();
-        
-        for (final var number : list) {
-            if (min > number) {
-                min = number;
-            }
-            
-            if (max < number) {
-                max = number;
-            }
+        for (var number : list) {
+            min = Math.min(min, number);
+            max = Math.max(max, number);
         }
-        
         return max - min;
     }
     
-    /// Calculates the total of the divisible quotients from all rows.
-    ///
-    /// @return the sum of the of divisible quotients.
+    /// Calculates the sum of the divisible quotients from all rows.
     @Override
     public Integer partTwo() {
-        var sum = 0;
-        
-        for (final var list : numberLists) {
-            sum += findDivisibleQuotient(list);
-        }
-        
-        return sum;
+        return numberLists
+            .stream()
+            .mapToInt(Day02::findDivisibleQuotient)
+            .sum();
     }
     
     /// Finds the only two numbers in the list that are divisible, and returns their quotient.
     ///
     /// Numbers are divisible if and only if they divide evenly without leaving a remainder.
-    ///
-    /// @param list a list of numbers.
-    /// @return the quotient of the only two divisible list.
     private static int findDivisibleQuotient(List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.size(); j++) {
                 if (i == j) {
                     continue;
                 }
-                
                 boolean isDivisible = ((list.get(i) % list.get(j)) == 0);
-                
                 if (isDivisible) {
                     return list.get(i) / list.get(j);
                 }
             }
         }
-        
         throw new IllegalArgumentException("No divisible numbers were found");
     }
 }
--- src/test/java/aoc/event/year2017/Day02Test.java
+++ src/test/java/aoc/event/year2017/Day02Test.java
@@ -9,70 +9,72 @@ public class Day02Test {
     
     @Nested
     class PartOne {
+        
         @Test
         void exampleSpreadsheetFirstRow() {
-            final var input = "5 1 9 5";
-            final var solution = new Day02(input);
+            var input = "5 1 9 5";
+            var solution = new Day02(input);
             assertEquals(8, solution.partOne());
         }
         
         @Test
         void exampleSpreadsheetSecondRow() {
-            final var input = "7 5 3  ";
-            final var solution = new Day02(input);
+            var input = "7 5 3  ";
+            var solution = new Day02(input);
             assertEquals(4, solution.partOne());
         }
         
         @Test
         void exampleSpreadsheetThirdRow() {
-            final var input = "2 4 6 8";
-            final var solution = new Day02(input);
+            var input = "2 4 6 8";
+            var solution = new Day02(input);
             assertEquals(6, solution.partOne());
         }
         
         @Test
         void exampleSpreadsheet() {
-            final var input = """
-                    5 1 9 5
-                    7 5 3
-                    2 4 6 8
-                    """;
-            final var solution = new Day02(input);
+            var input = """
+                5 1 9 5
+                7 5 3
+                2 4 6 8
+                """;
+            var solution = new Day02(input);
             assertEquals(18, solution.partOne());
         }
     }
     
     @Nested
     class PartTwo {
+        
         @Test
         void exampleSpreadsheetFirstRow() {
-            final var input = "5 9 2 8";
-            final var solution = new Day02(input);
+            var input = "5 9 2 8";
+            var solution = new Day02(input);
             assertEquals(4, solution.partTwo());
         }
         
         @Test
         void exampleSpreadsheetSecondRow() {
-            final var input = "9 4 7 3";
-            final var solution = new Day02(input);
+            var input = "9 4 7 3";
+            var solution = new Day02(input);
             assertEquals(3, solution.partTwo());
         }
         
         @Test
         void exampleSpreadsheetThirdRow() {
-            final var input = "3 8 6 5";
-            final var solution = new Day02(input);
+            var input = "3 8 6 5";
+            var solution = new Day02(input);
             assertEquals(2, solution.partTwo());
         }
         
         @Test
         void exampleSpreadsheet() {
-            final var input = """
-                    5 9 2 8
-                    9 4 7 3
-                    3 8 6 5
-                    """;
-            final var solution = new Day02(input);
+            var input = """
+                5 9 2 8
+                9 4 7 3
+                3 8 6 5
+                """;
+            var solution = new Day02(input);
             assertEquals(9, solution.partTwo());
         }
     }
