e14158ea16e0854e193b7eff7027a9d8b8808678
Sei-Jin <seijin.tufts@gmail.com>
2025-06-07 22:57:59 -0300

    [2017-05] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2017/Day05.java
+++ src/main/java/aoc/event/year2017/Day05.java
@@ -1,18 +1,17 @@
 package aoc.event.year2017;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /// # [2017-05: A Maze of Twisty Trampolines, All Alike](https://adventofcode.com/2017/day/5)
-public class Day05 implements DeprecatedSolver2<Integer> {
+public class Day05 implements Solver<Integer, Integer> {
     
     private static List<Integer> instructions;
     
     public Day05(String input) {
-        instructions = Collections.unmodifiableList(parse(input));
+        instructions = parse(input);
     }
     
     private static List<Integer> parse(String input) {
@@ -22,49 +21,34 @@ public class Day05 implements DeprecatedSolver2<Integer> {
             .toList();
     }
     
-    /// @return the number of steps it takes to reach the exit.
     @Override
     public Integer partOne() {
-        final var list = new ArrayList<>(instructions);
-        
-        var steps = 0;
-        var next = list.getFirst();
-        
-        while (next >= 0 && next < list.size()) {
-            final int previous = next;
-            final var jumpOffset = list.get(next);
-            
-            list.set(previous, list.get(previous) + 1);
-            
-            next += jumpOffset;
-            steps++;
-        }
-        
-        return steps;
+        return calculateSteps(instructions, false);
     }
     
-    /// @return the number of steps it now takes to reach the exit.
     @Override
     public Integer partTwo() {
-        final var list = new ArrayList<>(instructions);
-        
+        return calculateSteps(instructions, true);
+    }
+    
+    private static int calculateSteps(List<Integer> instructions, boolean partTwo) {
+        var list = new ArrayList<>(instructions);
         var steps = 0;
         var next = list.getFirst();
         
         while (next >= 0 && next < list.size()) {
-            final int previous = next;
-            final var jumpOffset = list.get(next);
+            int previous = next;
+            var jumpOffset = list.get(next);
             
-            if (jumpOffset >= 3) {
+            if (partTwo && jumpOffset >= 3) {
                 list.set(previous, list.get(previous) - 1);
-            } else {
+            }
+            else {
                 list.set(previous, list.get(previous) + 1);
             }
-            
             next += jumpOffset;
             steps++;
         }
-        
         return steps;
     }
 }
--- src/test/java/aoc/event/year2017/Day05Test.java
+++ src/test/java/aoc/event/year2017/Day05Test.java
@@ -8,14 +8,14 @@ public class Day05Test {
     
     @Test
     void example() {
-        final var input = """
+        var input = """
             0
             3
             0
             1
             -3
             """;
-        final var solution = new Day05(input);
+        var solution = new Day05(input);
         assertEquals(5, solution.partOne());
         assertEquals(10, solution.partTwo());
     }
