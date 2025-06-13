3c0333cb5a9f38b0777c0416b9668fb2f0a25a53
Sei-Jin <seijin.tufts@gmail.com>
2025-06-13 15:30:07 -0300

    Remove deprecated solver interface

--- src/main/java/aoc/DeprecatedSolver2.java
+++
@@ -1,9 +0,0 @@
-package aoc;
-
-/// @deprecated because the new solver interface should be used instead.
-@Deprecated(since = "2025-05-26", forRemoval = true)
-public interface DeprecatedSolver2<T> {
-    
-    T partOne();
-    T partTwo();
-}
