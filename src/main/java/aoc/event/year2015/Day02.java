68b5b9b81d83b3cf3153812ddba7745311f6fecd
Sei-Jin <seijin.tufts@gmail.com>
2025-06-13 12:09:32 -0300

    [2015-02] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2015/Day02.java
+++ src/main/java/aoc/event/year2015/Day02.java
@@ -1,16 +1,11 @@
 package aoc.event.year2015;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /// # [2015-02: I Was Told There Would Be No Math](https://adventofcode.com/2015/day/2)
-public class Day02 implements DeprecatedSolver2 {
-    
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("^(\\d+)x(\\d+)x(\\d+)$");
+public class Day02 implements Solver<Integer, Integer> {
     
     private final List<Box> boxes;
     
@@ -19,63 +14,53 @@ public class Day02 implements DeprecatedSolver2 {
     }
     
     private static List<Box> parse(String input) {
-        final var boxes = new ArrayList<Box>();
-        
-        for (final var line : input.lines().toList()) {
-            final var matcher = NUMBER_PATTERN.matcher(line);
-            
-            if (matcher.find()) {
-                final var length = Integer.parseInt(matcher.group(1));
-                final var width = Integer.parseInt(matcher.group(2));
-                final var height = Integer.parseInt(matcher.group(3));
+        return input
+            .lines()
+            .map(line -> {
+                var parts = line.split("x");
                 
-                boxes.add(new Box(length, width, height));
-            }
-        }
-        
-        return boxes;
+                var length = Integer.parseInt(parts[0]);
+                var width = Integer.parseInt(parts[1]);
+                var height = Integer.parseInt(parts[2]);
+                
+                return new Box(length, width, height);
+            })
+            .toList();
     }
     
-    /// @return the total square feet of wrapping paper the elves should order.
+    /// @return the total area of wrapping paper the elves should order.
     @Override
     public Integer partOne() {
-        var totalArea = 0;
-        
-        for (final var box : boxes) {
-            final var area1 = box.length * box.width;
-            final var area2 = box.width * box.height;
-            final var area3 = box.height * box.length;
-            
-            final var minArea = Integer.min(Integer.min(area1, area2), area3);
-            final var presentArea = 2 * (area1 + area2 + area3) + minArea;
-            
-            totalArea += presentArea;
-        }
-        
-        return totalArea;
+        return boxes
+            .stream()
+            .mapToInt(box -> {
+                var area1 = box.length * box.width;
+                var area2 = box.width * box.height;
+                var area3 = box.height * box.length;
+                
+                var minArea = Integer.min(Integer.min(area1, area2), area3);
+                
+                return 2 * (area1 + area2 + area3) + minArea;
+            })
+            .sum();
     }
     
-    /// @return the total length of ribbon in feet the elves should order.
+    /// @return the total length of ribbon the elves should order.
     @Override
     public Integer partTwo() {
-        var totalRibbonLength = 0;
-        
-        for (final var box : boxes) {
-            final var sideLengths = new ArrayList<Integer>();
-            
-            sideLengths.add(box.length);
-            sideLengths.add(box.width);
-            sideLengths.add(box.height);
-            
-            Collections.sort(sideLengths);
-            
-            final var wrapLength = 2 * (sideLengths.get(0) + sideLengths.get(1));
-            final var bowLength = box.length * box.width * box.height;
-            
-            totalRibbonLength += wrapLength + bowLength;
-        }
-        
-        return totalRibbonLength;
+        return boxes
+            .stream()
+            .mapToInt(box -> {
+                var minLengthHeight = Integer.min(box.length, box.height);
+                var min = Integer.min(minLengthHeight, box.width);
+                var mid = Integer.max(minLengthHeight, box.width);
+                
+                var wrapLength = 2 * (min + mid);
+                var bowLength = box.length * box.width * box.height;
+                
+                return wrapLength + bowLength;
+            })
+            .sum();
     }
     
     private record Box(int length, int width, int height) {}
--- src/test/java/aoc/event/year2015/Day02Test.java
+++ src/test/java/aoc/event/year2015/Day02Test.java
@@ -1,31 +1,32 @@
 package aoc.event.year2015;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day02Test {
     
     @Test
     void increasingDimensions() {
-        final var input = "2x3x4";
-        final var solution = new Day02(input);
-        Assertions.assertEquals(58, solution.partOne());
-        Assertions.assertEquals(34, solution.partTwo());
+        var input = "2x3x4";
+        var solution = new Day02(input);
+        assertEquals(58, solution.partOne());
+        assertEquals(34, solution.partTwo());
     }
     
     @Test
     void smallSmallestSide() {
-        final var input = "1x1x10";
-        final var solution = new Day02(input);
-        Assertions.assertEquals(43, solution.partOne());
-        Assertions.assertEquals(14, solution.partTwo());
+        var input = "1x1x10";
+        var solution = new Day02(input);
+        assertEquals(43, solution.partOne());
+        assertEquals(14, solution.partTwo());
     }
     
     @Test
     void evenDimensions() {
-        final var input = "5x5x5";
-        final var solution = new Day02(input);
-        Assertions.assertEquals(175, solution.partOne());
-        Assertions.assertEquals(145, solution.partTwo());
+        var input = "5x5x5";
+        var solution = new Day02(input);
+        assertEquals(175, solution.partOne());
+        assertEquals(145, solution.partTwo());
     }
 }
