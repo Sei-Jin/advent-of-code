3841c0ca30cb71b33a6e450be52f110899654b8b
Sei-Jin <seijin.tufts@gmail.com>
2025-06-13 14:34:34 -0300

    [2015-06] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2015/Day06.java
+++ src/main/java/aoc/event/year2015/Day06.java
@@ -1,19 +1,17 @@
 package aoc.event.year2015;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /// # [2015-06: Probably a Fire Hazard](https://adventofcode.com/2015/day/6)
-public class Day06 implements DeprecatedSolver2 {
+public class Day06 implements Solver<Integer, Integer> {
     
     private static final Pattern INSTRUCTION_PATTERN =
         Pattern.compile("([\\w ]+) (\\d+),(\\d+) through (\\d+),(\\d+)");
-    
     private static final int GRID_SIZE = 1000;
-    
     private final List<Instruction> instructions;
     
     public Day06(String input) {
@@ -21,76 +19,54 @@ public class Day06 implements DeprecatedSolver2 {
     }
     
     private static List<Instruction> parse(String input) {
-        final var instructions = new ArrayList<Instruction>();
-        
-        for (final var line : input.lines().toList()) {
-            final var instruction = parseInstruction(line);
-            instructions.add(instruction);
-        }
-        
-        return instructions;
+        return input
+            .lines()
+            .map(INSTRUCTION_PATTERN::matcher)
+            .flatMap(Matcher::results)
+            .map(result -> {
+                var operation = Operation.parse(result.group(1));
+                var minX = Integer.parseInt(result.group(2));
+                var minY = Integer.parseInt(result.group(3));
+                var maxX = Integer.parseInt(result.group(4));
+                var maxY = Integer.parseInt(result.group(5));
+                return new Instruction(operation, minX, minY, maxX, maxY);
+            })
+            .toList();
     }
     
-    private static Instruction parseInstruction(String line) {
-        final var matcher = INSTRUCTION_PATTERN.matcher(line);
-        
-        if (matcher.find()) {
-            final var operation = Operation.of(matcher.group(1));
-            final var minX = Integer.parseInt(matcher.group(2));
-            final var minY = Integer.parseInt(matcher.group(3));
-            final var maxX = Integer.parseInt(matcher.group(4));
-            final var maxY = Integer.parseInt(matcher.group(5));
-            
-            return new Instruction(operation, minX, minY, maxX, maxY);
-        } else {
-            throw new AssertionError("Line did not meet the expect format" + line);
-        }
-    }
-    
-    private static int sumGrid(int[][] lightGrid) {
-        var sum = 0;
-        
-        for (int[] row : lightGrid) {
-            for (var column = 0; column < lightGrid[0].length; column++) {
-                sum += row[column];
-            }
-        }
-        
-        return sum;
-    }
-    
-    /// @return the number of lit lights after following the instructions.
     @Override
     public Integer partOne() {
-        final var lightGrid = new int[GRID_SIZE][GRID_SIZE];
+        var lightGrid = new boolean[GRID_SIZE][GRID_SIZE];
         
-        for (final var instruction : instructions) {
+        for (var instruction : instructions) {
             for (var row = instruction.minY; row <= instruction.maxY; row++) {
                 for (var column = instruction.minX; column <= instruction.maxX; column++) {
                     switch (instruction.operation) {
-                        case ON -> lightGrid[row][column] = 1;
-                        case OFF -> lightGrid[row][column] = 0;
-                        case TOGGLE -> {
-                            if (lightGrid[row][column] == 1) {
-                                lightGrid[row][column] = 0;
-                            } else {
-                                lightGrid[row][column] = 1;
-                            }
-                        }
+                        case ON -> lightGrid[row][column] = true;
+                        case OFF -> lightGrid[row][column] = false;
+                        case TOGGLE -> lightGrid[row][column] = !lightGrid[row][column];
                     }
                 }
             }
         }
-        
         return sumGrid(lightGrid);
     }
     
-    /// @return the total brightness of all lights combined after following Santa's instructions.
+    private static int sumGrid(boolean[][] lightGrid) {
+        var sum = 0;
+        for (var row : lightGrid) {
+            for (var column = 0; column < lightGrid[0].length; column++) {
+                sum += row[column] ? 1 : 0;
+            }
+        }
+        return sum;
+    }
+    
     @Override
     public Integer partTwo() {
-        final var lightGrid = new int[GRID_SIZE][GRID_SIZE];
+        var lightGrid = new int[GRID_SIZE][GRID_SIZE];
         
-        for (final var instruction : instructions) {
+        for (var instruction : instructions) {
             for (var row = instruction.minY; row <= instruction.maxY; row++) {
                 for (var column = instruction.minX; column <= instruction.maxX; column++) {
                     switch (instruction.operation) {
@@ -105,18 +81,26 @@ public class Day06 implements DeprecatedSolver2 {
                 }
             }
         }
-        
         return sumGrid(lightGrid);
+    }
+    
+    private static int sumGrid(int[][] lightGrid) {
+        var sum = 0;
+        for (var row : lightGrid) {
+            for (var column = 0; column < lightGrid[0].length; column++) {
+                sum += row[column];
+            }
+        }
+        return sum;
     }
     
     private record Instruction(Operation operation, int minX, int minY, int maxX, int maxY) {}
     
     private enum Operation {
-        ON,
-        OFF,
-        TOGGLE;
         
-        private static Operation of(String string) {
+        ON, OFF, TOGGLE;
+        
+        public static Operation parse(String string) {
             return switch (string) {
                 case "turn on" -> ON;
                 case "turn off" -> OFF;
--- src/test/java/aoc/event/year2015/Day06Test.java
+++ src/test/java/aoc/event/year2015/Day06Test.java
@@ -8,54 +8,54 @@ public class Day06Test {
     
     @Test
     void turnOnEverything() {
-        final var input = "turn on 0,0 through 999,999";
-        final var solution = new Day06(input);
+        var input = "turn on 0,0 through 999,999";
+        var solution = new Day06(input);
         assertEquals(1000000, solution.partOne());
         assertEquals(1000000, solution.partTwo());
     }
     
     @Test
     void toggleFirstRow() {
-        final var input = "toggle 0,0 through 999,0";
-        final var solution = new Day06(input);
+        var input = "toggle 0,0 through 999,0";
+        var solution = new Day06(input);
         assertEquals(1000, solution.partOne());
         assertEquals(2000, solution.partTwo());
     }
     
     @Test
     void turnOffMiddleFour() {
-        final var input = "turn off 499,499 through 500,500";
-        final var solution = new Day06(input);
+        var input = "turn off 499,499 through 500,500";
+        var solution = new Day06(input);
         assertEquals(0, solution.partOne());
         assertEquals(0, solution.partTwo());
     }
     
     @Test
     void turnOnEverythingThenTurnOffMiddleFour() {
-        final var input = """
+        var input = """
             turn on 0,0 through 999,999
             turn off 499,499 through 500,500
             """;
-        final var solution = new Day06(input);
+        var solution = new Day06(input);
         assertEquals(999996, solution.partOne());
         assertEquals(999996, solution.partTwo());
     }
     
     @Test
     void turnOnFirstTwice() {
-        final var input = """
+        var input = """
             turn on 0,0 through 0,0
             turn on 0,0 through 0,0
             """;
-        final var solution = new Day06(input);
+        var solution = new Day06(input);
         assertEquals(1, solution.partOne());
         assertEquals(2, solution.partTwo());
     }
     
     @Test
     void toggleEverything() {
-        final var input = "toggle 0,0 through 999,999";
-        final var solution = new Day06(input);
+        var input = "toggle 0,0 through 999,999";
+        var solution = new Day06(input);
         assertEquals(1000000, solution.partOne());
         assertEquals(2000000, solution.partTwo());
     }
