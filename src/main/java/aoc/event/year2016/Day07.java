cd86e640d0a0e4ed02112eb861af14bcf6da938b
Sei-Jin <seijin.tufts@gmail.com>
2025-06-13 14:42:04 -0300

    [2016-07] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2016/Day07.java
+++ src/main/java/aoc/event/year2016/Day07.java
@@ -1,13 +1,13 @@
 package aoc.event.year2016;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Gatherers;
 
 /// # [2016-07: Internet Protocol Version 7](https://adventofcode.com/2016/day/7)
-public class Day07 implements DeprecatedSolver2<Integer> {
+public class Day07 implements Solver<Integer, Integer> {
     
     private static List<Sequence> sequences;
     
@@ -19,27 +19,27 @@ public class Day07 implements DeprecatedSolver2<Integer> {
         return input
             .lines()
             .map(line -> {
-                final var outside = new ArrayList<Character>();
-                final var inside = new ArrayList<Character>();
-                
+                var outside = new ArrayList<Character>();
+                var inside = new ArrayList<Character>();
                 var isInside = false;
                 
                 for (int i = 0; i < line.length(); i++) {
-                    final var character = line.charAt(i);
+                    var character = line.charAt(i);
                     
                     if (character == '[') {
                         isInside = true;
-                    } else if (character == ']') {
+                    }
+                    else if (character == ']') {
                         isInside = false;
                     }
                     
                     if (isInside) {
                         inside.add(character);
-                    } else {
+                    }
+                    else {
                         outside.add(character);
                     }
                 }
-                
                 return new Sequence(outside, inside);
             })
             .toList();
@@ -50,8 +50,8 @@ public class Day07 implements DeprecatedSolver2<Integer> {
         return (int) sequences
             .stream()
             .filter(sequence -> {
-                final var hasAbbaOutside = hasAbba(sequence.outside());
-                final var hasAbbaInside = hasAbba(sequence.inside());
+                var hasAbbaOutside = hasAbba(sequence.outside());
+                var hasAbbaInside = hasAbba(sequence.inside());
                 return hasAbbaOutside && !hasAbbaInside;
             })
             .count();
@@ -68,39 +68,32 @@ public class Day07 implements DeprecatedSolver2<Integer> {
     
     @Override
     public Integer partTwo() {
-        var count = 0;
-        
-        for (final var pair : sequences) {
-            final var outsideAbas = pair.outside()
-                .stream()
-                .gather(Gatherers.windowSliding(3))
-                .filter(i -> i.get(0) == i.get(2) && i.get(0) != i.get(1))
-                .toList();
-            
-            final var outsideBabs = outsideAbas
-                .stream()
-                .map(list -> {
-                    final var inverted = new ArrayList<Character>();
-                    
-                    inverted.add(list.get(1));
-                    inverted.add(list.get(0));
-                    inverted.add(list.get(1));
-                    
-                    return inverted;
-                })
-                .toList();
-            
-            final var supportsSsl = pair.inside()
-                .stream()
-                .gather(Gatherers.windowSliding(3))
-                .anyMatch(i -> outsideBabs.contains(i.subList(0, i.size())));
-            
-            if (supportsSsl) {
-                count++;
-            }
-        }
-        
-        return count;
+        return (int) sequences
+            .stream()
+            .filter(pair -> {
+                var outsideAbas = pair.outside()
+                    .stream()
+                    .gather(Gatherers.windowSliding(3))
+                    .filter(i -> i.get(0) == i.get(2) && i.get(0) != i.get(1))
+                    .toList();
+                
+                var outsideBabs = outsideAbas
+                    .stream()
+                    .map(list -> {
+                        var inverted = new ArrayList<Character>();
+                        inverted.add(list.get(1));
+                        inverted.add(list.get(0));
+                        inverted.add(list.get(1));
+                        return inverted;
+                    })
+                    .toList();
+                
+                return pair.inside()
+                    .stream()
+                    .gather(Gatherers.windowSliding(3))
+                    .anyMatch(i -> outsideBabs.contains(i.subList(0, i.size())));
+            })
+            .count();
     }
     
     private record Sequence(List<Character> outside, List<Character> inside) {}
--- src/test/java/aoc/event/year2016/Day07Test.java
+++ src/test/java/aoc/event/year2016/Day07Test.java
@@ -12,29 +12,29 @@ public class Day07Test {
         
         @Test
         void example1() {
-            final var input = "abba[mnop]qrst";
-            final var solution = new Day07(input);
+            var input = "abba[mnop]qrst";
+            var solution = new Day07(input);
             assertEquals(1, solution.partOne());
         }
         
         @Test
         void example2() {
-            final var input = "abcd[bddb]xyyx";
-            final var solution = new Day07(input);
+            var input = "abcd[bddb]xyyx";
+            var solution = new Day07(input);
             assertEquals(0, solution.partOne());
         }
         
         @Test
         void example3() {
-            final var input = "aaaa[qwer]tyui";
-            final var solution = new Day07(input);
+            var input = "aaaa[qwer]tyui";
+            var solution = new Day07(input);
             assertEquals(0, solution.partOne());
         }
         
         @Test
         void example4() {
-            final var input = "ioxxoj[asdfgh]zxcvbn";
-            final var solution = new Day07(input);
+            var input = "ioxxoj[asdfgh]zxcvbn";
+            var solution = new Day07(input);
             assertEquals(1, solution.partOne());
         }
     }
@@ -44,29 +44,29 @@ public class Day07Test {
         
         @Test
         void example1() {
-            final var input = "aba[bab]xyz";
-            final var solution = new Day07(input);
+            var input = "aba[bab]xyz";
+            var solution = new Day07(input);
             assertEquals(1, solution.partTwo());
         }
         
         @Test
         void example2() {
-            final var input = "xyx[xyx]xyx";
-            final var solution = new Day07(input);
+            var input = "xyx[xyx]xyx";
+            var solution = new Day07(input);
             assertEquals(0, solution.partTwo());
         }
         
         @Test
         void example3() {
-            final var input = "aaa[kek]eke";
-            final var solution = new Day07(input);
+            var input = "aaa[kek]eke";
+            var solution = new Day07(input);
             assertEquals(1, solution.partTwo());
         }
         
         @Test
         void example4() {
-            final var input = "zazbz[bzb]cdb";
-            final var solution = new Day07(input);
+            var input = "zazbz[bzb]cdb";
+            var solution = new Day07(input);
             assertEquals(1, solution.partTwo());
         }
     }
