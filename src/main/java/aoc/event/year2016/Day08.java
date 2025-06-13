7215486586392c2a2725ecafcbfcc9d1f47fd6e0
Sei-Jin <seijin.tufts@gmail.com>
2025-06-13 15:23:19 -0300

    [2016-08] Update solution and implement new solver interface

--- src/main/java/aoc/event/year2016/Day08.java
+++ src/main/java/aoc/event/year2016/Day08.java
@@ -1,33 +1,37 @@
 package aoc.event.year2016;
 
-import aoc.DeprecatedSolver2;
+import aoc.Solver;
 
 import java.util.List;
 import java.util.regex.Pattern;
 
 /// # [2016-08: Two-Factor Authentication](https://adventofcode.com/2016/day/8)
-public class Day08 implements DeprecatedSolver2<Integer> {
+public class Day08 implements Solver<Integer, String> {
     
     private static final int SCREEN_WIDTH = 50;
     private static final int SCREEN_HEIGHT = 6;
     
-    private static List<String> lines;
+    private static List<Command> commands;
     
     public Day08(String input) {
-        lines = input.lines().toList();
+        commands = parse(input);
     }
     
-    @Override
-    public Integer partOne() {
-        final var screen = new int[SCREEN_HEIGHT][SCREEN_WIDTH];
-        
-        for (final var line : lines) {
-            final var command = Command.of(line);
-            
-            switch (command) {
+    private static List<Command> parse(String input) {
+        return input
+            .lines()
+            .map(Command::parse)
+            .toList();
+    }
+    
+    private static int[][] applyCommands(List<Command> commands) {
+        var screen = new int[SCREEN_HEIGHT][SCREEN_WIDTH];
+        for (var command : commands) {
+            var type = command.commandType;
+            switch (type) {
                 case RECTANGLE -> {
-                    final var columns = command.first();
-                    final var rows = command.second();
+                    var columns = command.first;
+                    var rows = command.second;
                     
                     for (int row = 0; row < rows; row++) {
                         for (int column = 0; column < columns; column++) {
@@ -36,53 +40,44 @@ public class Day08 implements DeprecatedSolver2<Integer> {
                     }
                 }
                 case ROTATE_ROW -> {
-                    final var row = command.first();
-                    final var offset = command.second();
-                    final var normalizedOffset = offset % SCREEN_WIDTH;
+                    var row = command.first;
+                    var offset = command.second;
+                    var normalizedOffset = offset % SCREEN_WIDTH;
                     
-                    final var temp = new int[SCREEN_WIDTH];
-                    
+                    var temp = new int[SCREEN_WIDTH];
                     for (int column = 0; column < SCREEN_WIDTH; column++) {
-                        final var newColumn = (column + normalizedOffset) % SCREEN_WIDTH;
+                        var newColumn = (column + normalizedOffset) % SCREEN_WIDTH;
                         temp[newColumn] = screen[row][column];
                     }
-                    
-                    for (int column = 0; column < temp.length; column++) {
-                        screen[row][column] = temp[column];
-                    }
+                    System.arraycopy(temp, 0, screen[row], 0, temp.length);
                 }
                 case ROTATE_COLUMN -> {
-                    final var column = command.first();
-                    final var offset = command.second();
-                    final var normalizedOffset = offset % SCREEN_HEIGHT;
+                    var column = command.first;
+                    var offset = command.second;
+                    var normalizedOffset = offset % SCREEN_HEIGHT;
                     
-                    final var temp = new int[SCREEN_HEIGHT];
-                    
+                    var temp = new int[SCREEN_HEIGHT];
                     for (int row = 0; row < SCREEN_HEIGHT; row++) {
-                        final var newRow = (row + normalizedOffset) % SCREEN_HEIGHT;
+                        var newRow = (row + normalizedOffset) % SCREEN_HEIGHT;
                         temp[newRow] = screen[row][column];
                     }
-                    
                     for (int row = 0; row < temp.length; row++) {
                         screen[row][column] = temp[row];
                     }
                 }
             }
-            
-            for (int i = 1; i <= SCREEN_HEIGHT; i++) {
-                for (int j = 1; j <= SCREEN_WIDTH; j++) {
-                    System.out.print(screen[i - 1][j - 1] + " ");
-                    if (j % 5 == 0) {
-                        System.out.print("    ");
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println();
         }
-        
+        return screen;
+    }
+    
+    @Override
+    public Integer partOne() {
+        var screen = applyCommands(commands);
+        return count(screen);
+    }
+    
+    private static int count(int[][] screen) {
         var count = 0;
-        
         for (int i = 0; i < SCREEN_HEIGHT; i++) {
             for (int j = 0; j < SCREEN_WIDTH; j++) {
                 if (screen[i][j] > 0) {
@@ -90,56 +85,54 @@ public class Day08 implements DeprecatedSolver2<Integer> {
                 }
             }
         }
-        
         return count;
     }
     
     @Override
-    public Integer partTwo() {
-        return 0;
+    public String partTwo() {
+        var screen = applyCommands(commands);
+        print(screen);
+        return "Output";
     }
     
-    private enum Command {
+    private static void print(int[][] screen) {
+        for (int i = 1; i <= SCREEN_HEIGHT; i++) {
+            for (int j = 1; j <= SCREEN_WIDTH; j++) {
+                System.out.print(screen[i - 1][j - 1] + " ");
+                if (j % 5 == 0) {
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    private enum CommandType {
+        
         RECTANGLE("rect (\\d+)x(\\d+)"),
         ROTATE_COLUMN("rotate column x=(\\d+) by (\\d+)"),
         ROTATE_ROW("rotate row y=(\\d+) by (\\d+)");
         
         private final Pattern pattern;
-        private int first;
-        private int second;
         
-        Command(String String) {
-            this.pattern = Pattern.compile(String);
+        CommandType(String regex) {
+            this.pattern = Pattern.compile(regex);
         }
+    }
+    
+    private record Command(CommandType commandType, int first, int second) {
         
-        public int first() {
-            return first;
-        }
-        
-        public int second() {
-            return second;
-        }
-        
-        public static Command of(String string) {
-            for (final var command : Command.values()) {
-                if (command.parse(string)) {
-                    return command;
+        public static Command parse(String input) {
+            for (var command : CommandType.values()) {
+                var matcher = command.pattern.matcher(input);
+                if (matcher.matches()) {
+                    int first = Integer.parseInt(matcher.group(1));
+                    int second = Integer.parseInt(matcher.group(2));
+                    return new Command(command, first, second);
                 }
             }
-            
-            throw new IllegalArgumentException("Input did not match any commands: " + string);
-        }
-        
-        private boolean parse(String string) {
-            final var matcher = this.pattern.matcher(string);
-            
-            if (matcher.find()) {
-                this.first = Integer.parseInt(matcher.group(1));
-                this.second = Integer.parseInt(matcher.group(2));
-                return true;
-            }
-            
-            return false;
+            throw new IllegalArgumentException("Input did not match any commands: " + input);
         }
     }
 }
--- src/test/java/aoc/event/year2016/Day08Test.java
+++ src/test/java/aoc/event/year2016/Day08Test.java
@@ -8,24 +8,23 @@ class Day08Test {
     
     @Test
     void example() {
-        final var input = """
+        var input = """
             rect 3x2
             rotate column x=1 by 1
             rotate row y=0 by 4
             rotate column x=1 by 1
             """;
-        final var solution = new Day08(input);
+        var solution = new Day08(input);
         assertEquals(6, solution.partOne());
     }
     
     @Test
     void example2() {
-        final var input = """
+        var input = """
             rect 3x2
             rotate column x=1 by 1
             """;
-        final var solution = new Day08(input);
+        var solution = new Day08(input);
         assertEquals(6, solution.partOne());
     }
-    
 }
